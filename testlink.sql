87f6a7528c967ae8907ebb076ec7341ffeff46d6
chadrosen <>
2004-11-30 23:54:57 +0000

    Updating schema for a few changes.. Moving priority/risk/owner to test case instead of category table

--- testlink.sql
+++ testlink.sql
@@ -1,20 +1,20 @@
-# phpMyAdmin SQL Dump
-# version 2.5.3
-# http://www.phpmyadmin.net
-#
-# Host: localhost
-# Generation Time: Oct 14, 2003 at 02:53 PM
-# Server version: 4.0.15
-# PHP Version: 4.3.3
-# 
-# Database : `TestLink`
-# 
+-- phpMyAdmin SQL Dump
+-- version 2.6.0-pl1
+-- http://www.phpmyadmin.net
+-- 
+-- Host: localhost
+-- Generation Time: Nov 30, 2004 at 03:49 PM
+-- Server version: 4.0.18
+-- PHP Version: 4.3.9
+-- 
+-- Database: `testlink`
+-- 
 
-# --------------------------------------------------------
+-- --------------------------------------------------------
 
-#
-# Table structure for table `bugs`
-#
+-- 
+-- Table structure for table `bugs`
+-- 
 
 CREATE TABLE `bugs` (
   `tcid` int(11) NOT NULL default '0',
@@ -25,10 +25,12 @@ CREATE TABLE `bugs` (
   KEY `build` (`build`),
   KEY `bug` (`bug`)
 ) TYPE=MyISAM COMMENT='This table holds the bugs filed for each result';
-    
-#
-# Table structure for table `build`
-#
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `build`
+-- 
 
 CREATE TABLE `build` (
   `build` int(11) NOT NULL default '0',
@@ -36,31 +38,28 @@ CREATE TABLE `build` (
   PRIMARY KEY  (`build`,`projid`)
 ) TYPE=MyISAM COMMENT='This table holds all of the available builds';
 
-# --------------------------------------------------------
+-- --------------------------------------------------------
 
-#
-# Table structure for table `category`
-#
+-- 
+-- Table structure for table `category`
+-- 
 
 CREATE TABLE `category` (
   `id` int(10) unsigned NOT NULL auto_increment,
   `name` text,
   `compid` int(10) unsigned default NULL,
-  `importance` enum('L','M','H') NOT NULL default 'M',
-  `risk` enum('1','2','3') NOT NULL default '2',
-  `owner` varchar(20) default 'none',
   `mgtcatid` int(11) NOT NULL default '0',
   `CATorder` int(11) NOT NULL default '0',
   PRIMARY KEY  (`id`),
   KEY `id` (`id`),
   KEY `compid` (`compid`)
-) TYPE=MyISAM COMMENT='This table holds all of the category information' AUTO_INCREMENT=121 ;
+) TYPE=MyISAM COMMENT='This table holds all of the category information' AUTO_INCREMENT=124 ;
 
-# --------------------------------------------------------
+-- --------------------------------------------------------
 
-#
-# Table structure for table `component`
-#
+-- 
+-- Table structure for table `component`
+-- 
 
 CREATE TABLE `component` (
   `id` int(10) unsigned NOT NULL auto_increment,
@@ -70,13 +69,13 @@ CREATE TABLE `component` (
   PRIMARY KEY  (`id`),
   KEY `id` (`id`),
   KEY `projid` (`projid`)
-) TYPE=MyISAM AUTO_INCREMENT=34 ;
+) TYPE=MyISAM AUTO_INCREMENT=36 ;
 
-# --------------------------------------------------------
+-- --------------------------------------------------------
 
-#
-# Table structure for table `keywords`
-#
+-- 
+-- Table structure for table `keywords`
+-- 
 
 CREATE TABLE `keywords` (
   `id` int(11) NOT NULL auto_increment,
@@ -84,13 +83,13 @@ CREATE TABLE `keywords` (
   `prodid` int(11) NOT NULL default '0',
   `notes` text,
   PRIMARY KEY  (`id`)
-) TYPE=MyISAM COMMENT='this table holds all of the keywords for each product' AUTO_INCREMENT=1 ;
+) TYPE=MyISAM COMMENT='this table holds all of the keywords for each product' AUTO_INCREMENT=5 ;
 
-# --------------------------------------------------------
+-- --------------------------------------------------------
 
-#
-# Table structure for table `mgtcategory`
-#
+-- 
+-- Table structure for table `mgtcategory`
+-- 
 
 CREATE TABLE `mgtcategory` (
   `id` int(11) NOT NULL auto_increment,
@@ -104,11 +103,13 @@ CREATE TABLE `mgtcategory` (
   `CATorder` int(11) NOT NULL default '0',
   PRIMARY KEY  (`id`),
   KEY `compid` (`compid`)
-) TYPE=MyISAM COMMENT='The different categories of the TC management DB' AUTO_INCREMENT=71 ;
+) TYPE=MyISAM COMMENT='The different categories of the TC management DB' AUTO_INCREMENT=80 ;
 
-#
-# Table structure for table `mgtcomponent`
-#
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `mgtcomponent`
+-- 
 
 CREATE TABLE `mgtcomponent` (
   `id` int(11) NOT NULL auto_increment,
@@ -121,25 +122,26 @@ CREATE TABLE `mgtcomponent` (
   `prodid` int(11) default NULL,
   PRIMARY KEY  (`id`),
   KEY `prodid` (`prodid`)
-) TYPE=MyISAM COMMENT='The different components of the TC management DB' AUTO_INCREMENT=1 ;
+) TYPE=MyISAM COMMENT='The different components of the TC management DB' AUTO_INCREMENT=8 ;
 
+-- --------------------------------------------------------
 
-#
-# Table structure for table `mgtproduct`
-#
+-- 
+-- Table structure for table `mgtproduct`
+-- 
 
 CREATE TABLE `mgtproduct` (
   `id` int(11) NOT NULL auto_increment,
   `name` text NOT NULL,
   `color` varchar(12) NOT NULL default '#0066CC',
   PRIMARY KEY  (`id`)
-) TYPE=MyISAM COMMENT='The different products of the TC management DB' AUTO_INCREMENT=3 ;
+) TYPE=MyISAM COMMENT='The different products of the TC management DB' AUTO_INCREMENT=6 ;
 
-# --------------------------------------------------------
+-- --------------------------------------------------------
 
-#
-# Table structure for table `mgttcarchive`
-#
+-- 
+-- Table structure for table `mgttcarchive`
+-- 
 
 CREATE TABLE `mgttcarchive` (
   `id` int(11) NOT NULL default '0',
@@ -154,10 +156,11 @@ CREATE TABLE `mgttcarchive` (
   KEY `id` (`id`)
 ) TYPE=MyISAM COMMENT='The test case archive for the TC management DB';
 
+-- --------------------------------------------------------
 
-#
-# Table structure for table `mgttestcase`
-#
+-- 
+-- Table structure for table `mgttestcase`
+-- 
 
 CREATE TABLE `mgttestcase` (
   `id` int(11) NOT NULL auto_increment,
@@ -172,11 +175,13 @@ CREATE TABLE `mgttestcase` (
   `TCorder` int(11) NOT NULL default '0',
   PRIMARY KEY  (`id`),
   KEY `catid` (`catid`)
-) TYPE=MyISAM COMMENT='The different test cases of the TC management DB' AUTO_INCREMENT=146 ;
+) TYPE=MyISAM COMMENT='The different test cases of the TC management DB' AUTO_INCREMENT=186 ;
 
-#
-# Table structure for table `milestone`
-#
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `milestone`
+-- 
 
 CREATE TABLE `milestone` (
   `id` int(11) NOT NULL auto_increment,
@@ -189,16 +194,11 @@ CREATE TABLE `milestone` (
   PRIMARY KEY  (`id`)
 ) TYPE=MyISAM AUTO_INCREMENT=1 ;
 
-#
-# Dumping data for table `milestone`
-#
+-- --------------------------------------------------------
 
-
-# --------------------------------------------------------
-
-#
-# Table structure for table `priority`
-#
+-- 
+-- Table structure for table `priority`
+-- 
 
 CREATE TABLE `priority` (
   `id` int(11) NOT NULL auto_increment,
@@ -206,30 +206,28 @@ CREATE TABLE `priority` (
   `riskImp` char(2) NOT NULL default '',
   `priority` enum('a','b','c') NOT NULL default 'b',
   PRIMARY KEY  (`id`)
-) TYPE=MyISAM AUTO_INCREMENT=325 ;
+) TYPE=MyISAM AUTO_INCREMENT=343 ;
 
-#
-# Dumping data for table `priority`
-#
+-- --------------------------------------------------------
 
-# --------------------------------------------------------
-
-#
-# Table structure for table `project`
-#
+-- 
+-- Table structure for table `project`
+-- 
 
 CREATE TABLE `project` (
   `id` int(10) NOT NULL auto_increment,
   `name` text,
   `notes` text,
   `active` enum('y','n') NOT NULL default 'y',
-  PRIMARY KEY  (`id`)
-) TYPE=MyISAM COMMENT='This table holds all of the project information' AUTO_INCREMENT=113 ;
+  PRIMARY KEY  (`id`),
+  KEY `id` (`id`)
+) TYPE=MyISAM COMMENT='This table holds all of the project information' AUTO_INCREMENT=115 ;
 
+-- --------------------------------------------------------
 
-#
-# Table structure for table `projrights`
-#
+-- 
+-- Table structure for table `projrights`
+-- 
 
 CREATE TABLE `projrights` (
   `userid` int(11) NOT NULL default '0',
@@ -237,10 +235,11 @@ CREATE TABLE `projrights` (
   PRIMARY KEY  (`userid`,`projid`)
 ) TYPE=MyISAM COMMENT='User''s project permissions';
 
+-- --------------------------------------------------------
 
-#
-# Table structure for table `results`
-#
+-- 
+-- Table structure for table `results`
+-- 
 
 CREATE TABLE `results` (
   `build` int(11) NOT NULL default '0',
@@ -251,13 +250,15 @@ CREATE TABLE `results` (
   `tcid` int(10) unsigned NOT NULL default '0',
   `notes` text,
   PRIMARY KEY  (`tcid`,`build`),
-  KEY `tcid` (`tcid`)
+  KEY `tcid` (`tcid`),
+  KEY `status` (`status`)
 ) TYPE=MyISAM;
 
+-- --------------------------------------------------------
 
-#
-# Table structure for table `rights`
-#
+-- 
+-- Table structure for table `rights`
+-- 
 
 CREATE TABLE `rights` (
   `id` int(11) NOT NULL auto_increment,
@@ -265,6 +266,9 @@ CREATE TABLE `rights` (
   `rights` text NOT NULL,
   PRIMARY KEY  (`id`)
 ) TYPE=MyISAM COMMENT='This table holds all of the rights (permissions)' AUTO_INCREMENT=10 ;
+
+-- --------------------------------------------------------
+
 
 #
 # Dumping data for table `rights`
@@ -276,11 +280,9 @@ INSERT INTO `rights` (`id`, `role`, `rights`) VALUES (7, 'otester', 'tp_execute,
 INSERT INTO `rights` (`id`, `role`, `rights`) VALUES (8, 'admin', 'tp_execute,tp_create_build,tp_metrics,tp_planning,tp_assign_rights,mgt_view_tc,mgt_modify_tc,mgt_view_key,mgt_modify_key,mgt_view_product,mgt_modify_product,mgt_users');
 INSERT INTO `rights` (`id`, `role`, `rights`) VALUES (9, 'lead', 'tp_execute,tp_create_build,tp_metrics,tp_planning,tp_assign_rights,mgt_view_tc,mgt_modify_tc,mgt_view_key,mgt_modify_key,mgt_view_product');
 
-# --------------------------------------------------------
-
-#
-# Table structure for table `testcase`
-#
+-- 
+-- Table structure for table `testcase`
+-- 
 
 CREATE TABLE `testcase` (
   `id` int(10) unsigned NOT NULL auto_increment,
@@ -294,25 +296,14 @@ CREATE TABLE `testcase` (
   `mgttcid` int(11) NOT NULL default '0',
   `keywords` text,
   `TCorder` int(11) NOT NULL default '0',
+  `risk` enum('1','2','3') NOT NULL default '2',
+  `importance` enum('L','M','H') NOT NULL default 'M',
+  `owner` int(11) NOT NULL default '0',
   PRIMARY KEY  (`id`),
-  KEY `id` (`id`)
-) TYPE=MyISAM COMMENT='This table holds all of the test case information' AUTO_INCREMENT=9 ;
-
-
-#
-# Table structure for table `user`
-#
-
-CREATE TABLE `user` (
-  `password` varchar(15) default NULL,
-  `login` varchar(15) default NULL,
-  `id` int(10) unsigned NOT NULL auto_increment,
-  `rightsid` int(11) NOT NULL default '0',
-  `email` varchar(35) NOT NULL default '',
-  `first` varchar(20) NOT NULL default '',
-  `last` varchar(20) NOT NULL default '',
-  PRIMARY KEY  (`id`)
-) TYPE=MyISAM COMMENT='This table holds all of the user information' AUTO_INCREMENT=45 ;
+  KEY `id` (`id`),
+  KEY `mgttcid` (`mgttcid`),
+  KEY `catid` (`catid`)
+) TYPE=MyISAM COMMENT='This table holds all of the test case information' AUTO_INCREMENT=20 ;
 
 #
 # Dumping data for table `user`

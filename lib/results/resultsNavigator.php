5e503b821d454d6f17169b2ea06a818d292074f5
havlat <>
2005-08-31 16:05:52 +0000

    added REQ support

--- lib/results/resultsNavigator.php
+++ lib/results/resultsNavigator.php
@@ -1,38 +1,43 @@
 <?php
 /** 
-* TestLink Open Source Project - http://testlink.sourceforge.net/ 
-* $Id: resultsNavigator.php,v 1.2 2005/08/16 18:00:58 franciscom Exp $ 
-*
-* @author	Martin Havlat <havlat@users.sourceforge.net>
-* 
-* This page list View of Test Results and Metrics.
-*
-* @todo Reload workarea if build is changed 
-* @todo xls ouput should be general over all builds
-*
-*
-* @author 20050807 - fm
-* refactoring:  
-* removed deprecated: $_SESSION['project']
-*
-*/
+ * TestLink Open Source Project - http://testlink.sourceforge.net/ 
+ * This script is distributed under the GNU General Public License 2 or later. 
+ * @version $Id: resultsNavigator.php,v 1.3 2005/08/31 16:05:52 havlat Exp $ 
+ * @author	Martin Havlat <havlat@users.sourceforge.net>
+ * 
+ * This page list View of Test Results and Metrics.
+ *
+ * @todo Reload all workarea if build is changed 
+ * @todo xls ouput should be general over all builds
+ *
+ * Revisions:
+ * 
+ * 20050807 - fm - refactoring; removed deprecated: $_SESSION['project']
+ * 20050831 - MHT - added req report; removed doubled include for lang support; updated file header
+ *
+ */
 require('../../config.inc.php');
 require_once('common.php');
 require_once('builds.inc.php');
-require_once("../../lib/functions/lang_api.php");
+
 testlinkInitPage();
 
 // there is list of available results and metrics view
 $arrData = array(
 	array('name' => lang_get('link_report_general_tp_metrics'), 'href' => 'resultsGeneral.php'),
 	array('name' => lang_get('link_report_overall_build'), 'href' => 'resultsAllBuilds.php'),
+    array('name' => lang_get('link_report_metrics_more_builds'), 'href' => 'resultsMoreBuilds.php'), // 'Query by Start and End Build'
 	array('name' => lang_get('link_report_test'), 'href' => 'resultsTC.php'),
 	array('name' => lang_get('link_report_excel'), 'href' => 'resultsTC.php?format=excel'),
 	array('name' => lang_get('link_report_blocked_tcs'), 'href' => 'resultsByStatus.php?type=b'),
 	array('name' => lang_get('link_report_failed'), 'href' => 'resultsByStatus.php?type=f'),
 	array('name' => lang_get('link_report_total_bugs'), 'href' => 'resultsBugs.php'),
-      array('name' => 'Query by Start and End Build', 'href' => 'mine_results.php'),
-  );
+);
+
+if ($_SESSION['productOptReqs'])
+{
+	$arrData[] = array('name' => lang_get('link_report_reqs_coverage'), 'href' => 'resultsReqs.php');
+}
 
 // this results are related to selected build
 $arrDataB = array(
@@ -40,7 +45,6 @@ $arrDataB = array(
 );
 
 // collect builds of Test Plan
-//
 // 20050807 - fm 
 $arrBuilds = getBuilds($_SESSION['testPlanId']);
 if (isset($_GET['build']))
@@ -55,4 +59,5 @@ $smarty->assign('arrDataB', $arrDataB);
 $smarty->assign('arrBuilds', $arrBuilds);
 $smarty->assign('selectedBuild', $selectedBuild);
 $smarty->display('resultsNavigator.tpl');
+
 ?>

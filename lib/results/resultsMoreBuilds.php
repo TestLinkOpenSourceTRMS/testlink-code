2d5f868771be5ad270700b57ba8f5173c266ac31
kevinlevy <>
2005-09-02 05:11:07 +0000

    provide arrays of users and owners to smarty template to use

--- lib/results/resultsMoreBuilds.php
+++ lib/results/resultsMoreBuilds.php
@@ -1,7 +1,7 @@
 <?php
 /** 
 * TestLink Open Source Project - http://testlink.sourceforge.net/ 
-* $Id: resultsMoreBuilds.php,v 1.2 2005/09/01 20:39:06 schlundus Exp $ 
+* $Id: resultsMoreBuilds.php,v 1.3 2005/09/02 05:11:07 kevinlevy Exp $ 
 *
 * @author	Kevin Levy <kevinlevy@users.sourceforge.net>
 * 
@@ -12,12 +12,19 @@
 require('../../config.inc.php');
 require_once('common.php');
 require_once('builds.inc.php');
+// allow us to retreive array of users 
+require_once('plan.core.inc.php');
+require_once('../keywords/keywords.inc.php');
 testlinkInitPage();
 
 $arrBuilds = getBuilds($_SESSION['testPlanId']); // get Builds
+$arrOwners = getProjectUsers();
+$arrKeywords = selectKeywords();
 $smarty = new TLSmarty();
 $smarty->assign('testPlanName',$_SESSION['testPlanName']);
 $smarty->assign('projectid', $_SESSION['project']);
 $smarty->assign('arrBuilds', $arrBuilds); 
+$smarty->assign('arrOwners', $arrOwners);
+$smarty->assign('arrKeywords', $arrKeywords);
 $smarty->display('resultsMoreBuilds_query_form.tpl');
 ?>

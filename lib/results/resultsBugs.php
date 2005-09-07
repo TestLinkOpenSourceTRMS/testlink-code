7e9610ed7cebcbb579938ceebf7659f0206cd1e6
franciscom <>
2005-09-07 12:29:22 +0000

    *** empty log message ***

--- lib/results/resultsBugs.php
+++ lib/results/resultsBugs.php
@@ -1,7 +1,7 @@
 <?php
 /** 
 * TestLink Open Source Project - http://testlink.sourceforge.net/ 
-* $Id: resultsBugs.php,v 1.3 2005/09/06 20:19:40 schlundus Exp $ 
+* $Id: resultsBugs.php,v 1.4 2005/09/07 12:29:22 franciscom Exp $ 
 *
 * @author	Martin Havlat <havlat@users.sourceforge.net>
 * 
@@ -14,7 +14,7 @@ require_once('results.inc.php');
 require_once("../../lib/functions/lang_api.php");
 testlinkInitPage();
 
-// get Priority data
+
 $arrData = getBugsReport($_SESSION['testPlanId']);
 
 $smarty = new TLSmarty;

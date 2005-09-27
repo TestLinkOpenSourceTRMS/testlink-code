4a90f87fe4610efa1ae742d756e6ef347a95e703
franciscom <>
2005-09-27 06:44:12 +0000

    bug fixed

--- install/sql/alter_tables/1.0.4_to_1.6/db_version_table.sql
+++ install/sql/alter_tables/1.0.4_to_1.6/db_version_table.sql
@@ -2,5 +2,5 @@
 CREATE TABLE `db_version` (
   version varchar(50) NOT NULL default '1.6 BETA 1',
   upgrade_date datetime NOT NULL default '0000-00-00 00:00'
-)
-INSERT INTO db_version VALUES('1.6 BETA 1', CURRENT_TIMESTAMP());
+);
+INSERT INTO db_version VALUES('1.6 POST RC1', CURRENT_TIMESTAMP());

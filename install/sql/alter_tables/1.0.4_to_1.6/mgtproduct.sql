0bc7715c1d305a86e5c610e87f279871b4873213
franciscom <>
2005-10-05 17:17:42 +0000

    BUGID  0000158: Failed to update database! - details: Product <Name>

--- install/sql/alter_tables/1.0.4_to_1.6/mgtproduct.sql
+++ install/sql/alter_tables/1.0.4_to_1.6/mgtproduct.sql
@@ -5,7 +5,8 @@ ALTER TABLE `mgtproduct` MODIFY `color` varchar(12) NOT NULL default '#9BD';
 
 ALTER TABLE `mgtproduct` ADD COLUMN `active` BOOL  NOT NULL default 1;
 
-ALTER TABLE `mgtproduct` ADD COLUMN `option_requirements` enum('Y','N') NOT NULL default 'N';
+/* 20051005 - fm - BUGID  0000158: Failed to update database! - details: Product <Name> */
+ALTER TABLE `mgtproduct` ADD COLUMN `option_reqs`bool NOT NULL default 0;
 
 ALTER TABLE `mgtproduct` ADD UNIQUE KEY `name` (`name`);
 ALTER TABLE `mgtproduct` ADD INDEX  `ActiveId` (`id`,`active`);

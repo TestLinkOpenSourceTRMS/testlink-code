6f445e962d80d518c2ee3a1ac71405b92792955e
asielb <>
2005-10-12 21:43:29 +0000

    updated rights for users upgrading from 1.5. This is primarily for mgt_view_reqs and mgt_modify_reqs. These permissions now match with those in the testlink_create_default_data.sql used for 1.6 clean install.

---
+++ install/sql/alter_tables/1.5_to_1.6/rights.sql
@@ -0,0 +1,6 @@
+UPDATE `rights` SET `rights` = 'tp_execute,tp_create_build,tp_metrics,tp_planning,tp_assign_rights,mgt_view_tc,mgt_modify_tc,mgt_view_key,mgt_modify_key,mgt_view_req,mgt_modify_req,mgt_modify_product,mgt_users' WHERE `id` = 8 LIMIT 1;
+UPDATE `rights` SET `rights` = 'tp_execute,tp_create_build,tp_metrics,tp_planning,tp_assign_rights,mgt_view_tc,mgt_modify_tc,mgt_view_key,mgt_modify_key,mgt_view_req,mgt_modify_req'  WHERE `id` = 9 LIMIT 1;
+UPDATE `rights` SET `rights` = 'tp_execute,tp_metrics,tp_create_build,mgt_view_tc,mgt_modify_tc,mgt_view_key,mgt_view_req' WHERE `id` = 6 LIMIT 1;
+UPDATE `rights` SET `rights` = 'tp_execute,tp_metrics,mgt_view_tc,mgt_view_key,mgt_view_req' WHERE `id` = 7 LIMIT 1;
+UPDATE `rights` SET `rights` = 'tp_metrics,mgt_view_tc,mgt_view_key,mgt_view_req' WHERE `id` = 5 LIMIT 1;
+UPDATE `rights` SET `rights` = 'tp_metrics,mgt_view_tc,mgt_modify_tc,mgt_view_key,mgt_modify_req,mgt_view_req' WHERE `id` = 4 LIMIT 1;

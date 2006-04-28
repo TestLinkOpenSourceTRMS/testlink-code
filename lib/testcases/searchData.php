0ca97c76bf208d7a0018b79a8fd9406fe0a2c0bc
franciscom <>
2006-04-28 17:07:41 +0000

    removed debug output

--- lib/testcases/searchData.php
+++ lib/testcases/searchData.php
@@ -1,15 +1,16 @@
 <?php
 /* TestLink Open Source Project - http://testlink.sourceforge.net/
- * $Id: searchData.php,v 1.15 2006/04/26 07:07:56 franciscom Exp $
+ * $Id: searchData.php,v 1.16 2006/04/28 17:07:41 franciscom Exp $
  * Purpose:  This page presents the search results. 
  *
+ * 20060427 - franciscom - added include tescase class
  * 20050821 - fm - changes to use template customization (trying to reduce code redundancy)
 **/
 require('../../config.inc.php');
 require_once("../functions/common.php");
 require_once("../functions/users.inc.php");
-//require("../functions/testproject.class.php");
 
+require_once(dirname(__FILE__) . "/../functions/testcase.class.php"); // 20060427 - franciscom
 
 testlinkInitPage($db);
 
@@ -20,19 +21,26 @@ $name = isset($_POST['name']) ? $db->prepare_string($_POST['name']) : null;
 $summary = isset($_POST['summary']) ? $db->prepare_string($_POST['summary']) : null;
 $steps = isset($_POST['steps']) ? $db->prepare_string($_POST['steps']) : null;
 $exresult = isset($_POST['exresult']) ? $db->prepare_string($_POST['exresult']) : null;
-$key = isset($_POST['key']) ? $db->prepare_string($_POST['key']) : null;
+$keyword_id = isset($_POST['key']) ? $db->prepare_string($_POST['key']) : null;
 $tc_id = isset($_POST['TCID']) ? $db->prepare_string($_POST['TCID']) : 0;
 
 $arrTc = null;
 $tproject = isset($_SESSION['testprojectID']) ? $_SESSION['testprojectID'] : 0;
 $tproject_mgr = new testproject($db);
 
-
-$filter=array('by_tc_id' => '', 'by_name' => '', 'by_summary' => '');
+// 20060427 - franciscom
+$filter=array('by_tc_id' => '', 'by_name' => '', 
+              'by_summary' => '', 'by_keyword_id' => '');
+$from  =array('by_keyword_id' => ' ');
 
 if ($tproject > 0)
 {
+    // Due to changes in the DB schema we can't do a simple query to
+    // filter testcase by Test project. We need to walk the tree hierarchy.
+    //
     $a_tcid=$tproject_mgr->get_all_testcases_id($tproject);
+    
+    // Now start building the query to filter
 		if( count($a_tcid) > 0 )
 		{
 			  if($tc_id > 0)
@@ -44,25 +52,28 @@ if ($tproject > 0)
 						$filter['by_tc_id'] = " AND NHB.parent_id IN (" . implode(",",$a_tcid) . ") ";
 				}
 
-        if( !is_null($name) )				
+        if( !is_null($name) && strlen(trim($name)) > 0)				
         {
         	  $filter['by_name'] = " AND NHA.name like '%{$name}%' ";	
         }
+
+        if( !is_null($keyword_id) )				
+        {
+            $from['by_keyword_id'] = ' ,testcase_keywords KW';
+        	  $filter['by_keyword_id'] = " AND NHA.id = KW.testcase_id 
+        	                               AND KW.keyword_id = {$keyword_id} ";	
+        }
+
      
 				$sql = " SELECT NHA.id AS testcase_id,NHA.name,summary,steps,version
-	      			   FROM nodes_hierarchy NHA, nodes_hierarchy NHB, tcversions
+	      			   FROM nodes_hierarchy NHA, nodes_hierarchy NHB, tcversions {$from['by_keyword_id']}
 	        			 WHERE NHA.id = NHB.parent_id
 	        			 AND   NHB.id = tcversions.id
-	        			 {$filter['by_tc_id']} {$filter['by_name']}
+	        			 {$filter['by_tc_id']} {$filter['by_name']} {$filter['by_keyword_id']}
 	        			 ";  
-	        echo "<br>debug - <b><i>" . __FUNCTION__ . "</i></b><br><b>" . $sql . "</b><br>";
-  			 
 		}
 
 		$map = $db->fetchRowsIntoMap($sql,'testcase_id');
-
-   echo "<pre>debug" . __FUNCTION__; print_r($map); echo "</pre>"; 
-
 }
 
 
@@ -70,11 +81,7 @@ $smarty = new TLSmarty();
 if( count($map) > 0 )
 {
   $tcase_mgr = new testcase($db);   
-  //foreach($map as $tcase_id => $tcase_data)
-  //{
-    $tcase_mgr->show_multi($smarty,array_keys($map), $_SESSION['userID']);
-  //}
-  
+  $tcase_mgr->show($smarty,array_keys($map), $_SESSION['userID']);
 }
 
 /*

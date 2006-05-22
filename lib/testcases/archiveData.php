6e596105782066c9f7e5020dec57aa97a0cd02c4
franciscom <>
2006-05-22 15:45:02 +0000

    *** empty log message ***

--- lib/testcases/archiveData.php
+++ lib/testcases/archiveData.php
@@ -3,7 +3,7 @@
  * TestLink Open Source Project - http://testlink.sourceforge.net/ 
  * This script is distributed under the GNU General Public License 2 or later. 
  *  
- * @version $Id: archiveData.php,v 1.13 2006/05/17 19:12:17 schlundus Exp $
+ * @version $Id: archiveData.php,v 1.14 2006/05/22 15:45:02 franciscom Exp $
  * @author Martin Havlat
  *  
  * This page allows you to show data (test cases, categories, and
@@ -16,6 +16,7 @@ require_once('common.php');
 require_once('archive.inc.php');
 testlinkInitPage($db);
 
+// 20060518 - franciscom
 $user_id = isset($_SESSION['userID']) ? $_SESSION['userID'] : 0;
 
 $feature = isset($_GET['edit']) ? $_GET['edit'] : null;
@@ -23,21 +24,26 @@ $id = isset($_GET['data']) ? intval($_GET['data']) : null;
 $allow_edit = isset($_GET['allow_edit']) ? intval($_GET['allow_edit']) : 1;
 
 // load data and show template
+$smarty = new TLSmarty();
+
 switch($feature)
 {
 	case 'testproject':
 		$item_mgr = new testproject($db);
+    $item_mgr->show($smarty,$id);
 		break;
 	case 'testsuite':
 		$item_mgr = new testsuite($db);
+    $item_mgr->show($smarty,$id);
 		break;
 	case 'testcase':
 		$item_mgr = new testcase($db);
+		$item_mgr->show($smarty,$id,$user_id);
 		break;
 	default:
 		tLog('$_GET["edit"] has invalid value: ' . $feature , 'ERROR');
 		trigger_error($_SESSION['user'].'> $_GET["edit"] has invalid value.', E_USER_ERROR);
 }
-$smarty = new TLSmarty();
-$item_mgr->show($smarty,$id,$user_id);
+
+
 ?>

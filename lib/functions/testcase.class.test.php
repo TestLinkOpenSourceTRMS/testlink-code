30601b7e22019d264853c1f3db2a572f32f15c29
kevinlevy <>
2006-07-03 18:51:03 +0000

    added additional methods - fully smartied now

--- lib/functions/testcase.class.test.php
+++ lib/functions/testcase.class.test.php
@@ -13,93 +13,119 @@ $classDescription = "This page will first call an initialization method, then th
 $classUsage = "initialize the page and \$db reference testlinkInitPage(\$db) testlinkInitPage(\$db) Instantiate the testplan object using the \$db reference \$tp = new testplan(\$db) \$tp = new testplan(\$db) Many of the values used by the methods can be retrieve from \$_SESSION ";
 
 $mapOfRows = array();
-
 /** ******************************************** */
-
 $signature = "testcase(&\$db)";
 $functionDesc = "testcase class constructor";
 $testParams ="no test" ;
 $returnValue = "no test";
 $testResult = "N/A";
-
 array_push($mapOfRows, array($signature,$functionDesc,$testParams,$returnValue, $testResult));
-
 /** ******************************************** */
-
 $signature = "create(\$parent_id, \$name, \$summary, \$steps, $\expected_results, \$author_id, \$keywords_id='',\$tc_order=null)";
 $functionDesc = "creates a test case";
 $testParams ="in progress" ;
 $returnValue = "in progress";
 $testResult = "in progress";
-
 array_push($mapOfRows, array($signature,$functionDesc,$testParams,$returnValue, $testResult));
-
 /** ******************************************** */
-
 $signature = "create_tcase_only(\$parent_id, \$name)";
 $functionDesc = "in progress";
 $testParams ="in progress" ;
 $returnValue = "in progress";
 $testResult = "in progress";
-
 array_push($mapOfRows, array($signature,$functionDesc,$testParams,$returnValue, $testResult));
-
 /** ******************************************** */
 $signature = "create_tcversion(\$id, \$version, \$summary, \$steps, \$expected_results, \$author_id)";
 $functionDesc = "in progress";
 $testParams ="in progress" ;
 $returnValue = "in progress";
 $testResult = "in progress";
-
 array_push($mapOfRows, array($signature,$functionDesc,$testParams,$returnValue, $testResult));
-
 /** ******************************************** */
 $signature = "get_by_name(\$name)";
 $functionDesc = "in progress";
 $testParams ="in progress" ;
 $returnValue = "in progress";
 $testResult = "in progress";
-
 array_push($mapOfRows, array($signature,$functionDesc,$testParams,$returnValue, $testResult));
-
 /** ******************************************** */
 $signature = "get_all()";
 $functionDesc = "in progress";
 $testParams ="in progress" ;
 $returnValue = "in progress";
 $testResult = "in progress";
-
 array_push($mapOfRows, array($signature,$functionDesc,$testParams,$returnValue, $testResult));
-
 /** ******************************************** */
 $signature = "show(&\$smarty, \$id, \$user_id, \$version_id=TC_ALL_VERSIONS, \$action='', \$msg_result='', \$refresh_tree='yes')";
 $functionDesc = "in progress";
 $testParams ="in progress" ;
 $returnValue = "in progress";
 $testResult = "in progress";
-
 array_push($mapOfRows, array($signature,$functionDesc,$testParams,$returnValue, $testResult));
-
 /** ******************************************** */
-
-/**
-
-
-
-print "function  <BR>";
-print "function viewer_edit_new(\$amy_keys, \$oFCK, \$action, \$parent_id, \$id=null) <BR>";
-print "function update(\$id, \$tcversion_id, \$name, \$summary, \$steps, \$expected_results, \$user_id, \$keywords_id='', \$tc_order=null) <BR>";
-print "function check_link_and_exec_status(\$id) <BR>";
-
-print "function delete(\$id, \$version_id=TC_ALL_VERSIONS) <BR>";
-print "function get_linked_versions(\$id, \$status=ALL) <BR>";
-print "function _blind_delete(\$id, \$version_id=TC_ALL_VERSIONS) <BR>";
-print "function _execution_delete(\$id, \$version_id=TC_ALL_VERSIONS) <BR>";
-
-print "function get_testproject(\$id) <BR>";
-print "function copy_to(\$id, \$parent_id, \$user_id, \$copyKeywords = 0) <BR>";
-
-*/
+$signature = "viewer_edit_new(\$amy_keys, \$oFCK, \$action, \$parent_id, \$id=null)";
+$functionDesc = "in progress";
+$testParams ="in progress" ;
+$returnValue = "in progress";
+$testResult = "in progress";
+array_push($mapOfRows, array($signature,$functionDesc,$testParams,$returnValue, $testResult));
+/** ******************************************** */
+$signature = "update(\$id, \$tcversion_id, \$name, \$summary, \$steps, \$expected_results, \$user_id, \$keywords_id='', \$tc_order=null)";
+$functionDesc = "in progress";
+$testParams ="in progress" ;
+$returnValue = "in progress";
+$testResult = "in progress";
+array_push($mapOfRows, array($signature,$functionDesc,$testParams,$returnValue, $testResult));
+/** ******************************************** */
+$signature = "check_link_and_exec_status(\$id)";
+$functionDesc = "in progress";
+$testParams ="in progress" ;
+$returnValue = "in progress";
+$testResult = "in progress";
+array_push($mapOfRows, array($signature,$functionDesc,$testParams,$returnValue, $testResult));
+/** ******************************************** */
+$signature = "delete(\$id, \$version_id=TC_ALL_VERSIONS)";
+$functionDesc = "in progress";
+$testParams ="in progress" ;
+$returnValue = "in progress";
+$testResult = "in progress";
+array_push($mapOfRows, array($signature,$functionDesc,$testParams,$returnValue, $testResult));
+/** ******************************************** */
+$signature = "get_linked_versions(\$id, \$status=ALL)";
+$functionDesc = "in progress";
+$testParams ="in progress" ;
+$returnValue = "in progress";
+$testResult = "in progress";
+array_push($mapOfRows, array($signature,$functionDesc,$testParams,$returnValue, $testResult));
+/** ******************************************** */
+$signature = "_blind_delete(\$id, \$version_id=TC_ALL_VERSIONS)";
+$functionDesc = "in progress";
+$testParams ="in progress" ;
+$returnValue = "in progress";
+$testResult = "in progress";
+array_push($mapOfRows, array($signature,$functionDesc,$testParams,$returnValue, $testResult));
+/** ******************************************** */
+$signature = "_execution_delete(\$id, \$version_id=TC_ALL_VERSIONS)";
+$functionDesc = "in progress";
+$testParams ="in progress" ;
+$returnValue = "in progress";
+$testResult = "in progress";
+array_push($mapOfRows, array($signature,$functionDesc,$testParams,$returnValue, $testResult));
+/** ******************************************** */
+$signature = "get_testproject(\$id)";
+$functionDesc = "in progress";
+$testParams ="in progress" ;
+$returnValue = "in progress";
+$testResult = "in progress";
+array_push($mapOfRows, array($signature,$functionDesc,$testParams,$returnValue, $testResult));
+/** ******************************************** */
+$signature = "copy_to(\$id, \$parent_id, \$user_id, \$copyKeywords = 0)";
+$functionDesc = "in progress";
+$testParams ="in progress" ;
+$returnValue = "in progress";
+$testResult = "in progress";
+array_push($mapOfRows, array($signature,$functionDesc,$testParams,$returnValue, $testResult));
+/** ******************************************** */
 
 $smarty = new TLSmarty;
 $smarty->assign('testFile', 'testcase.class.test.php');

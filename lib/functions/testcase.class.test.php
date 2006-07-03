2d07c144331b77fb389fde5b76b006031c50e017
kevinlevy <>
2006-07-03 18:32:37 +0000

    uses smarty templates

--- lib/functions/testcase.class.test.php
+++ lib/functions/testcase.class.test.php
@@ -8,29 +8,106 @@ require_once('../../config.inc.php');
 require_once('common.php');
 require_once('testcase.class.php');
 
-print "<h3>/lib/functions/testcase.class.test.php</h3>";
-print "author : Kevin Levy <BR>";
-print "last updated 20060630 <BR>";
-print "<BR>This page displays the functions in /lib/functions/testcase.class.php and examples of their usage.  This page will first call an initialization method, then the testplan class will be instantiated, then we will retrieve the current testplan and testproject ids.  Once this initial information has been gathered, each method of the testplan class will be used and we will inspect the results.<BR>";
+$classDescription = "This page will first call an initialization method, then the testplan class will be instantiated, then we will retrieve the current testplan and testproject ids.  Once this initial information has been gathered, each method of the testplan class will be used and we will inspect the results.";
 
-print "============================================== <BR> ";
+$classUsage = "initialize the page and \$db reference testlinkInitPage(\$db) testlinkInitPage(\$db) Instantiate the testplan object using the \$db reference \$tp = new testplan(\$db) \$tp = new testplan(\$db) Many of the values used by the methods can be retrieve from \$_SESSION ";
 
-print "<h3>MUST BE DONE 1st : initialize the page and \$db reference</h3>";
-print "testlinkInitPage(\$db) <BR>";
-testlinkInitPage($db);
+$mapOfRows = array();
 
-print "============================================== <BR> ";
-print "<h3>Instantiate the testplan object using the \$db reference</h3>";
-print "\$tp = new testplan(\$db)<BR>";
-$tp = new testplan($db);
+/** ******************************************** */
 
-print "============================================== <BR> ";
+$signature = "testcase(&\$db)";
+$functionDesc = "testcase class constructor";
+$testParams ="no test" ;
+$returnValue = "no test";
+$testResult = "N/A";
 
-print "<h3>Many of the values used by the methods can be retrieve from \$_SESSION</h3>";
-print "contents of the \$_SESSION object : <BR>";
-print_r($_SESSION);
-print "<BR>";
+array_push($mapOfRows, array($signature,$functionDesc,$testParams,$returnValue, $testResult));
 
-print "============================================== <BR>";
+/** ******************************************** */
+
+$signature = "create(\$parent_id, \$name, \$summary, \$steps, $\expected_results, \$author_id, \$keywords_id='',\$tc_order=null)";
+$functionDesc = "creates a test case";
+$testParams ="in progress" ;
+$returnValue = "in progress";
+$testResult = "in progress";
+
+array_push($mapOfRows, array($signature,$functionDesc,$testParams,$returnValue, $testResult));
+
+/** ******************************************** */
+
+$signature = "create_tcase_only(\$parent_id, \$name)";
+$functionDesc = "in progress";
+$testParams ="in progress" ;
+$returnValue = "in progress";
+$testResult = "in progress";
+
+array_push($mapOfRows, array($signature,$functionDesc,$testParams,$returnValue, $testResult));
+
+/** ******************************************** */
+$signature = "create_tcversion(\$id, \$version, \$summary, \$steps, \$expected_results, \$author_id)";
+$functionDesc = "in progress";
+$testParams ="in progress" ;
+$returnValue = "in progress";
+$testResult = "in progress";
+
+array_push($mapOfRows, array($signature,$functionDesc,$testParams,$returnValue, $testResult));
+
+/** ******************************************** */
+$signature = "get_by_name(\$name)";
+$functionDesc = "in progress";
+$testParams ="in progress" ;
+$returnValue = "in progress";
+$testResult = "in progress";
+
+array_push($mapOfRows, array($signature,$functionDesc,$testParams,$returnValue, $testResult));
+
+/** ******************************************** */
+$signature = "get_all()";
+$functionDesc = "in progress";
+$testParams ="in progress" ;
+$returnValue = "in progress";
+$testResult = "in progress";
+
+array_push($mapOfRows, array($signature,$functionDesc,$testParams,$returnValue, $testResult));
+
+/** ******************************************** */
+$signature = "show(&\$smarty, \$id, \$user_id, \$version_id=TC_ALL_VERSIONS, \$action='', \$msg_result='', \$refresh_tree='yes')";
+$functionDesc = "in progress";
+$testParams ="in progress" ;
+$returnValue = "in progress";
+$testResult = "in progress";
+
+array_push($mapOfRows, array($signature,$functionDesc,$testParams,$returnValue, $testResult));
+
+/** ******************************************** */
+
+/**
+
+
+
+print "function  <BR>";
+print "function viewer_edit_new(\$amy_keys, \$oFCK, \$action, \$parent_id, \$id=null) <BR>";
+print "function update(\$id, \$tcversion_id, \$name, \$summary, \$steps, \$expected_results, \$user_id, \$keywords_id='', \$tc_order=null) <BR>";
+print "function check_link_and_exec_status(\$id) <BR>";
+
+print "function delete(\$id, \$version_id=TC_ALL_VERSIONS) <BR>";
+print "function get_linked_versions(\$id, \$status=ALL) <BR>";
+print "function _blind_delete(\$id, \$version_id=TC_ALL_VERSIONS) <BR>";
+print "function _execution_delete(\$id, \$version_id=TC_ALL_VERSIONS) <BR>";
+
+print "function get_testproject(\$id) <BR>";
+print "function copy_to(\$id, \$parent_id, \$user_id, \$copyKeywords = 0) <BR>";
+
+*/
+
+$smarty = new TLSmarty;
+$smarty->assign('testFile', 'testcase.class.test.php');
+$smarty->assign('classFile', 'testcase.class.php');
+$smarty->assign('classDescription', $classDescription);
+$smarty->assign('classUsage', $classUsage);
+
+$smarty->assign('mapOfRows', $mapOfRows);
+$smarty->display('classTester.tpl');
 
 ?>

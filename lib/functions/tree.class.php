513f4ddc5e3d9969fa83e3f3284515fe391be786
franciscom <>
2006-07-31 16:44:33 +0000

    *** empty log message ***

--- lib/functions/tree.class.php
+++ lib/functions/tree.class.php
@@ -5,10 +5,11 @@
  *
  * Filename $RCSfile: tree.class.php,v $
  *
- * @version $Revision: 1.20 $
- * @modified $Date: 2006/07/28 17:22:04 $ by $Author: schlundus $
+ * @version $Revision: 1.21 $
+ * @modified $Date: 2006/07/31 16:44:33 $ by $Author: franciscom $
  * @author Francisco Mancardi
  *
+ * 20060729 - franciscom - fixed bug on new_node() after refactoring in version 1.20
  * 20060722 - franciscom - added possibility to create a new node with an specific ID
  * 20060511 - franciscom - changes in call to insert_id() due to problems with Postgres
  * 20060316 - franciscom - bug on get_path
@@ -72,11 +73,10 @@ class tree
 		
 		$values=" VALUES('" . $this->db->prepare_string($name). "'," .
 		        " {$node_type_id},{$node_order}";
-		
 		if ($node_id)
 		{
 			$sql .= ",id";
-			$values = ",{$node_id}";
+			$values .= ",{$node_id}";
 		}
 		
 		if(is_null($parent_id))
@@ -86,11 +86,11 @@ class tree
 		else
 		{
 			$sql .= ",parent_id) {$values},{$parent_id})";
-	    }
+    }
 
 		$this->db->exec_query($sql);
 		return ($this->db->insert_id($this->obj_table));
-  	}
+ 	}
 
 	/*
 	get all node hierarchy info from hierarchy table

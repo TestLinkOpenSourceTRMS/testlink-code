da84424f8dfc3d660dd84988416dce4b094ea836
franciscom <>
2006-09-15 13:16:25 +0000

    user assignment feature

---
+++ lib/functions/assignment_mgr.class.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * TestLink Open Source Project - http://testlink.sourceforge.net/ 
+ * This script is distributed under the GNU General Public License 2 or later. 
+ *
+ * Filename $RCSfile: assignment_mgr.class.php,v $
+ *
+ * @version $Revision: 1.1 $
+ * @modified $Date: 2006/09/15 13:14:48 $ by $Author: franciscom $
+ * @author Francisco Mancardi
+ *
+ * Manager for assignment activities
+ *
+ * 20060908 - franciscom - 
+*/
+
+class assignment_mgr 
+{
+	
+	var $db;
+
+	function assignment_mgr(&$db) 
+	{
+		$this->db = &$db;
+	}
+
+  /*
+   $key_field: contains the filename that has to be used as the key of
+               the returned hash.    
+  */
+	function get_available_types($key_field='description') 
+	{
+		$sql = " SELECT * FROM assignment_types "; 
+		$hash_types = $this->db->fetchRowsIntoMap($sql,$key_field);
+		
+		return $hash_types;
+	}
+
+  /*
+   $key_field: contains the filename that has to be used as the key of
+               the returned hash.    
+  */
+	function get_available_status($key_field='description') 
+	{
+		$sql = " SELECT * FROM assignment_status "; 
+		$hash_types = $this->db->fetchRowsIntoMap($sql,$key_field);
+		
+		return $hash_types;
+	}
+
+  // $id can be an scalar or an array
+	function delete_by_feature_id($feature_id) 
+	{
+    if( is_array($feature_id) )
+    {
+      $feature_id_list=implode(",",$feature_id);
+      $where_clause=" WHERE feature_id IN ($feature_id_list) ";
+    }
+    else
+    {
+      $where_clause=" WHERE feature_id={$feature_id}";
+    }
+		$sql = " DELETE FROM user_assignments {$where_clause}"; 
+		
+		  echo "<br>debug - <b><i>" . __FUNCTION__ . "</i></b><br><b>" . $sql . "</b><br>";
+
+		$result=$this->db->exec_query($sql);
+	}
+
+  // 20060913 - franciscom
+  // 
+  // $feature_map['feature_id']['user_id']
+  // $feature_map['feature_id']['type']
+  // $feature_map['feature_id']['status']
+  // $feature_map['feature_id']['assigner_id']
+  //
+  //
+	function assign($feature_map) 
+	{
+	  echo "<pre>debug 20060914 \$feature_map" . __FUNCTION__ . " --- "; print_r($feature_map); echo "</pre>";
+	  
+	  foreach($feature_map as $feature_id => $elem)
+	  {
+	    $sql="INSERT INTO user_assignments " .
+	         "(feature_id,user_id,assigner_id," .
+	         "type,status,creation_ts";
+	        
+	    $values="VALUES({$feature_id},{$elem['user_id']},{$elem['assigner_id']}," .
+	            "{$elem['type']},{$elem['status']}," . $elem['creation_ts'];
+	         
+	    if(isset($elem['deadline_ts']) )
+	    {
+	       $sql .=",deadline_ts";
+	       $values .="," . $elem['deadline_ts']; 
+	    }     
+	    
+	    $sql .= ") " . $values . ")";
+      $this->db->exec_query($sql);
+	  } // foreach 
+	}
+	
+
+	// 20060913 - franciscom
+  // 
+  // $feature_map: key   => feature_id
+  //               value => hash with optional keys 
+  //                        that have the same name of user_assignment fields
+  //
+  //
+	function update($feature_map) 
+	{
+	  
+	  foreach($feature_map as $feature_id => $elem)
+	  {
+	    
+	    $sepa="";
+	    $sql="UPDATE user_assignments SET ";
+	    $simple_fields=array('user_id','assigner_id','type','status');
+	    $date_fields=array('deadline_ts','creation_ts');  
+	      
+	    foreach($simple_fields as $idx => $field)
+	    {
+	      if( isset($elem[$field]) )
+	      {
+	       $sql.=$sepa . "$field={$elem[$field]} ";
+	       $sepa=",";
+	      }
+	    }
+	    
+	    foreach($date_fields as $idx => $field)
+	    {
+	      if( isset($elem[$field]) )
+	      {
+	       $sql.=$sepa . "$field=" . $elem[$field] . " ";
+	       $sepa=",";
+	      }
+	    }
+	    
+	    $sql.="WHERE feature_id={$feature_id}";
+        echo "<br>debug - <b><i>" . __FUNCTION__ . "</i></b><br><b>" . $sql . "</b><br>";
+
+	    $this->db->exec_query($sql);
+	  }
+	}
+}// end class
+?>
---
+++ lib/plan/tc_exec_assignment.php
@@ -0,0 +1,171 @@
+<?php
+/** 
+ * TestLink Open Source Project - http://testlink.sourceforge.net/ 
+ * @version $Id: tc_exec_assignment.php,v 1.1 2006/09/15 13:16:25 franciscom Exp $ 
+ * 
+ * 
+ * 
+ */         
+require('../../config.inc.php');
+require_once("../functions/common.php");
+require_once("../functions/assignment_mgr.class.php");
+require_once("plan.inc.php");
+
+testlinkInitPage($db);
+
+$tree_mgr = new tree($db); 
+$tsuite_mgr = new testsuite($db); 
+$tplan_mgr = new testplan($db); 
+$tcase_mgr = new testcase($db); 
+$assignment_mgr = new assignment_mgr($db); 
+
+$tplan_id = $_SESSION['testPlanId'];
+$tplan_name = $_SESSION['testPlanName'];
+
+$id = isset($_REQUEST['id']) ? $_REQUEST['id'] : null;
+$version_id = isset($_REQUEST['version_id']) ? $_REQUEST['version_id'] : 0;
+$level = isset($_REQUEST['level']) ? $_REQUEST['level'] : null;
+$keyword_id = isset($_REQUEST['keyword_id']) ? $_REQUEST['keyword_id'] : 0;
+$do_action = isset($_POST['assign_tc']) ? 1 : 0;
+
+$resultString = null;
+$arrData = array();
+
+echo "<pre>debug 20060912 \$_POST" . __FUNCTION__ . " --- "; print_r($_POST); echo "</pre>";
+
+// ---------------------------------------------------------------------------------------
+if($do_action)
+{
+  echo "<pre>debug 20060912 \$_POST" . __FUNCTION__ . " --- "; print_r($_POST); echo "</pre>";
+  $a_tc = isset($_POST['achecked_tc']) ? $_POST['achecked_tc'] : null;
+  if(!is_null($a_tc))
+  {
+      $types_map=$assignment_mgr->get_available_types();
+      $status_map=$assignment_mgr->get_available_status();
+      
+      $task_test_execution=$types_map['testcase_execution']['id'];
+      $open=$status_map['open']['id'];
+      $db_now=$db->db_now();
+      
+      
+      $features2upd=array();
+      $features2ins=array();
+      $features2del=array();
+      
+      //echo "<pre>debug 20060914 \$_POST" . __FUNCTION__ . " --- "; print_r($_POST); echo "</pre>";
+      
+      foreach($a_tc as $key_tc => $value_tcversion)
+      {
+          
+        $feature_id=$_POST['feature_id'][$key_tc];
+        
+        if($_POST['has_prev_assignment'][$key_tc] > 0)
+        {
+           if( $_POST['tester_for_tcid'][$key_tc] > 0 )
+           {
+              $features2upd[$feature_id]['user_id']=$_POST['tester_for_tcid'][$key_tc];
+              $features2upd[$feature_id]['assigner_id']=$_SESSION['userID'];
+              $features2upd[$feature_id]['type']=$task_test_execution;
+              $features2upd[$feature_id]['status']=$open;
+           } 
+           else
+           {
+              $features2del[$feature_id]=$feature_id;
+           }
+        }
+        else if($_POST['tester_for_tcid'][$key_tc] > 0)
+        {
+           $features2ins[$feature_id]['user_id']=$_POST['tester_for_tcid'][$key_tc];
+           $features2ins[$feature_id]['type']=$task_test_execution;
+           $features2ins[$feature_id]['status']=$open;
+           $features2ins[$feature_id]['creation_ts']=$db_now;
+           $features2ins[$feature_id]['assigner_id']=$_SESSION['userID'];
+        }
+      }
+      echo "<pre>debug 20060913 \$features2upd" . __FUNCTION__ . " --- "; print_r($features2upd); echo "</pre>";
+      echo "<pre>debug 20060913 \$features2del" . __FUNCTION__ . " --- "; print_r($features2del); echo "</pre>";
+      echo "<pre>debug 20060913 \$features2ins" . __FUNCTION__ . " --- "; print_r($features2ins); echo "</pre>";
+      
+      
+      if( count($features2upd) > 0 )
+      {
+         $assignment_mgr->update($features2upd);      
+      }
+      if( count($features2del) > 0 )
+      {
+         $assignment_mgr->delete_by_feature_id($features2del);      
+      }
+      if( count($features2ins) > 0 )
+      {
+         $assignment_mgr->assign($features2ins);      
+      }
+      
+      
+      
+      
+      //      $assignment_mgr->update($feature_map,'testcase_execution');      
+  }  
+}
+
+define('FILTER_BY_TC_OFF',null); 
+define('WRITE_BUTTON_ONLY_IF_LINKED',1);
+
+define('ALL_USERS_FILTER',null); 
+define('ADD_BLANK_OPTION',true); 
+$users=get_users_for_html_options($db,ALL_USERS_FILTER,ADD_BLANK_OPTION);
+
+switch($level)
+{
+	case 'testcase':
+		$out = null;
+		
+		if(!$do_action) 
+		{
+			// build the data need to call gen_spec_view
+			$my_path = $tree_mgr->get_path($id);
+			$idx_ts = count($my_path)-1;
+			$tsuite_data= $my_path[$idx_ts-1];
+			
+			
+			$pp = $tcase_mgr->get_versions_status_quo($id, $version_id);
+			$linked_items[$id] = $pp[$version_id];
+			$linked_items[$id]['testsuite_id'] = $tsuite_data['id'];
+			$linked_items[$id]['tc_id'] = $id;
+
+      $p3 = $tcase_mgr->get_version_exec_assignment($version_id,$tplan_id);
+			$linked_items[$id]['user_id'] = $p3[$version_id]['user_id'];
+      $linked_items[$id]['feature_id'] = $p3[$version_id]['feature_id'];
+
+			//echo "<pre>debug 20060908 \$pp" . __FUNCTION__ . " --- "; print_r($pp); echo "</pre>";
+			//echo "<pre>debug 20060908 \$linked_items" . __FUNCTION__ . " --- "; print_r($linked_items); echo "</pre>";
+
+			
+			$out = gen_spec_view($db,'testplan',$tplan_id,$tsuite_data['id'],$tsuite_data['name'],
+			                     $linked_items,$keyword_id,FILTER_BY_TC_OFF,WRITE_BUTTON_ONLY_IF_LINKED);
+			                     
+			echo "<pre>debug 20060908 \$out" . __FUNCTION__ . " --- "; print_r($out); echo "</pre>";                     
+		}
+		break;
+
+	case 'testsuite':
+		$tsuite_data = $tsuite_mgr->get_by_id($id);
+		$out = gen_spec_view($db,'testplan',
+                         $tplan_id,$id,$tsuite_data['name'],
+                         $tplan_mgr->get_linked_tcversions($tplan_id,FILTER_BY_TC_OFF,$keyword_id),
+                         $keyword_id,FILTER_BY_TC_OFF,WRITE_BUTTON_ONLY_IF_LINKED);
+
+			echo "<pre>debug 20060908 \$out" . __FUNCTION__ . " --- "; print_r($out); echo "</pre>";                     
+		break;
+	default:
+		// show instructions
+		redirect( $_SESSION['basehref'] . $g_rpath['instructions'].'/tc_exec_assignment.html');
+	break;
+}
+
+$smarty = new TLSmarty();
+$smarty->assign('users', $users);
+$smarty->assign('has_tc', ($out['num_tc'] > 0 ? 1:0));
+$smarty->assign('arrData', $out['spec_view']);
+$smarty->assign('testPlanName', $tplan_name);
+$smarty->display('tc_exec_assignment.tpl');
+?>

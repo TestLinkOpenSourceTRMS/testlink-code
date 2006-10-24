35465878a3248767c40af7d48c35d183f37e134c
kevinlevy <>
2006-10-24 16:42:10 +0000

    major changes to the way suiteStructure is built - use method simliar to treeMenu.inc's generateExecTree()

--- lib/functions/results.class.php
+++ lib/functions/results.class.php
@@ -6,7 +6,7 @@
  * Filename $RCSfile: results.class.php,v $
  *
  * @version $Revision: 1.8 
- * @modified $Date: 2006/10/12 19:50:03 $ by $Author: schlundus $
+ * @modified $Date: 2006/10/24 16:42:10 $ by $Author: kevinlevy $
  *
  *
  * This class is encapsulates most functionality necessary to query the database
@@ -18,15 +18,19 @@
  * 20060829 - kevinlevy - development in progress
 **/
 
+require_once('treeMenu.inc.php');
 
 class results
 {
+
   var $suitesSelected = "";	
 
   // class references passed in by constructor
   var $db = null;
   var $tp = null;
-  var $tree = null;
+//  var $tree = null;
+  var $testPlanID = -1;
+  var $prodID = -1;
 
   // construct map linking suite ids to execution rows 
   var $SUITE_TYPE_ID = 2;  
@@ -79,22 +83,57 @@ class results
   var $totalsForPlan = null;
 	
 	//TODO: shallow initialization!  
-  function results(&$db, &$tp, &$tree, $suitesSelected, $builds_to_query = -1)
+  function results(&$db,&$tp,$suitesSelected,$builds_to_query = -1,$prodID,$testPlanID)
   {
-   	$this->db = $db;	
-    $this->tp = $tp;    
-    $this->tree = $tree;
-  	$this->suitesSelected = $suitesSelected;  	
-  	
-  	// retrieve results from executions table
-    $this->executionsMap = $this->buildExecutionsMap($builds_to_query);    
 
+    //print "builds_to_query = $builds_to_query <BR>";
+    $this->db = $db;	
+    $this->tp = $tp;    
+  //  $this->tree = $tree;
+    $this->suitesSelected = $suitesSelected;  	
+    $this->prodID = $prodID;
+    $this->testPlanID = $testPlanID;
+	
+    // retrieve results from executions table
+    $this->executionsMap = $this->buildExecutionsMap($builds_to_query);    
+/**
     // build data objects which can be used to describe the tree structure of the test project 
     $this->suiteStructure = $this->buildSuiteStructure($this->suitesSelected);    
+    print "suiteStructure from buildSuiteStructure <BR>";
+    print_r($this->suiteStructure);
+    print "<BR><BR>";
+    print "flatarray from buildSuiteStructure <BR>";
+    print_r($this->flatArray);
+    print "<BR><BR>";
     
+    // KL - 20061022 - call new method I am working on 
+   // ONLY FOR COMPARING
+	
+  $this->suiteStructure = null;
+  $this->flatArray = null;
+  // items assoicated with flatArray	
+   $this->flatArrayIndex = 0;	
+   $this->depth = 0;	
+    $this->previousDepth = 0;
+   // END ONLY FOR COMPARING
+*/
+    $this->suiteStructure = $this->generateExecTree();
+    $this->suiteStructure = $this->suiteStructure[0];
+	print_r($this->suiteStructure);
+/**		
+    print "suiteStructure from generateExecTree <BR>";
+    print_r($this->suiteStructure);
+    print "<BR><BR>";
+    print "flatarray from generateExecTree <BR>";
+    print_r($this->flatArray);
+    print "<BR>";
+
+*/
+
+
     // create data object which tallies last result for each test case
     $this->createMapOfLastResult($this->suiteStructure, $this->executionsMap);
-    
+	    
     // create data object which tallies totals for individual suites
     // child suites are NOT taken into account in this step
     $this->createMapOfSuiteSummary($this->mapOfLastResult);
@@ -104,6 +143,8 @@ class results
     $this->createAggregateMap($this->suiteStructure, $this->mapOfSuiteSummary);
 	
     $this->totalsForPlan = $this->createTotalsForPlan($this->suiteStructure, $this->mapOfSuiteSummary);
+
+ 	
   }
    
   function getSuiteList(){
@@ -151,7 +192,9 @@ class results
  *
  * 
  *
- */	
+ */
+// KL - 20061023 - no longer using this method in favor of generateExecTree	
+/**
   function buildSuiteStructure($suiteId)
   {
   	// do not null this out 
@@ -213,7 +256,7 @@ class results
 	$this->depth--;
   	return $currentNode;
   }
-	
+*/	
 	/**
 	 * mapOfLastResult -> arrayOfSuiteIds
 	 * 
@@ -458,8 +501,14 @@ class results
     // otherwise duplicate executions will be added to suites
     $executionsMap = null;  
     $linked_tcversions = $this->tp->get_linked_tcversions($_SESSION['testPlanId']);
-    
+    /**
+    print "\$linked_tcversions = <BR>";
+	print_r($linked_tcversions);
+	print "<BR>";
+	print "<BR>";
+    */
     while ($testcaseID = key($linked_tcversions)){
+	//print "\$testcaseID = $testcaseID <BR>";
       $info = $linked_tcversions[$testcaseID];
             
       $testsuite_id = $info['testsuite_id'];
@@ -478,9 +527,14 @@ class results
       //$notSure3 = $info[3];
       $executed = $info['executed'];
       $executionExists = true;
-
+	/**
+	print "\$tcversion_id = $tcversion_id <BR>";
+	print "\$executed = $executed <BR>";
+	print "<BR><BR>";	
+	*/
       if ($tcversion_id != $executed){
-			// this test case not been executed in this test plan
+			//print "test case \$tcversion_id $tcversion_id not executed <BR>";
+       		// this test case not been executed in this test plan
 			$executionExists = false;
 			// print "test case version id " . $tcversion_id . " not executed <BR>";
 			// print "executed = $executed <BR>";
@@ -521,9 +575,16 @@ class results
 			    //print_r($infoToSave);
 		  		array_push($currentSuite, $infoToSave);
 		  		next($execQuery);
-			}		
+			} // end while		
+		} // end if($execQuery)
+		// HANDLE scenario where execution does not exist		
+		else {
+			$infoToSave = array(testcaseID => $testcaseID, tcversion_id => $tcversion_id, 
+			build_id => '', tester_id => '', execution_ts => '', status => 'n', notes => '');
+			array_push($currentSuite, $infoToSave);			
+
 		}
-      }
+      } // end if($executionExists)
       $executionsMap[$testsuite_id] = $currentSuite;
       next($linked_tcversions);
     } 
@@ -552,6 +613,146 @@ class results
   	} // end for loop
   	return $returnList;
   } // end function getTopLevelSuites
+
+function generateExecTree($keyword_id = 0,$bForPrinting = false,$tc_id = 0)
+{
+	// $menustring = null;
+	// print "generateExecTree() <BR>";
+	
+	$tplan_mgr = $this->tp;
+	$tproject_mgr = new testproject($this->db);
+	
+	$tree_manager = $tplan_mgr->tree_manager;
+	$tcase_node_type = $tree_manager->node_descr_id['testcase'];
+	$hash_descr_id = $tree_manager->get_available_node_types();
+	$hash_id_descr = array_flip($hash_descr_id);
+	$test_spec = $tree_manager->get_subtree($this->prodID,array('testplan'=>'exclude me'),
+	                                                     array('testcase'=>'exclude my children'),null,null,true);
+
+	$tp_tcs = $tplan_mgr->get_linked_tcversions($this->testPlanID,$tc_id,$keyword_id);
+	if (is_null($tp_tcs)) { 
+		$tp_tcs = array();
+	}
+	// KL - TO-DO pass in test plan name
+	$test_spec['name'] = "test_spec_name";
+	$test_spec['id'] = $this->prodID;
+	$test_spec['node_type_id'] = $hash_descr_id['testproject'];
+	// KL - not sure what to name this var
+	$suiteStructure = null;
+
+	if($test_spec)
+	{
+		$tck_map = null;
+		if($keyword_id) {
+			$tck_map = $tproject_mgr->get_keywords_tcases($this->prodid,$keyword_id);
+		}
+		// KL - comment back in when we add prepareNode() to this class
+		$testcase_count = prepareNode($test_spec,$hash_id_descr,$tck_map,$tp_tcs,$bForPrinting);
+		//print "\$testcase_count = $testcase_count <BR>";
+		$test_spec['testcase_count'] = $testcase_count;
+		$getArguments = "getArguments";
+		$menuUrl = "menuUrl";
+		$currentNode = null;
+		$currentNodeIndex = 0;
+		$suiteStructure = $this->processExecTreeNode(1,$test_spec,$getArguments,$hash_id_descr,1,$menuUrl,$bForPrinting);
+	}
+	return $suiteStructure;	
+} // end generateExecTree
+
+
+function processExecTreeNode($level,&$node,$getArguments,$hash_id_descr,$tc_action_enabled,$linkto,$bForPrinting)
+{
+	$currentNode = null;
+	$currentNodeIndex = 0;
+
+	$nodeDesc = $hash_id_descr[$node['node_type_id']];
+	$nodeInfo = $this->getSuiteInfoIfItExists($node,$nodeDesc,$tc_action_enabled,$bForPrinting, $currentNode, $currentNodeIndex);
+
+	//$nodeIsTS = 0;
+	if ($nodeInfo[0] == TS){
+		/** flat array logic */
+		//$changeInDepth = $this->depth - $this->previousDepth;
+		//$this->previousDepth = $this->depth;
+		// depth only used by flatArrayIndex to help describe the tree
+	
+		//$this->flatArray[$this->flatArrayIndex] = $changeInDepth;
+		/**
+		$CONSTANT_DEPTH_ADJUSTMENT = 2;
+		$this->flatArray[$this->flatArrayIndex] = $level - $CONSTANT_DEPTH_ADJUSTMENT  ;
+		$this->flatArrayIndex++;
+		$this->flatArray[$this->flatArrayIndex] = $nodeInfo[2];
+		$this->flatArrayIndex++;		
+		$this->flatArray[$this->flatArrayIndex] = $nodeInfo[1];
+		$this->flatArrayIndex++;
+		*/
+		/** end flat array logic */
+
+		/** suite structure logic */
+		$currentNode[$currentNodeIndex] = $nodeInfo[2];
+		$currentNodeIndex++;
+		$currentNode[$currentNodeIndex] = $nodeInfo[1];
+		$currentNodeIndex++;
+		/** end suite structure logic */
+	}
+	if (isset($node['childNodes']) && $node['childNodes'] )
+	{
+		print "$nodeInfo[2] has child nodes <BR>";
+		$childNodes = $node['childNodes'];
+		for($i = 0;$i < sizeof($childNodes);$i++)
+		{
+			$current = $childNodes[$i];
+			if(is_null($current)) {
+				continue;
+			}
+			$currentNode[$currentNodeIndex] = $this->processExecTreeNode($level+1,$current,$getArguments,$hash_id_descr,$tc_action_enabled,$linkto,$bForPrinting);
+			$currentNodeIndex++;
+		}
+	}
+	return $currentNode;
+}
+
+// KL - 20061021
+function getSuiteInfoIfItExists($current,$nodeDesc,$tc_action_enabled,$bForPrinting, &$currentNode, $currentNodeIndex)
+{
+	//$menustring = "['";
+	$name = filterString($current['name']);
+	//$buildLinkTo = 1;
+	//$pfn = "testCase";
+	//$testcase_count = isset($current['testcase_count']) ? $current['testcase_count'] : 0;	
+	//$versionID = 0;
+	
+	$returnValue = array();
+	$returnType = null;	
+	if($nodeDesc == 'testproject')
+	{
+		$returnType = "TP";
+	}
+	else if ($nodeDesc == 'testsuite')
+	{
+		$returnType = "TS";
+	}
+	else if ($nodeDesc == 'testcase')
+	{
+		$returnType = "TC";
+	}
+		
+	$returnValue[0] = $returnType;
+	$returnValue[1] = $current['id'];
+	$returnValue[2] = $name;	
+
+	return $returnValue;
+}
+
+/**
+function jtree_renderTestSpecTreeNodeOnClose2($current,$nodeDesc)
+{
+	//$menustring =  "],";
+	//return $menustring;
+	return null;
+}
+*/
+
+
 } // end class result
 
 

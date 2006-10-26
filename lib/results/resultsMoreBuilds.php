af0f10df8a7a80af4c111d12a74d3954e4745873
kevinlevy <>
2006-10-26 20:01:15 +0000

    added funciontality to display all users in select owner select list - temporary fix - eventually we just want to display users that are owners of cases in the test plan

--- lib/results/resultsMoreBuilds.php
+++ lib/results/resultsMoreBuilds.php
@@ -1,7 +1,7 @@
 <?php
 /** 
 * TestLink Open Source Project - http://testlink.sourceforge.net/ 
-* $Id: resultsMoreBuilds.php,v 1.40 2006/10/24 21:51:00 kevinlevy Exp $ 
+* $Id: resultsMoreBuilds.php,v 1.41 2006/10/26 20:01:15 kevinlevy Exp $ 
 *
 * @author	Kevin Levy <kevinlevy@users.sourceforge.net>
 * 
@@ -14,11 +14,18 @@ require('../../config.inc.php');
 require_once('common.php');
 require_once('builds.inc.php');
 require_once('../functions/results.class.php');
+// used to retrieve users 
+require_once('../functions/users.inc.php');
 testlinkInitPage($db);
 
 $prodID = isset($_SESSION['testprojectID']) ? $_SESSION['testprojectID'] : 0;
 $tpID = isset($_SESSION['testPlanId']) ? $_SESSION['testPlanId'] : 0 ;
 $tplanName = isset($_SESSION['testPlanName']) ? $_SESSION['testPlanName'] : null;
+
+define('ALL_USERS_FILTER', null);
+define('ADD_BLANK_OPTION', false);
+$arrOwners = get_users_for_html_options($db, ALL_USERS_FILTER, ADD_BLANK_OPTION);
+
 
 $tp = new testplan($db);
 //$tree = new tree($db);
@@ -63,5 +70,6 @@ $smarty->assign('testPlanName',$tplanName);
 $smarty->assign('arrBuilds', $arrBuilds); 
 $smarty->assign('arrKeywords', $arrKeywords);
 $smarty->assign('arrComponents', $arrComponents);
+$smarty->assign('arrOwners', $arrOwners);
 $smarty->display('resultsMoreBuilds_query_form.tpl');
 ?>

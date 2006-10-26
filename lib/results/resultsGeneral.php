6af683788b034e7c25ec5b365fd11fc3b8c288a4
kevinlevy <>
2006-10-26 06:37:37 +0000

    moving to 1.7

--- lib/results/resultsGeneral.php
+++ lib/results/resultsGeneral.php
@@ -3,8 +3,8 @@
  * TestLink Open Source Project - http://testlink.sourceforge.net/ 
  * 
  * @filesource $RCSfile: resultsGeneral.php,v $
- * @version $Revision: 1.6 $
- * @modified $Date: 2006/07/15 19:55:30 $ by $Author: schlundus $
+ * @version $Revision: 1.7 $
+ * @modified $Date: 2006/10/26 06:37:37 $ by $Author: kevinlevy $
  * @author	Martin Havlat <havlat@users.sourceforge.net>
  * 
  * This page show Test Results over all Builds.
@@ -16,16 +16,29 @@
  *
  * 
  */
+
+print "KL - 20061025 - work in progress <BR>";
 require('../../config.inc.php');
 require_once('common.php');
-require_once('results.inc.php');
+require_once('builds.inc.php');
+require_once('../functions/results.class.php');
 testlinkInitPage($db);
 
+
 $tpID = $_SESSION['testPlanId']; 
-$arrDataPriority = getPriorityReport($db,$tpID);
-$arrDataSuite = getTestSuiteReport($db,$tpID);
-$arrDataKeys = getKeywordsReport($db,$tpID);
-$arrDataOwner = getOwnerReport($db,$tpID);
+
+
+//$arrDataPriority = getPriorityReport($db,$tpID);
+$arrDataPriority = null;
+
+//$arrDataSuite = getTestSuiteReport($db,$tpID);
+$arrDataSuite = null;
+
+//$arrDataKeys = getKeywordsReport($db,$tpID);
+$arrDataKeys = null;
+
+//$arrDataOwner = getOwnerReport($db,$tpID);
+$arrDataOwner = null;
 
 $smarty = new TLSmarty;
 $smarty->assign('tpName', $_SESSION['testPlanName']);
@@ -34,4 +47,5 @@ $smarty->assign('arrDataSuite', $arrDataSuite);
 $smarty->assign('arrDataOwner', $arrDataOwner);
 $smarty->assign('arrDataKeys', $arrDataKeys);
 $smarty->display('resultsGeneral.tpl');
+
 ?>

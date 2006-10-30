8b4998d7a3fe75022c72f9d26a496328e686d92a
kevinlevy <>
2006-10-30 00:50:48 +0000

    using result.class.php to produce results instead of code Andreas wrote

--- lib/results/resultsByStatus.php
+++ lib/results/resultsByStatus.php
@@ -1,7 +1,7 @@
 <?php
 /** 
 * TestLink Open Source Project - http://testlink.sourceforge.net/ 
-* $Id: resultsByStatus.php,v 1.12 2006/10/13 20:06:15 schlundus Exp $ 
+* $Id: resultsByStatus.php,v 1.13 2006/10/30 00:50:48 kevinlevy Exp $ 
 *
 * @author	Martin Havlat <havlat@users.sourceforge.net>
 * @author 	Chad Rosen
@@ -13,13 +13,16 @@
 */
 require('../../config.inc.php');
 require_once('common.php');
-require_once('builds.inc.php');	
-require_once('results.inc.php');
+//require_once('builds.inc.php');	
+//require_once('results.inc.php');
 require_once('exec.inc.php');
 require_once("../../lib/functions/lang_api.php");
+require_once("../../lib/functions/results.class.php");
 testlinkInitPage($db);
-
+$tp = new testplan($db);
 $type = isset($_GET['type']) ? $_GET['type'] : 'n';
+//print "type = $type <BR>";
+
 if($type == $g_tc_status['failed'])
 	$title = lang_get('list_of_failed');
 else if($type == $g_tc_status['blocked'])
@@ -32,10 +35,40 @@ else
 
 $tpID = isset($_SESSION['testPlanId']) ?  $_SESSION['testPlanId'] : 0;
 $bCanExecute = has_rights($db,"tp_execute");
+$SUITES_SELECTED = "all";
+
+// TO-DO : KL define constants and verify localization is not necessary
+$builds = 'a';
+
+$tp = new testplan($db);
+$results = new results($db, $tp, $SUITES_SELECTED, $builds, $type);
+$mapOfLastResult = $results->getMapOfLastResult();
+//print "map of last results = <BR>";
+//print_r($mapOfLastResult);
+//print "<BR>";
+
+$arrDataIndex = 0;
+while ($suiteId = key($mapOfLastResult)){
+//	print "suiteId = $suiteId <BR>";
+	while($tcId = key($mapOfLastResult[$suiteId])){
+//		print "tcId = $tcId <BR>";
+		$lastBuildIdExecuted = $mapOfLastResult[$suiteId][$tcId]['buildIdLastExecuted'];
+//		print "lastBuildIdExecuted = $lastBuildIdExecuted <BR>";
+		$arrData[$arrDataIndex] = array("suiteId=". $suiteId,"tcID=" . $tcId,"buildId=" . $lastBuildIdExecuted,'run by','date','notes','bugs');
+		$arrDataIndex++;
+		next($mapOfLastResult[$suiteId]);
+	}
+	next($mapOfLastResult);
+}
+
+/**  ****************************************************************************
+KL - 20061029 - I will review this code and use some logic and thoughts that Andreas has added herer
+but for now I will use the same code I am using for the other reports to consolidate development
+$tp = new testplan($db);
+
 $arrData = array();
 $dummy = null;
 
-$tp = new testplan($db);
 $tcs = $tp->get_linked_tcversions($tpID,null,0,1);
 $maxBuildID = $tp->get_max_build_id($tpID);
 
@@ -73,6 +106,8 @@ if ($tcs && $maxBuildID)
 	}
 }
 
+
+
 function buildBugString(&$db,$execID)
 {
 	$bugString = null;
@@ -86,6 +121,8 @@ function buildBugString(&$db,$execID)
 	}
 	return $bugString;
 }
+
+*/
 
 $smarty = new TLSmarty;
 $smarty->assign('title', $title);

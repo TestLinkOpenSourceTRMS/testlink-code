b438198c0490196e4311a584c8175c325cfa3fca
kevinlevy <>
2006-11-26 05:57:24 +0000

    use only 2 decimal places in percent complete field

--- lib/results/resultsBuild.php
+++ lib/results/resultsBuild.php
@@ -1,7 +1,7 @@
 <?php
 /** 
 * TestLink Open Source Project - http://testlink.sourceforge.net/ 
-* $Id: resultsBuild.php,v 1.11 2006/11/06 20:22:30 schlundus Exp $ 
+* $Id: resultsBuild.php,v 1.12 2006/11/26 05:57:24 kevinlevy Exp $ 
 *
 * @author	Martin Havlat <havlat@users.sourceforge.net>
 * 
@@ -16,7 +16,6 @@ require_once('common.php');
 require_once('builds.inc.php');
 require_once('../functions/results.class.php');
 require_once('../functions/testplan.class.php');
-
 
 $builds_to_query = isset($_GET['build']) ? intval($_GET['build']) : null;
 if (!isset($_GET['build']))
@@ -49,134 +48,71 @@ $mapOfAggregate = $re->getAggregateMap();
 $arrDataSuite = null;
 $arrDataSuiteIndex = 0;
 while ($i = key($topLevelSuites)) {
-	//print_r($arrDataSuite);
-	//print "<BR>";
 	$pairArray = $topLevelSuites[$i];
 	$currentSuiteId = $pairArray['id'];
 	$currentSuiteName = $pairArray['name'];
 	$resultArray = $mapOfAggregate[$currentSuiteId];	
-
 	$total = $resultArray['total'];
-
 	$notRun = $resultArray['notRun'];
-
 	$percentCompleted = (($total - $notRun) / $total) * 100;
-
+	$percentCompleted = number_format($percentCompleted,2);
 	$arrDataSuite[$arrDataSuiteIndex] = array($currentSuiteName,$total,$resultArray['pass'],$resultArray['fail'],$resultArray['blocked'],$notRun,$percentCompleted);
-
 	$arrDataSuiteIndex++;
-
 	next($topLevelSuites);
-
 } 
 
-
-
 /**
-
 * PRIORITY REPORT
-
 */
-
 //$arrDataPriority = getPriorityReport($db,$tpID);
-
 $arrDataPriority = null;
 
-
-
-
-
 /**
-
 * KEYWORDS REPORT
-
 */
 
-
-
 $arrDataKeys = null;
-
 $arrDataKeysIndex = 0;
-
 $arrKeywords = $tp->get_keywords_map($tpID); 
 
 while ($keyword_id = key($arrKeywords)) {
-
 	$keyword_name = $arrKeywords[$keyword_id] ;
-
 	$specificKeywordResults = new results($db, $tp, $suitesSelected, $builds_to_query, 'a', $keyword_id);
-
 	$resultArray = $specificKeywordResults->getTotalsForPlan();
-
 	$total = $resultArray['total'];
-
 	$notRun = $resultArray['notRun'];
-
 	$percentCompleted = (($total - $notRun) / $total) * 100;
-
+	$percentCompleted = number_format($percentCompleted,2);
 	$arrDataKeys[$arrDataKeysIndex] = array($keyword_name,$total,$resultArray['pass'],$resultArray['fail'],$resultArray['blocked'],$notRun,$percentCompleted);
-
 	$arrDataKeysIndex++;
-
 	next($arrKeywords);
-
 }
 
-
-
-
-
-
-
 /** 
-
 * OWNERS REPORT 
-
 */
-
 define('ALL_USERS_FILTER', null);
-
 define('ADD_BLANK_OPTION', false);
-
 $arrOwners = get_users_for_html_options($db, ALL_USERS_FILTER, ADD_BLANK_OPTION);
-
 //$arrDataOwner = getOwnerReport($db,$tpID);
-
 $arrDataOwner = null;
-
 $arrDataOwnerIndex = 0;
 
 while ($owner_id = key($arrOwners)) {
-
 	$owner_name = $arrOwners[$owner_id] ;
-
 	$specificOwnerResults = new results($db, $tp, $suitesSelected, $builds_to_query, 'a', 0, $owner_id);
-
 	$resultArray = $specificOwnerResults->getTotalsForPlan();
-
 	$total = $resultArray['total'];
-
 	$notRun = $resultArray['notRun'];
-
 	$percentCompleted = 0;
-	if ($total)
+	if ($total) {
 		$percentCompleted = (($total - $notRun) / $total) * 100;
-
+		$percentCompleted = number_format($percentCompleted,2);
+	}
 	$arrDataOwner[$arrDataOwnerIndex] = array($owner_name,$total,$resultArray['pass'],$resultArray['fail'],$resultArray['blocked'],$notRun,$percentCompleted);
-
 	$arrDataOwnerIndex++;
-
 	next($arrOwners);
-
 }
-
-
-
-
-
-
-
-
 
 /**
 
@@ -184,25 +120,13 @@ while ($owner_id = key($arrOwners)) {
 
 */
 
-
-
-
-
 $smarty = new TLSmarty;
-
 $smarty->assign('tpName', $_SESSION['testPlanName']);
-
-
 $smarty->assign('buildName', $buildName);
-
 $smarty->assign('arrDataPriority', $arrDataPriority);
-
 $smarty->assign('arrDataSuite', $arrDataSuite);
-
 $smarty->assign('arrDataCategory', $arrDataCategory);
-
 $smarty->assign('arrDataKeys', $arrDataKeys);
-
 $smarty->display('resultsBuild.tpl');
 
 ?>

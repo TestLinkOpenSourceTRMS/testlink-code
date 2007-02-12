98eb0a69f5abd55b7a7cbab3fc053429c37ca890
franciscom <>
2007-02-12 08:02:42 +0000

    changes html input name to avoid conflicts

--- gui/templates/execNavigator.tpl
+++ gui/templates/execNavigator.tpl
@@ -1,10 +1,18 @@
 {* TestLink Open Source Project - http://testlink.sourceforge.net/ *}
-{* $Id: execNavigator.tpl,v 1.12 2007/01/29 20:19:42 schlundus Exp $ *}
+{* $Id: execNavigator.tpl,v 1.13 2007/02/12 08:02:16 franciscom Exp $ *}
 {* Purpose: smarty template - show test set tree *}
-{* 20050828 - scs - added searching for tcID *}
+{* 
+rev :
+     20070212 - franciscom - name changes on html inputs
+                             use input_dimensions.conf
+
+*}
 {include file="inc_head.tpl" jsTree="yes"}
 
 <body>
+{assign var="cfg_section" value=$smarty.template|replace:".tpl":"" }
+{config_load file="input_dimensions.conf" section=$cfg_section}
+
 <h1>{lang_get s='TestPlan'} {$tplan_name|escape}</h1> 
 
 {* $filterForm *}
@@ -18,7 +26,7 @@
 		</caption>
 		<tr>
 			<td>{lang_get s='filter_tcID'}</td>
-			<td><input type="text" name="tcID" value="{$tcID}" maxlength="10" size="5"/></td>
+			<td><input type="text" name="tcID" value="{$tcID}" maxlength="{#TC_ID_MAXLEN#}" size="{#TC_ID_SIZE#}"/></td>
 		</tr>
 		<tr>
 			<td>{lang_get s='keyword'}</td>
@@ -29,12 +37,12 @@
 		</tr>
 		<tr>
 				<td>{lang_get s='filter_result'}</td>
-			<td><select name="result">
+			<td><select name="filter_status">
 			{html_options options=$optResult selected=$optResultSelected}
 			</select></td>
 		</tr>
 		<tr>
-			<td>{lang_get s='current_build'}</td>
+			<td>{lang_get s='build'}</td>
 			<td><select name="build_id">
 				{html_options options=$optBuild selected=$optBuildSelected}
 				</select>
@@ -42,14 +50,14 @@
 		</tr>
 		<tr>
 			<td>{lang_get s='filter_owner'}</td>
-			<td><select name="owner">
+			<td><select name="filter_assigned_to">
 				{html_options options=$users selected=$selectedOwner}
 				</select>
 			</td>
 		</tr>
 		<tr>
 			<td>&nbsp;</td>
-			<td><input type="submit" name="submitOptions" value="{lang_get s='btn_update_menu'}" style="font-size: 90%;" /></td>
+			<td><input type="submit" name="submitOptions" value="{lang_get s='btn_apply_filter'}" style="font-size: 90%;" /></td>
 		</tr>
 	</table>
 </form>
--- lib/execute/execNavigator.php
+++ lib/execute/execNavigator.php
@@ -5,9 +5,11 @@
  *
  * Filename $RCSfile: execNavigator.php,v $
  *
- * @version $Revision: 1.33 $
- * @modified $Date: 2007/01/29 20:19:42 $ by $Author: schlundus $
+ * @version $Revision: 1.34 $
+ * @modified $Date: 2007/02/12 08:02:42 $ by $Author: franciscom $
  *
+ *
+ * 20070212 - franciscom - name changes on html inputs
  *
  * 20070123 - franciscom - 
  * 1. added logic to only show ACTIVE BUILDS
@@ -26,7 +28,7 @@ require_once('builds.inc.php');
 testlinkInitPage($db);
 
 $treeColored = (isset($_POST['colored']) && ($_POST['colored'] == 'result')) ? 'selected="selected"' : null;
-$selectedOwner = isset($_POST['owner']) ? intval($_POST['owner']) : 0;             
+$filter_assigned_to = isset($_POST['filter_assigned_to']) ? intval($_POST['filter_assigned_to']) : 0;             
 
 $tc_id = isset($_POST['tcID']) ? intval($_POST['tcID']) : null;
 $keyword_id = isset($_POST['keyword_id']) ? $_POST['keyword_id'] : 0;             
@@ -39,7 +41,7 @@ $tplan_mgr = new testplan($db);
 // only active builds no matter user role
 //
 $optBuild = $tplan_mgr->get_builds_for_html_options($tplan_id,ACTIVE);
-$optResultSelected = isset($_POST['result']) ? $_POST['result'] : 'all';
+$optResultSelected = isset($_POST['filter_status']) ? $_POST['filter_status'] : 'all';
 
 $maxBuildID = $tplan_mgr->get_max_build_id($tplan_id);
 $optBuildSelected = isset($_POST['build_id']) ? $_POST['build_id'] : $maxBuildID;
@@ -63,6 +65,8 @@ $menuUrl = null;
 $SP_html_help_file = TL_INSTRUCTIONS_RPATH . $_SESSION['locale'] . "/executeTest.html";
 
 $menuUrl = 'lib/execute/execSetResults.php';
+
+/*
 $getArguments = '&build_id=' . $optBuildSelected;
 if ($keyword_id)
 	$getArguments .= '&keyword_id='.$keyword_id;
@@ -72,13 +76,27 @@ if ($selectedOwner)
 	$getArguments .= '&owner='.$selectedOwner;
 if ($optResultSelected != 'all')
 	$getArguments .= '&status='.$optResultSelected;
+*/
+
+// 20070212 - franciscom
+$getArguments = '&build_id=' . $optBuildSelected;
+if ($keyword_id)
+	$getArguments .= '&keyword_id='.$keyword_id;
+if ($tc_id)
+	$getArguments .= '&tc_id='.$tc_id;
+if ($filter_assigned_to)
+	$getArguments .= '&filter_assigned_to='.$filter_assigned_to;
+if ($optResultSelected != 'all')
+	$getArguments .= '&filter_status='.$optResultSelected;
+
 
 $optResult = createResultsMenu();
 
 if ($optResultSelected == 'all')
 	$optResultSelected = null;
 $sMenu = generateExecTree($db,$menuUrl,$tproject_id,$tproject_name,$tplan_id,$tplan_name,
-                          $optBuildSelected,$getArguments,$keyword_id,$tc_id,false,$selectedOwner,$optResultSelected);
+                          $optBuildSelected,$getArguments,$keyword_id,$tc_id,false,$filter_assigned_to,
+                          $optResultSelected);
 
 // link to load frame named 'workframe' when the update button is pressed
 $src_workframe = null;
@@ -102,9 +120,11 @@ $smarty->assign('treeKind', TL_TREE_KIND);
 $smarty->assign('treeColored', $treeColored);
 $smarty->assign('optBuild', $optBuild);
 $smarty->assign('optBuildSelected', $optBuildSelected);
+
 $smarty->assign('optResult', $optResult);
 $smarty->assign('optResultSelected', $optResultSelected); 
-$smarty->assign('selectedOwner', $selectedOwner);
+
+$smarty->assign('filter_assigned_to', $filter_assigned_to);
 $smarty->assign('keywords_map', $keywords_map);
 $smarty->assign('keyword_id', $keyword_id);
 $smarty->assign('tcID', intval($tc_id) > 0 ? $tc_id : '');

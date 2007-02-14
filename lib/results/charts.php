e72227efaf31285722e6757ae0617546cc09f304
kevinlevy <>
2007-02-14 23:03:21 +0000

    display results by keyword

--- lib/results/charts.php
+++ lib/results/charts.php
@@ -1,28 +1,21 @@
-<table>
 <h3>
 	Overall Metrics
 </h3>
-<tr>
-<td>
 <?php
 //include charts.php to access the InsertChart function
 include "../../third_party/charts/charts.php";
 echo InsertChart ( "../../third_party/charts/charts.swf", "../../third_party/charts/charts_library", "overallPieChart.php", 400, 250 );
 ?>
-</td>
 
-</tr>
-</table>
 <h6>
 Copyright © 2003-2007, maani.us
 </h6>
-<!--
-<tr>
 
-<td>
+<h3>Results by Keyword</h3>
+
 <?php
 echo InsertChart ( "../../third_party/charts/charts.swf", "../../third_party/charts/charts_library", "priorityBarChart.php", 450, 300 );
 ?>
-</td>
-</tr>
--->
+<h6>
+Copyright © 2003-2007, maani.us
+</h6>

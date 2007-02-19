40890cc74d521cff66ddb18c35932323b48fb4ec
franciscom <>
2007-02-19 13:22:35 +0000

    localization

--- lib/results/charts.php
+++ lib/results/charts.php
@@ -1,26 +1,35 @@
-<h2>Graphical Reports</h2><h6>Copyright © 2003-2007, maani.us</h6>
-<h3>
-	Overall Metrics
-</h3>
 <?php
+/** TestLink Open Source Project - http://testlink.sourceforge.net/ 
+ * 
+ * @filesource $RCSfile: charts.php,v $
+ * @version $Revision: 1.7 $
+ * @modified $Date: 2007/02/19 13:22:35 $  $Author: franciscom $
+ * @author kevin
+ *
+ *
+**/
+require_once('../../config.inc.php');
+require_once('../functions/common.php');
+
+echo "<h2>",lang_get('graphical_reports'),"</h2><h6>",lang_get('maani_copyright'),"</h6>";
+echo "<h3>",lang_get('overall_metrics'),"</h3>";
+	
+
 //include charts.php to access the InsertChart function
 include "../../third_party/charts/charts.php";
-echo InsertChart ( "../../third_party/charts/charts.swf", "../../third_party/charts/charts_library", "overallPieChart.php", 400, 250 );
-?>
+echo InsertChart ( "../../third_party/charts/charts.swf", 
+                   "../../third_party/charts/charts_library", "overallPieChart.php", 400, 250 );
 
-<h3>Results by Keyword</h3>
-<?php
-echo InsertChart ( "../../third_party/charts/charts.swf", "../../third_party/charts/charts_library", "keywordBarChart.php", 800, 600 );
-?>
+echo "<h3>",lang_get('results_by_keyword'),"</h3>";
 
+echo InsertChart ( "../../third_party/charts/charts.swf", 
+                   "../../third_party/charts/charts_library", "keywordBarChart.php", 800, 600 );
 
-<h3>Results by Owner</h3>
-<?php
-echo InsertChart ( "../../third_party/charts/charts.swf", "../../third_party/charts/charts_library", "ownerBarChart.php", 800, 600);
-?>
+echo "<h3>",lang_get('results_by_tester'),"</h3>";
+echo InsertChart ( "../../third_party/charts/charts.swf", "../../third_party/charts/charts_library", 
+                   "ownerBarChart.php", 800, 600);
 
-
-<h3>Results for Top Level Suites</h3>
-<?php
-echo InsertChart ( "../../third_party/charts/charts.swf", "../../third_party/charts/charts_library", "topLevelSuitesBarChart.php", 800, 600);
+echo "<h3>",lang_get('results_top_level_suites'),"</h3>";
+echo InsertChart ( "../../third_party/charts/charts.swf", "../../third_party/charts/charts_library", 
+                   "topLevelSuitesBarChart.php", 800, 600);
 ?>

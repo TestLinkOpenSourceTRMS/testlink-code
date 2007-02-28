aba2a966fc22c82ad1667799fb73115dba9547eb
kevinlevy <>
2007-02-28 07:41:39 +0000

    print name of test plan in header of each graph

--- lib/results/charts.php
+++ lib/results/charts.php
@@ -2,8 +2,8 @@
 /** TestLink Open Source Project - http://testlink.sourceforge.net/ 
  * 
  * @filesource $RCSfile: charts.php,v $
- * @version $Revision: 1.8 $
- * @modified $Date: 2007/02/28 07:16:22 $  $Author: kevinlevy $
+ * @version $Revision: 1.9 $
+ * @modified $Date: 2007/02/28 07:41:39 $  $Author: kevinlevy $
  * @author kevin
  *
  *
@@ -14,7 +14,7 @@ testlinkInitPage($db);
 $testPlanName = $_SESSION['testPlanName']; 
 
 echo "<h2>",$_SESSION['testPlanName']," ",lang_get('graphical_reports'),"</h2><h6>",lang_get('maani_copyright'),"</h6>";
-echo "<h3>",lang_get('overall_metrics'),"</h3>";
+echo "<h3>",$_SESSION['testPlanName']," ",lang_get('overall_metrics'),"</h3>";
 	
 
 //include charts.php to access the InsertChart function
@@ -22,16 +22,16 @@ include "../../third_party/charts/charts.php";
 echo InsertChart ( "../../third_party/charts/charts.swf", 
                    "../../third_party/charts/charts_library", "overallPieChart.php", 400, 250 );
 
-echo "<h3>",lang_get('results_by_keyword'),"</h3>";
+echo "<h3>",$_SESSION['testPlanName']," ",lang_get('results_by_keyword'),"</h3>";
 
 echo InsertChart ( "../../third_party/charts/charts.swf", 
                    "../../third_party/charts/charts_library", "keywordBarChart.php", 800, 600 );
 
-echo "<h3>",lang_get('results_by_tester'),"</h3>";
+echo "<h3>",$_SESSION['testPlanName']," ",lang_get('results_by_tester'),"</h3>";
 echo InsertChart ( "../../third_party/charts/charts.swf", "../../third_party/charts/charts_library", 
                    "ownerBarChart.php", 800, 600);
 
-echo "<h3>",lang_get('results_top_level_suites'),"</h3>";
+echo "<h3>",$_SESSION['testPlanName']," ",lang_get('results_top_level_suites'),"</h3>";
 echo InsertChart ( "../../third_party/charts/charts.swf", "../../third_party/charts/charts_library", 
                    "topLevelSuitesBarChart.php", 800, 600);
 ?>

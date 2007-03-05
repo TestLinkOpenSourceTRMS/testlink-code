b35672213a99f7b0f470e4376a87fa9865611670
franciscom <>
2007-03-05 18:22:04 +0000

    *** empty log message ***

--- cfg/mantis.cfg.php
+++ cfg/mantis.cfg.php
@@ -1,7 +1,7 @@
 <?php
 /**
 * TestLink Open Source Project - http://testlink.sourceforge.net/ 
-* $Id: mantis.cfg.php,v 1.6 2007/03/04 00:03:19 schlundus Exp $ 
+* $Id: mantis.cfg.php,v 1.7 2007/03/05 18:22:04 franciscom Exp $ 
 * 
 * Constants used throughout TestLink are defined within this file
 * they should be changed for your environment
@@ -13,17 +13,19 @@
 //also tested with MANTIS 1.0.0.a3
 
 /** The DB host to use when connecting to the mantis db */
-define('BUG_TRACK_DB_HOST', '[mantisdbhost]');
+define('BUG_TRACK_DB_HOST', '[CONFIGURE_BUG_TRACK_DB_HOST]');
 
 /** The name of the database that contains the mantis tables */
-define('BUG_TRACK_DB_NAME', '[mantisdbname]');
+define('BUG_TRACK_DB_NAME', '[CONFIGURE_BUG_TRACK_DB_NAME]');
 
-/** The DB type being used by mantis */
-define('BUG_TRACK_DB_TYPE', '[mantisdbtype]');
+/** The DB type being used by mantis 
+values: mysql,mssql,postgres
+*/
+define('BUG_TRACK_DB_TYPE', '[CONFIGURE_BUG_TRACK_DB_TYPE]');
 
 /** The DB password to use for connecting to the mantis db */
-define('BUG_TRACK_DB_USER', '[mantisdbuser]');
-define('BUG_TRACK_DB_PASS', '[mantisdbpassword]');
+define('BUG_TRACK_DB_USER', '[CONFIGURE_BUG_TRACK_DB_USER]');
+define('BUG_TRACK_DB_PASS', '[CONFIGURE_BUG_TRACK_DB_USER_PASS]');
 
 
 /* link of the web server for mantis*/
@@ -35,6 +37,7 @@ define('BUG_TRACK_DB_PASS', '[mantisdbpassword]');
 	$g_anonymous_account		= 'dummy';
 */
 define('BUG_TRACK_HREF', "http://localhost/mantis/view.php?id="); 
+
 /** link to the bugtracking system, for entering new bugs */
-define('BUG_TRACK_ENTER_BUG_HREF',"http://localhost/mantis-1.0.6/");
+define('BUG_TRACK_ENTER_BUG_HREF',"http://localhost/mantis/");
 ?>

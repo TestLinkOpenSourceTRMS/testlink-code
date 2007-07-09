8699e1365366aa577d9aa0c32d178a5a13c7f6d6
franciscom <>
2007-07-09 07:55:17 +0000

    empty custom_config.inc.php - to explain user to use it

---
+++ custom_config.inc.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * TestLink Open Source Project - http://testlink.sourceforge.net/ 
+ * This script is distributed under the GNU General Public License 2 or later. 
+ *
+ * Filename $RCSfile: custom_config.inc.php,v $
+ *
+ * @version $Revision: 1.1 $
+ * @modified $Date: 2007/07/09 07:55:17 $ by $Author: franciscom $
+ *
+ * SCOPE:
+ * Constants and configuration parameters used throughout TestLink 
+ * DEFINED BY USERS.
+ *
+ * Use this page to overwrite configuration parameters (variables and defines)
+ * presente in:
+ *
+ *             config.inc.php
+ *             cfg\const.inc.php
+ *-----------------------------------------------------------------------------
+*/
+
+?>

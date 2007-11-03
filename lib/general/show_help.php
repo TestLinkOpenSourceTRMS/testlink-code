b1fa09ddadd300e57e75b585178a5159b9699498
franciscom <>
2007-11-03 07:36:11 +0000

    BUGID 1033

--- lib/general/show_help.php
+++ lib/general/show_help.php
@@ -4,10 +4,13 @@
  *
  * Filename $RCSfile: show_help.php,v $
  *
- * @version $Revision: 1.3 $
- * @modified $Date: 2007/05/19 21:08:15 $  $Author: schlundus $
+ * @version $Revision: 1.4 $
+ * @modified $Date: 2007/11/03 07:36:11 $  $Author: franciscom $
  *
  * manage launch of help pages.
+ *
+ * rev:
+ *     20071102 - franciscom - BUGID 1033
 **/
 require('../../config.inc.php');
 require_once("common.php");
@@ -17,5 +20,8 @@ testlinkInitPage($db);
 $smarty = new TLSmarty();
 $td = TL_ABS_PATH . TL_HELP_RPATH . $_REQUEST['locale'];
 $smarty->template_dir = $td;
+
+// BUGID 1033
+$smarty->clear_compiled_tpl($_REQUEST['help'] . ".html"); 
 $smarty->display($_REQUEST['help'] . ".html");
 ?>

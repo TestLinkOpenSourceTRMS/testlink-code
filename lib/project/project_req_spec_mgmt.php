408edf6bac95970c4974bf8e63f74ea3692c78da
franciscom <>
2007-11-19 21:02:06 +0000

    new for refactored requirements feature

---
+++ lib/project/project_req_spec_mgmt.php
@@ -0,0 +1,32 @@
+<?php
+/** 
+ * TestLink Open Source Project - http://testlink.sourceforge.net/ 
+ * This script is distributed under the GNU General Public License 2 or later. 
+ *  
+ * @version $Id: project_req_spec_mgmt.php,v 1.1 2007/11/19 21:02:06 franciscom Exp $
+ * @author Martin Havlat
+ *  
+ * Allows you to show test suites, test cases.
+ * Normally launched from tree navigator.
+ *
+ * rev :
+ *      20070930 - franciscom - REQ - BUGID 1078
+ * 
+ */
+require_once('../../config.inc.php');
+require_once('common.php');
+testlinkInitPage($db);
+
+$user_id = isset($_SESSION['userID']) ? $_SESSION['userID'] : 0;
+$tproject_id   = isset($_SESSION['testprojectID']) ? $_SESSION['testprojectID'] : 0;
+$tproject_name = isset($_SESSION['testprojectName']) ? $_SESSION['testprojectName'] : 'undefned';
+
+// load data and show template
+$smarty = new TLSmarty();
+$smarty->assign('name', $tproject_name);
+
+$smarty->display('requirements/project_req_spec_mgmt.tpl');
+// $smarty->assign('page_title',lang_get('container_title_' . $feature));
+// $tproject_mgr = new testproject($db);
+// $tproject_mgr->show_req_mgmt($smarty,$id);
+?>

8cef91d8fdc62d98fd205e64e3a05cf78687e897
franciscom <>
2008-01-04 16:20:11 +0000

    REQID 1232

--- lib/execute/getExecNotes.php
+++ lib/execute/getExecNotes.php
@@ -5,10 +5,10 @@
  *
  * Filename $RCSfile: getExecNotes.php,v $
  *
- * @version $Revision: 1.1 $
- * @modified $Date: 2007/12/31 16:25:19 $ by $Author: franciscom $
+ * @version $Revision: 1.2 $
+ * @modified $Date: 2008/01/04 16:20:11 $ by $Author: franciscom $
  *
- * Deletes a bug
+ * 
 **/
 require_once('../../config.inc.php');
 require_once('common.php');
@@ -18,11 +18,11 @@ testlinkInitPage($db);
 $template_dir='execute/';
 $default_template = str_replace('.php','.tpl',basename($_SERVER['SCRIPT_NAME']));
 
-
+$webeditorType=config_get('gui')->webeditor;
 $map=get_execution($db,$_REQUEST['exec_id']);
-//echo(nl2br($map[0]['notes']));
 $smarty = new TLSmarty();
 $smarty->assign('notes',$map[0]['notes']);
+$smarty->assign('webeditorType',$webeditorType);
 $smarty->display($template_dir . $default_template);
 
 ?>

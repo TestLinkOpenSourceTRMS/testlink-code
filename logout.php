4a9d84dad144c31b8a01356b0b2eba96619e9c87
schlundus <>
2008-01-18 21:33:20 +0000

    logging added

--- logout.php
+++ logout.php
@@ -5,14 +5,20 @@
  * 
  * Filename $RCSfile: logout.php,v $
  *
- * @version $Revision: 1.9 $
- * @modified $Date: 2008/01/05 22:00:51 $
+ * @version $Revision: 1.10 $
+ * @modified $Date: 2008/01/18 21:33:20 $
 **/
 // Unset all of the session variables.
+require_once('config.inc.php');
+require_once('common.php');
+$userID = null;
+doDBConnect($db);
 if(!isset($_SESSION))
 { 
-  session_start();
+	session_start();
+	$userID = $_SESSION['userID'] ?  $_SESSION['userID'] : null;
 }
+tLog(TLS("audit_user_logout"),'AUDIT',null,$userID,"users");  
 session_unset();
 session_destroy();
 ?>

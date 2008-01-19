010d8784d729035c7f1ebe702f6048620a7da538
franciscom <>
2008-01-19 17:51:21 +0000

    minor refactoring

--- lib/testcases/archiveData.php
+++ lib/testcases/archiveData.php
@@ -3,13 +3,14 @@
  * TestLink Open Source Project - http://testlink.sourceforge.net/ 
  * This script is distributed under the GNU General Public License 2 or later. 
  *  
- * @version $Id: archiveData.php,v 1.30 2008/01/14 08:08:18 franciscom Exp $
+ * @version $Id: archiveData.php,v 1.31 2008/01/19 17:51:21 franciscom Exp $
  * @author Martin Havlat
  *  
  * Allows you to show test suites, test cases.
  * Normally launched from tree navigator.
  *
  * rev :
+ *      20080119 - franciscom - code clean up
  *      20070930 - franciscom - REQ - BUGID 1078
  * 
  */
@@ -19,50 +20,44 @@ require_once("../functions/attachments.inc.php");
 testlinkInitPage($db);
 
 $template_dir='testcases/';
-
-$user_id = isset($_SESSION['userID']) ? $_SESSION['userID'] : 0;
-$feature = isset($_GET['edit']) ? $_GET['edit'] : null;
-$id = isset($_GET['id']) ? intval($_GET['id']) : null;
-$allow_edit = isset($_GET['allow_edit']) ? intval($_GET['allow_edit']) : 1;
-$targetTestCase = isset($_GET['targetTestCase']) ? $_GET['targetTestCase'] : null;
+$args=init_args();
 
 // load data and show template
 $smarty = new TLSmarty();
-$smarty->assign('page_title',lang_get('container_title_' . $feature));
-switch($feature)
+$smarty->assign('page_title',lang_get('container_title_' . $args->feature));
+switch($args->feature)
 {
 	case 'testproject':
 		$item_mgr = new testproject($db);
-		$smarty->assign('id',$id);
-		$attachments = getAttachmentInfosFrom($item_mgr,$id);
+		$smarty->assign('id',$args->id);
+		$attachments = getAttachmentInfosFrom($item_mgr,$args->id);
 		$smarty->assign('attachmentInfos',$attachments);
-		
-    	$item_mgr->show($smarty,$template_dir,$id);
+   	$item_mgr->show($smarty,$template_dir,$args->id);
 		break;
 
 	case 'testsuite':
-		$smarty->assign('id',$id);
+		$smarty->assign('id',$args->id);
 		$item_mgr = new testsuite($db);
-		$attachments = getAttachmentInfosFrom($item_mgr,$id);
+		$attachments = getAttachmentInfosFrom($item_mgr,$args->id);
 		$smarty->assign('attachmentInfos',$attachments);
 		
 	    $_SESSION['tcspec_refresh_on_action'] = isset($_REQUEST['tcspec_refresh_on_action'])? "yes":"no";
-		$item_mgr->show($smarty,$template_dir,$id);
+		$item_mgr->show($smarty,$template_dir,$args->id);
 		break;
 
 	case 'testcase':
 		$item_mgr = new testcase($db);
-    if( !is_null($targetTestCase) )
+    if( !is_null($args->targetTestCase) )
     {
        // need to get internal Id from External ID
        $cfg = config_get('testcase_cfg');
-       $id=$item_mgr->getInternalID($targetTestCase,$cfg->glue_character); 
+       $args->id=$item_mgr->getInternalID($args->targetTestCase,$cfg->glue_character); 
     }
 
-		$attachments = getAttachmentInfosFrom($item_mgr,$id);
-		$attachmentsTpl[$id] = $attachments;
+		$attachments = getAttachmentInfosFrom($item_mgr,$args->id);
+		$attachmentsTpl[$args->id] = $attachments;
 		
-		$smarty->assign('id',$id);
+		$smarty->assign('id',$args->id);
 		$smarty->assign('attachments',$attachmentsTpl);
 				
 		$no_msg = '';
@@ -76,12 +71,12 @@ switch($feature)
     	
 	    // 20070930 - franciscom - REQ - BUGID 1078
     	// added two arguments on call.
-		$item_mgr->show($smarty,$template_dir,$id,$user_id,TC_ALL_VERSIONS,
-		                $no_action,$no_msg,$do_refresh_yes_no,$no_user_feedback,!$allow_edit);
+		$item_mgr->show($smarty,$template_dir,$args->id,$args->user_id,TC_ALL_VERSIONS,
+		                $no_action,$no_msg,$do_refresh_yes_no,$no_user_feedback,!$args->allow_edit);
 		break;
 
 	default:
-		tLog('$_GET["edit"] has invalid value: ' . $feature , 'ERROR');
+		tLog('$_GET["edit"] has invalid value: ' . $args->feature , 'ERROR');
 		trigger_error($_SESSION['currentUser']->login.'> $_GET["edit"] has invalid value.', E_USER_ERROR);
 }
 ?>
@@ -90,10 +85,10 @@ switch($feature)
 function init_args()
 {
     $args->user_id = isset($_SESSION['userID']) ? $_SESSION['userID'] : 0;
-    $args->feature = isset($_GET['edit']) ? $_GET['edit'] : null;
-    $args->id = isset($_GET['id']) ? intval($_GET['id']) : null;
-    // $targetTestCase
-    $args->allow_edit = isset($_GET['allow_edit']) ? intval($_GET['allow_edit']) : 1;
+    $args->feature = isset($_REQUEST['edit']) ? $_REQUEST['edit'] : null;
+    $args->id = isset($_REQUEST['id']) ? intval($_REQUEST['id']) : null;
+    $args->targetTestCase = isset($_REQUEST['targetTestCase']) ? $_REQUEST['targetTestCase'] : null;
+    $args->allow_edit = isset($_REQUEST['allow_edit']) ? intval($_REQUEST['allow_edit']) : 1;
     return $args;  
 }
 ?>

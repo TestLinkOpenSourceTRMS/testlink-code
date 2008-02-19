c9da0636100bf196fa1ff24d3bef45a245c5ed8d
franciscom <>
2008-02-19 17:10:13 +0000

    *** empty log message ***

--- install/sql/alter_tables/1.8/mysql/DB.1.2/db_version.sql
+++
@@ -1,8 +0,0 @@
-/* 
-$Revision: 1.1 $
-$Date: 2008/01/02 18:56:05 $
-$Author: franciscom $
-$Name:  $
-*/
-ALTER TABLE db_version ADD COLUMN notes  text;
-ALTER TABLE executions COMMENT = 'Updated to TL 1.8.0 Development - DB 1.2';

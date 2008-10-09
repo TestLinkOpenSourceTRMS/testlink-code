a9599566d2c72d076e07d15c6614a3185e6a8278
schlundus <>
2008-10-09 18:42:29 +0000

    0001781: Cannon open requiremen document from "Assign Requirements" page.

--- lib/requirements/reqView.php
+++ lib/requirements/reqView.php
@@ -4,8 +4,8 @@
  * This script is distributed under the GNU General Public License 2 or later. 
  *  
  * @filesource $RCSfile: reqView.php,v $
- * @version $Revision: 1.9 $
- * @modified $Date: 2008/05/12 19:46:59 $ by $Author: franciscom $
+ * @version $Revision: 1.10 $
+ * @modified $Date: 2008/10/09 18:42:29 $ by $Author: schlundus $
  * @author Martin Havlat
  * 
  * Screen to view content of requirement.
@@ -23,24 +23,22 @@ testlinkInitPage($db);
 $templateCfg = templateConfiguration();
 $req_mgr = new requirement_mgr($db);
 
-$gui=new stdClass();
-$gui->grants=new stdClass();
+$gui = new stdClass();
+$gui->grants = new stdClass();
 $gui->grants->req_mgmt = has_rights($db,"mgt_modify_req");
 
 $gui->req_id = isset($_REQUEST['requirement_id']) ? intval($_REQUEST['requirement_id']) : null;
 $gui->req = $req_mgr->get_by_id($gui->req_id);
 
 $gui->showReqSpecTitle = isset($_REQUEST['showReqSpecTitle']) ? intval($_REQUEST['showReqSpecTitle']) : 0;
-if( $gui->showReqSpecTitle )
-{
+if($gui->showReqSpecTitle)
     $gui->parent_descr = lang_get('req_spec') . TITLE_SEP . $gui->req['req_spec_title'];
-}
 
 $gui->req['coverage'] = $req_mgr->get_coverage($gui->req_id);
 $gui->main_descr = lang_get('req') . TITLE_SEP . $gui->req['title'];
 $gui->cfields = $req_mgr->html_table_of_custom_field_values($gui->req_id);
 $gui->attachments = getAttachmentInfosFrom($req_mgr,$gui->req_id);
-$gui->reqStatus=init_labels(config_get('req_status'));
+$gui->reqStatus = init_labels(config_get('req_status'));
 
 $smarty = new TLSmarty();
 $smarty->assign('gui',$gui);

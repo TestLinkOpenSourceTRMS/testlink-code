0f95ba1304b82713462da5d0c8f157bde37e9b76
franciscom <>
2009-03-14 09:41:08 +0000

    new feature report developed by Amit (test cases with Cf values)

---
+++ gui/templates/results/testCasesWithCF.tpl
@@ -0,0 +1,69 @@
+{* 
+TestLink Open Source Project - http://testlink.sourceforge.net/ 
+$Id: testCasesWithCF.tpl,v 1.1 2009/03/14 09:40:57 franciscom Exp $
+
+Purpose: For a test plan, list test cases with Custom Fields at Execution
+
+rev:  
+*}
+
+{lang_get var="labels" 
+          s='no_uncovered_testcases,testproject_has_no_reqspec,
+             testproject_has_no_requirements,no_linked_tc_cf,generated_by_TestLink_on,
+             test_case,build,th_owner,date,status'}
+{include file="inc_head.tpl" openHead="yes"}
+</head>
+<body>
+<h1 class="title">{$gui->pageTitle|escape}</h1>
+<div class="workBack" style="overflow-y: auto;">
+
+ {include file="inc_result_tproject_tplan.tpl" 
+          arg_tproject_name=$gui->tproject_name arg_tplan_name=$gui->tplan_name}	
+
+
+
+{if $gui->warning_msg == ''}
+    {if $gui->resultSet }
+        <table class="simple">
+	          <tr>
+	          <th> {$labels.test_case}</th>
+	          <th> {$labels.build}</th>
+	          <th> {$labels.th_owner} </th>
+	          <th> {$labels.date} </th>
+	          <th> {$labels.status} </th>
+	          {foreach from=$gui->cfields item=cfield}
+	              <th>{$cfield.label|escape}</th>
+	          {/foreach}
+	          </tr>
+            
+	          {foreach from=$gui->resultSet item=arrData }
+	            <tr bgcolor="{cycle values="#eeeeee,#d0d0d0"}">  
+	            <td>	<a href="lib/testcases/archiveData.php?edit=testcase&id={$arrData.tcase_id}">
+	          	{$gui->tcasePrefix}{$arrData.tc_external_id|escape}:{$arrData.tcase_name|escape}</a>
+	            </td>
+	            <td>{$arrData.build_name|escape}</td>
+	            <td>{$arrData.tester|escape}</td>
+	            
+	            <td><a href="lib/execute/execSetResults.php?level=testcase&build_id={$arrData.builds_id}&id={$arrData.tcase_id}&version_id={$arrData.tcversion_id}">
+	               {localize_timestamp ts=$arrData.execution_ts}
+                  </a>
+	            </td>
+	            <td> {$gui->status_code_labels[$arrData.exec_status]}</td>
+	          	{foreach from=$arrData.cfields item=cfield_value}
+	          		<td> {$cfield_value}</td>
+	          	{/foreach}	
+	            </tr>
+	          {/foreach}
+                 
+        </table>
+
+      {$labels.generated_by_TestLink_on} {$smarty.now|date_format:$gsmarty_timestamp_format}
+    {else}
+    	<h2>{$labels.no_linked_tc_cf}</h2>
+    {/if}
+{else}
+    {$gui->warning_msg}
+{/if}    
+</div>
+</body>
+</html>
---
+++ lib/results/testCasesWithCF.php
@@ -0,0 +1,147 @@
+<?php
+/** 
+ * TestLink Open Source Project - http://testlink.sourceforge.net/
+ * This script is distributed under the GNU General Public License 2 or later. 
+ *  
+ * @filesource $RCSfile: testCasesWithCF.php,v $
+ * @version $Revision: 1.1 $
+ * @modified $Date: 2009/03/14 09:41:08 $ by $Author: franciscom $
+ * @author Amit Khullar - amkhullar@gmail.com
+ * 
+ * For a test plan, list test cases with Execution Custom Field Data 
+ * 
+ */
+require_once("../../config.inc.php");
+require_once("common.php");
+$cfield_mgr = new cfield_mgr($db);
+testlinkInitPage($db);
+$templateCfg = templateConfiguration();
+
+$tplan_mgr=new testplan($db);
+
+$args=init_args($tplan_mgr);
+
+$gui = new stdClass();
+$gui->pageTitle=lang_get('caption_testCasesWithCF');
+$gui->warning_msg='';
+$gui->tcasePrefix='';
+$gui->path_info=null;
+$gui->resultSet=null;
+$gui->tproject_name=$args->tproject_name;
+$gui->tplan_name=$args->tplan_name;
+$testCaseSet = array();
+$msg_key='no_linked_tc_cf';
+if( $tplan_mgr->count_testcases($args->tplan_id) > 0 )
+{
+	
+		$resultsCfg = config_get('results');
+    $tcase_cfg = config_get('testcase_cfg');
+
+		// -----------------------------------------------------------------------------------
+		// Get the mapping for the Verbose Status Description of Test Case Status
+		$map_tc_status_verbose_code = $resultsCfg['code_status'];
+		$map_tc_status_verbose_label = $resultsCfg['status_label'];
+		foreach($map_tc_status_verbose_code as $code => $verbose )
+		{
+		  if( isset($map_tc_status_verbose_label[$verbose]) )
+		  {
+			    $label = $map_tc_status_verbose_label[$verbose];
+			    $gui->status_code_labels[$code]=lang_get($label);
+			    // $map_label_css[$map_tc_status_code_langet[$code]]=$resultsCfg['code_status'][$code];
+		  }
+		}
+		// $not_run_label=lang_get('test_status_not_run');
+		// -----------------------------------------------------------------------------------
+
+    $tproject_mgr = new testproject($db);   
+    $gui->tcasePrefix=$tproject_mgr->getTestCasePrefix($args->tproject_id);
+    $gui->tcasePrefix .= $tcase_cfg->glue_character;
+
+		// Get the custom fields linked/enabled on execution to a test project 
+    // This will be used on report to give name to header of columns that hold custom field value
+		$gui->cfields = $cfield_mgr->get_linked_cfields_at_execution($args->tproject_id,1,'testcase',
+		                                                             null,null,null,'name');
+    
+    foreach($gui->cfields as $key => $values)
+    {
+       $cf_place_holder['cfields'][$key]='';
+    }
+   	// 
+   	
+   	// Now get exeutions with custom field values
+    $cf_map = $cfield_mgr->get_linked_cfields_at_execution($args->tproject_id,1,'testcase',
+                                                           null,null,$args->tplan_id,'exec_id');
+  	
+		// need to transform in structure that allow easy display
+		// Every row is an execution with exec data plus a column that contains following map:
+		// 'cfields' => CFNAME1 => value
+		//              CFNAME2 => value
+		$result=array();
+		if(!is_null($cf_map))
+		{
+       foreach($cf_map as $exec_id => $exec_info)
+		   {
+           // Get common exec info and remove useless keys
+		       $result[$exec_id]=$exec_info[0];
+		       unset($result[$exec_id]['name']);
+           unset($result[$exec_id]['label']);
+           unset($result[$exec_id]['display_order']);
+           unset($result[$exec_id]['id']);
+           unset($result[$exec_id]['value']);
+
+           // Collect custom fields values
+           $result[$exec_id] += $cf_place_holder;
+           foreach($exec_info as $cfield_data)
+		       {
+                 $result[$exec_id]['cfields'][$cfield_data['name']]=$cfield_data['value'];
+           }		
+		   }  
+		}
+		
+	  if(($gui->row_qty=count($cf_map)) > 0 )
+	  {
+	  		$msg_key='';
+	  		$gui->pageTitle .= " - " . lang_get('match_count') . ":" . $gui->row_qty;
+	  		$gui->resultSet=$result;
+	  }
+}
+
+$smarty = new TLSmarty();
+$smarty->assign('gui',$gui);
+$smarty->display($templateCfg->template_dir . $templateCfg->default_template);
+ 
+
+
+/*
+  function: 
+
+  args :
+  
+  returns: 
+
+*/
+function init_args(&$tplan_mgr)
+{
+    $args = new stdClass();
+    
+    $args->tproject_id = isset($_SESSION['testprojectID']) ? $_SESSION['testprojectID'] : 0;
+    $args->tproject_name = isset($_SESSION['testprojectName']) ? $_SESSION['testprojectName'] : '';
+
+    $args->tplan_id = isset($_REQUEST['tplan_id']) ? $_REQUEST['tplan_id'] : 0;
+    $args->tplan_name = '';
+    if( $args->tplan_id == 0 )
+    {
+        $args->tplan_id = isset($_SESSION['testPlanId']) ? $_SESSION['testPlanId'] : 0;
+    }
+    
+    if( $args->tplan_id > 0 )
+    {
+       $tplan_info = $tplan_mgr->get_by_id($args->tplan_id);
+       $args->tplan_name = $tplan_info['name'];  
+    }
+    
+    return $args;
+}
+?>
+
+

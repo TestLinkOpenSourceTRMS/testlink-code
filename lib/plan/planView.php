0645603be3937eb6067e5ea099e5b64dd4c50dd1
franciscom <>
2009-05-17 16:25:45 +0000

    test plan is public attribute

--- lib/plan/planView.php
+++ lib/plan/planView.php
@@ -5,34 +5,55 @@
  *
  * Filename $RCSfile: planView.php,v $
  *
- * @version $Revision: 1.9 $
- * @modified $Date: 2009/02/07 19:44:03 $ $Author: schlundus $
+ * @version $Revision: 1.10 $
+ * @modified $Date: 2009/05/17 16:25:45 $ $Author: franciscom $
  *
 */
 require_once('../../config.inc.php');
 require_once("common.php");
 testlinkInitPage($db,false,false,"checkRights");
 
-$template_dir = 'plan/';
+$templateCfg = templateConfiguration();
 
-$tplans = null;
-$tproject_id = isset($_SESSION['testprojectID']) ? intval($_SESSION['testprojectID']) : 0 ;
-$tproject_name = isset($_SESSION['testprojectName']) ? trim($_SESSION['testprojectName']) : '' ;
+$args=init_args();
 
-$main_descr = lang_get('testplan_title_tp_management'). " - " . 
-            lang_get('testproject') . ' ' . $tproject_name;
+$gui = new stdClass();
+$gui->tplans = null;
+$gui->user_feedback = '';
+$gui->grants = new stdClass();
+$gui->grants->testplan_create=has_rights($db,"mgt_testplan_create");
+$gui->main_descr = lang_get('testplan_title_tp_management'). " - " . 
+                   lang_get('testproject') . ' ' . $args->tproject_name;
 
-if($tproject_id)
+
+if($args->tproject_id)
 {
 	$tproject_mgr = new testproject($db);
-	$tplans = $tproject_mgr->get_all_testplans($tproject_id);
+	$gui->tplans = $tproject_mgr->get_all_testplans($args->tproject_id);
 }
-$smarty = new TLSmarty();
-$smarty->assign('main_descr',$main_descr);
-$smarty->assign('tplans',$tplans);
-$smarty->assign('testplan_create', has_rights($db,"mgt_testplan_create"));
-$smarty->display($template_dir . 'planView.tpl');
 
+$smarty = new TLSmarty();
+$smarty->assign('gui',$gui);
+$smarty->display($templateCfg->template_dir . $templateCfg->default_template);
+
+
+/**
+ * init_args
+ *
+ */
+function init_args()
+{
+    $args = new stdClass();
+    $args->tproject_id = isset($_SESSION['testprojectID']) ? intval($_SESSION['testprojectID']) : 0 ;
+    $args->tproject_name = isset($_SESSION['testprojectName']) ? trim($_SESSION['testprojectName']) : '' ;
+
+    return $args;
+}
+
+/**
+ * checkRights
+ *
+ */
 function checkRights(&$db,&$user)
 {
 	return $user->hasRight($db,'mgt_testplan_create');

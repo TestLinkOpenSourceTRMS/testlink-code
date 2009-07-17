971ad0a3b4f7a85df9cbacd43e020d0437a595e4
franciscom <>
2009-07-17 17:18:23 +0000

    changed in use value (23) -> (24) for right added is_public + location

--- install/sql/alter_tables/1.9/mysql/DB.1.3/db_schema_update.sql
+++ install/sql/alter_tables/1.9/mysql/DB.1.3/db_schema_update.sql
@@ -3,15 +3,27 @@
  * This script is distributed under the GNU General Public License 2 or later. 
  * 
  * SQL script: Update schema MySQL database for TestLink 1.9 from version 1.8 
- * "/ *prefix* /" - placeholder for tables with defined prefix
+ * "/ *prefix* /" - placeholder for tables with defined prefix, used by sqlParser.class.php.
  *
- * $Id: db_schema_update.sql,v 1.2 2009/07/15 21:37:51 havlat Exp $
+ * $Id: db_schema_update.sql,v 1.3 2009/07/17 17:18:23 franciscom Exp $
  *
  * Important Warning: 
  * This file will be processed by sqlParser.class.php, that uses SEMICOLON to find end of SQL Sentences.
  * It is not intelligent enough to ignore  SEMICOLONS inside comments, then PLEASE
  * USE SEMICOLONS ONLY to signal END of SQL Statements.
+ *
+ * internal revision:
+ *  20090717 - franciscom
+ *  cfield_testprojects new field location
+ *  testprojects new fiels is_public
+ *  testplans new fiels is_public
  */
+
+/* testprojects */
+ALTER TABLE /*prefix*/testprojects ADD COLUMN is_public tinyint NOT NULL DEFAULT '1',
+
+/* testplans */
+ALTER TABLE /*prefix*/testplans ADD COLUMN is_public tinyint NOT NULL DEFAULT '1',
 
 /* builds */
 ALTER TABLE /*prefix*/builds ADD COLUMN `creation_ts` datetime NOT NULL default NOW();
@@ -23,10 +35,13 @@ ALTER TABLE /*prefix*/builds ADD COLUMN `closed_on_date` date NULL;
 ALTER TABLE /*prefix*/testplan_tcversions ADD COLUMN author_id int(10) unsigned default NULL;
 ALTER TABLE /*prefix*/testplan_tcversions ADD COLUMN creation_ts datetime default NOW();
 
-/* data update */
-INSERT INTO /*prefix*/rights (id,description) VALUES (23,'project_review');
+/* cfield_testprojects */
+ALTER TABLE /*prefix*/cfield_testprojects  ADD COLUMN location tinyint NOT NULL DEFAULT '1';
 
-INSERT INTO /*prefix*/role_rights (role_id,right_id) VALUES (8,23);
+/* data update */
+INSERT INTO /*prefix*/rights (id,description) VALUES (24,'project_review');
+
+INSERT INTO /*prefix*/role_rights (role_id,right_id) VALUES (8,24);
 
 /* database version update */
 INSERT INTO /*prefix*/db_version (version,notes,upgrade_ts) VALUES('DB 1.3', 'TestLink 1.9',CURRENT_TIMESTAMP());

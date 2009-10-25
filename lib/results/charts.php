58af0b77dd9ff0fbcefda95eee5fae5f02295997
franciscom <>
2009-10-25 19:22:53 +0000

    start work on platform feature

--- lib/results/charts.php
+++ lib/results/charts.php
@@ -2,8 +2,8 @@
 /** TestLink Open Source Project - http://testlink.sourceforge.net/ 
  * 
  * @filesource $RCSfile: charts.php,v $
- * @version $Revision: 1.27 $
- * @modified $Date: 2009/10/05 08:47:11 $ by $Author: franciscom $
+ * @version $Revision: 1.28 $
+ * @modified $Date: 2009/10/25 19:22:53 $ by $Author: franciscom $
  * @author kevin
  *
  * Revisions:
@@ -21,12 +21,14 @@ $gui=new stdClass();
 
 $tplan_mgr = new testplan($db);
 $tproject_mgr = new testproject($db);
-$tplan_id=$_REQUEST['tplan_id'];
+$gui->tplan_id=$_REQUEST['tplan_id'];
 $tproject_id=$_SESSION['testprojectID'];
-$tplan_info = $tplan_mgr->get_by_id($tplan_id);
+$tplan_info = $tplan_mgr->get_by_id($gui->tplan_id);
 $tproject_info = $tproject_mgr->get_by_id($tproject_id);
 
+$tplan_mgr->getStatusTotalsByPlatform($gui->tplan_id);
 $gui->can_use_charts = checkLibGd();
+$totals = $tplan_mgr->getStatusTotals($gui->tplan_id);
 
 
 // $tplan_mgr->
@@ -43,24 +45,40 @@ if($gui->can_use_charts == 'OK')
     unset($_SESSION['statistics']);
     
     $re=new results($db, $tplan_mgr, $tproject_info, $tplan_info,ALL_TEST_SUITES,ALL_BUILDS);
-    $_SESSION['statistics']['getTotalsForPlan']=$re->getTotalsForPlan();
+    // $_SESSION['statistics']['getTotalsForPlan']=$re->getTotalsForPlan();
     $_SESSION['statistics']['getTopLevelSuites'] = $re->getTopLevelSuites();
     $_SESSION['statistics']['getAggregateMap'] = $re->getAggregateMap();
     $_SESSION['statistics']['getAggregateOwnerResults'] = $re->getAggregateOwnerResults();
     $_SESSION['statistics']['getAggregateKeywordResults']= $re->getAggregateKeywordResults();
+
+    // new dBug($_SESSION['statistics']);
+    
     
     $pathToScripts = "lib/results/";
     $chartsUrl=new stdClass();
-    $chartsUrl->overallPieChart = $pathToScripts . "overallPieChart.php";
+    $chartsUrl->overallPieChart = $pathToScripts . "overallPieChart.php?tplan_id={$gui->tplan_id}";
     $chartsUrl->keywordBarChart = $pathToScripts . "keywordBarChart.php";
     $chartsUrl->ownerBarChart = $pathToScripts . "ownerBarChart.php";
     $chartsUrl->topLevelSuitesBarChart = $pathToScripts . "topLevelSuitesBarChart.php";
     
-    $gui->charts = array(lang_get('overall_metrics') => $chartsUrl->overallPieChart,
-                         lang_get('results_by_keyword') => $chartsUrl->keywordBarChart,
+    $platformSet = $tplan_mgr->getPlatforms($gui->tplan_id);
+    $platformIDSet = is_null($platformSet) ? array(0) : array_keys($platformSet);
+
+    $gui->charts = array(lang_get('overall_metrics') => $chartsUrl->overallPieChart);
+    if(!is_null($platformSet))
+    {
+    	$label =lang_get('overall_metrics_for_platform');
+    	foreach($platformIDSet as $platform_id)
+    	{
+    	    $description = $label .  ' ' . $platformSet[$platform_id];
+    		$gui->charts[$description] = $pathToScripts . "platformPieChart.php?tplan_id={$gui->tplan_id}&platform_id={$platform_id}";
+    	}
+    }
+    
+    $gui->charts = array_merge( $gui->charts,
+                         array(lang_get('results_by_keyword') => $chartsUrl->keywordBarChart,
                          lang_get('results_by_tester') => $chartsUrl->ownerBarChart,
-                         lang_get('results_top_level_suites') => $chartsUrl->topLevelSuitesBarChart );
-           
+                         lang_get('results_top_level_suites') => $chartsUrl->topLevelSuitesBarChart) );
 }       
 
 $smarty = new TLSmarty();

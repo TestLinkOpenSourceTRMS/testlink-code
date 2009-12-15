455fa791e3b99b242d9da81ae07c75de4cd8783e
franciscom <>
2009-12-15 10:14:38 +0000

    BUGID 2999 - contribution - external links to TL items - work finished

--- linkto.php
+++ linkto.php
@@ -5,13 +5,38 @@
  *
  * Direct links to testlink items from the outside with frames for navigation and tree.
  *
+ * IMPORTANT - LIMITATIONS:
+ * User has to login before clicking the link!
+ * If user is not logged in he is redirected to login page. 
+ * After login main page is shown, Clicking the link again than it works!
+ *
+ * How this feature works:
+ * 
+ * - direct link to testcase KAOS-4 in test project KAOS:
+ * http://<testlink_home>/linkto.php?tprojectPrefix=KAOS&item=testcase&id=KAOS-4
+ * 
+ * - direct link to requirement REQ-002 in test project KAOS:
+ * http://<testlink_home>/linkto.php?tprojectPrefix=KAOS&item=req&id=REQ-002
+ *
+ * - direct link to requirement specification REQ-SPEC-AK89 in test project KAOS:
+ * http://<testlink_home>/linkto.php?tprojectPrefix=KAOS&item=reqspec&id=REQ-SPEC-AK89
+ * 
+ * Specials:
+ * - tree for requirement specification or test specification are expanded to the level of the item you created the link to
+ * - if a user has no right to view item he is redirected to main page
+ * - if item does not exist an errormessage shows
+ * 
+ * IMPORTANT - LIMITATIONS:
+ * User has to login before clicking the link!
+ * If user is not logged in he is redirected to login page. 
+ * After login main page is shown, Clicking the link again than it works!
+ * 
  *
  * @package 	TestLink
  * @author 		asimon
  * @copyright 	2007-2009, TestLink community 
- * @version    	CVS: $Id: linkto.php,v 1.1 2009/12/15 08:49:08 franciscom Exp $
+ * @version    	CVS: $Id: linkto.php,v 1.2 2009/12/15 10:14:38 franciscom Exp $
  * @link 		http://www.teamst.org/index.php
- *
  *
  * @internal Revisions:
  *	20091215 - franciscom - refactored
@@ -34,7 +59,6 @@ require_once('testproject.class.php');
 require_once('users.inc.php');
 testlinkInitPage($db, true);
 
-$tables = tlObjectWithDB::getDBTables(array('requirements','nodes_hierarchy', 'req_specs', 'tcversions'));
 $smarty = new TLSmarty();
 $smarty->assign('title', lang_get('main_page_title'));
 
@@ -62,7 +86,7 @@ else
 	
 	// First check for keys in _GET that MUST EXIST
 	// key: key on _GET, value: labelID defined on strings.txt
-    $mustKeys = array('tprojectPrefix' => 'testproject_not_set' , 
+    $mustKeys = array('tprojectPrefix' => 'testproject_not_set', 
                       'item' => 'item_not_set', 'id' => 'id_not_set');
                       
 	foreach($mustKeys as $key => $labelID)
@@ -94,6 +118,7 @@ else
      
 	if($op['status_ok'])
 	{
+		// Build  name of function to call for doing the job.
 		$pfn = 'process_' . $_GET['item'];
 		$jump_to = $pfn($db,$_GET['id'],$tproject_data['id']);
 		
@@ -146,9 +171,12 @@ function process_testcase(&$dbHandler,$externalID,$tprojectID)
  */
 function process_req(&$dbHandler,$docID,$tprojectID)
 {
+	$tables = tlObjectWithDB::getDBTables(array('requirements','nodes_hierarchy', 
+	                                            'req_specs', 'tcversions'));
+
 	$ret = array();
 	$ret['url'] = null;
-    $ret['msg'] = sprintf(lang_get('req_not_found'), $docID);
+    $ret['msg'] = sprintf(lang_get('req_not_found'), $docID,$tprojectID);
 
 	// get internal id from external id
 	$sql = " SELECT R.id, R.req_doc_id, NH.parent_id, RS.id as req_spec_id " .
@@ -156,17 +184,46 @@ function process_req(&$dbHandler,$docID,$tprojectID)
 	       " WHERE R.id=NH.id and NH.parent_id=RS.id " .
 	       " AND RS.testproject_id={$tprojectID} AND R.req_doc_id='{$docID}' ";
 	
-	$map = $dbHandler->fetchRowsIntoMap($sql, 'id');
-	if( count($map) > 0) 
+	$req = $dbHandler->fetchRowsIntoMap($sql, 'id');
+	if( count($req) > 0) 
 	{
 		// link to open in requirement frame
-		$ret['url'] = "lib/requirements/reqView.php?item=requirement&requirement_id={$map['id']}&";
+		$req = current($req);
+		$ret['url'] = "lib/requirements/reqView.php?item=requirement&requirement_id={$req['id']}&";
 
-        $cookie = buildCookie($dbHandler,$tcaseID,$tprojectID,'ys-requirement_spec');
+        $cookie = buildCookie($dbHandler,$req['id'],$tprojectID,'ys-requirement_spec');
 		setcookie($cookie['value'], $cookie['path'], TL_COOKIE_KEEPTIME, '/');
 	} 
 	return $ret;
 }
+
+
+
+/**
+ * process_reqspec
+ *
+ */
+function process_reqspec(&$dbHandler,$docID,$tprojectID)
+{
+	$ret = array();
+	$ret['url'] = null;
+    $ret['msg'] = sprintf(lang_get('req_spec_not_found'), $docID,$tprojectID);
+    
+    $reqspec_mgr = new requirement_spec_mgr($dbHandler);
+    $reqSpec = $reqspec_mgr->getByDocID($docID,$tprojectID);
+    
+    if( !is_null($reqSpec) )
+    {
+    	$reqSpec = current($reqSpec);
+    	$id = $reqSpec['id'];
+		$ret['url'] = "lib/requirements/reqSpecView.php?req_spec_id={$id}&";;
+
+        $cookie = buildCookie($dbHandler,$id,$tprojectID,'ys-requirement_spec');
+		setcookie($cookie['value'], $cookie['path'], TL_COOKIE_KEEPTIME, '/');
+    }
+	return $ret;
+}
+
 
 
 /**

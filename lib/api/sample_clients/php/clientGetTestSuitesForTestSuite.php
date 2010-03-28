cadafcef461b28ec15a09d04322c834ec76ddfd8
franciscom <>
2010-03-28 16:29:51 +0000

    new sample

---
+++ lib/api/sample_clients/php/clientGetTestSuitesForTestSuite.php
@@ -0,0 +1,85 @@
+<?php
+ /**
+ * TestLink Open Source Project - http://testlink.sourceforge.net/
+ * This script is distributed under the GNU General Public License 2 or later.
+ *
+ * Filename $RCSfile: clientGetTestSuitesForTestSuite.php,v $
+ *
+ * @version $Revision: 1.1 $
+ * @modified $Date: 2010/03/28 16:29:51 $ by $Author: franciscom $
+ * @Author: francisco.mancardi@gmail.com
+ *
+ * rev: 
+ */
+ 
+require_once 'util.php';
+require_once 'sample.inc.php';
+show_api_db_sample_msg();
+
+$method='getTestSuitesForTestSuite';
+$test_num=1;
+$unitTestDescription="Test {$test_num} - {$method}";
+
+$args=array();
+$args["devKey"]=DEV_KEY;
+$args["testsuiteid"]=-123;
+
+$additionalInfo='<br>Test suite ID is INEXISTENT<br>';
+$debug=true;
+echo $unitTestDescription;
+echo $additionalInfo;
+
+$client = new IXR_Client($server_url);
+$client->debug=$debug;
+
+runTest($client,$method,$args);
+// ---------------------------------------------------------------------------------
+
+// ---------------------------------------------------------------------------------
+$test_num++;
+$unitTestDescription="Test {$test_num} - {$method}";
+
+$args=array();
+$args["devKey"]=DEV_KEY;
+$args["testsuiteid"]=689;
+
+$additionalInfo='<br><br>';
+$debug=true;
+echo $unitTestDescription;
+echo $additionalInfo;
+echo 'arguments:<br>';
+foreach($args as $key => $value)
+{
+	echo $key . '=' . $value . '<br>';
+}
+
+$client = new IXR_Client($server_url);
+$client->debug=$debug;
+runTest($client,$method,$args);
+// ---------------------------------------------------------------------------------
+
+// ---------------------------------------------------------------------------------
+$test_num++;
+$unitTestDescription="Test {$test_num} - {$method}";
+
+$args=array();
+$args["devKey"]=DEV_KEY;
+$args["testsuiteid"]=123;
+
+$additionalInfo='<br>Test Suite HAS NO CHILDREN<br>';
+$debug=true;
+echo $unitTestDescription;
+echo $additionalInfo;
+echo 'arguments:<br>';
+foreach($args as $key => $value)
+{
+	echo $key . '=' . $value . '<br>';
+}
+
+$client = new IXR_Client($server_url);
+$client->debug=$debug;
+runTest($client,$method,$args);
+// ---------------------------------------------------------------------------------
+
+
+?>

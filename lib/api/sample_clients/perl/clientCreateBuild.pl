66982a92c7ae7bea487988e24e98dfaa07a4fd6b
franciscom <>
2010-05-12 21:09:53 +0000

    new samples -> perl

---
+++ lib/api/sample_clients/perl/clientCreateBuild.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl
+#
+# TestLink Open Source Project - http://testlink.sourceforge.net/
+# This script is distributed under the GNU General Public License 2 or later.
+#
+# Filename $RCSfile: clientCreateBuild.pl,v $
+#
+# ------------------------------------------------------------------------------
+# Sample Contributed by: Renato S de Araujo
+#
+# Tested on windows using strawberry-perl-5.10.1.2-portable (Francisco Mancardi)
+# After installing strawberry perl you need to do following steps:
+# 1. go to strawberry perl installation dir
+# 2. run portableshell.bat
+# 3. using cpan command install RPC XML:
+#    cpan RPC:XML:Client
+# 4. if something fails with this instructions, do some search on Internet
+# 5. after succesful installation: set in this script correct parameters
+#    testlink xmlrpc server location, tl items id, etc.
+#
+# 6. now run the script: 
+#    perl clientCreateBuild.pl
+#    
+# ------------------------------------------------------------------------------
+#
+#
+# @version $Revision: 1.1 $
+# @modified $Date: 2010/05/12 21:09:53 $ by $Author: franciscom $
+# @Author: Renato S de Araujo
+#
+# rev: 
+#
+#
+use RPC::XML::Client;
+my $devkey='CLIENTSAMPLEDEVKEY';
+my $testplanid=425;
+my $buildname='8.0.28';
+my $notes='Created by API';
+# my $cli = RPC::XML::Client->new('http://localhost:8900/head-20100501/lib/api/xmlrpc.php');
+my $cli = RPC::XML::Client->new('http://localhost:8600/testlink-1.9.beta4/lib/api/xmlrpc.php');
+
+my $build=$cli->send_request('tl.createBuild',
+                              {
+                               devKey=>$devkey,
+                               testplanid=>$testplanid,
+                               buildname=>$buildname,
+                               buildnotes=>$notes
+                              }
+                              );
+
+my @resp = @{$build->value};
+my $b  = $resp[0];
+
+foreach my $k (keys(%$b)){
+    print "key \'$k\' has value  \'$b->{$k}\'\n";
+}
+

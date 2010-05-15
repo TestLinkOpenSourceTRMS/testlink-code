ab3d8b57f671adeee21e3a8d275b693b5bddfa88
franciscom <>
2010-05-15 11:11:51 +0000

    sendMail() removed

--- lib/functions/info.inc.php
+++ lib/functions/info.inc.php
@@ -8,13 +8,12 @@
  * @package 	TestLink
  * @author 		Martin Havlat
  * @copyright 	2005-2009, TestLink community 
- * @version    	CVS: $Id: info.inc.php,v 1.8 2009/06/30 10:59:53 havlat Exp $
+ * @version    	CVS: $Id: info.inc.php,v 1.9 2010/05/15 11:11:51 franciscom Exp $
  * @link 		http://www.teamst.org/index.php
  * @uses 		common.php
  *
  * @internal Revisions:
  * 
- * 20070109 - KL - altered to allow for html reports if caller so chooses
  */
 
 /** @uses email_api.php */
@@ -35,42 +34,5 @@ function displayInfo($title, $message)
 	$smarty->display('workAreaSimple.tpl');
 
 	exit();
-}
-
-
-/**
- * Display simple info and exit
- *
- * @param string $from
- * @param string $to
- * @param string $title
- * @param string $message
- * @param string $cc (optional) yes = send a copy myself
- *
- * @return string Ok message.
- *
- * @internal Revisions:
- * 20051106 - fm - use of email_send()
- * 20050906 - fm - added from
- * @todo use email_send() directly - remove
- */
-function sendMail($from,$to, $title, $message, $send_cc_to_myself = false, $isHtmlFormat = false)
-{
-	$cc = '';
-	if ($send_cc_to_myself)
-	{
-		$cc = $from;
-	}
-	
-	$email_op = @email_send($from, $to, $title, $message, $cc, false, $isHtmlFormat);
-	
-	if ($email_op->status_ok)
-	{
-		return lang_get('email_sent_message');
-	}	
-	else
-	{
-		die("Error sending email" . $email_op->msg);
-	}	
 }
 ?>

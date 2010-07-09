f16d65b46dc22b824651e99c94cdb36257a7d458
mx-julian <>
2010-07-09 11:39:24 +0000

    changed TL_VERSION to Beta 6

--- cfg/const.inc.php
+++ cfg/const.inc.php
@@ -9,7 +9,7 @@
  * @package 	TestLink
  * @author 		Martin Havlat
  * @copyright 	2007-2009, TestLink community 
- * @version    	CVS: $Id: const.inc.php,v 1.150 2010/07/02 19:05:10 franciscom Exp $
+ * @version    	CVS: $Id: const.inc.php,v 1.151 2010/07/09 11:39:24 mx-julian Exp $
  * @see 		config.inc.php
  *
  * @internal 
@@ -21,7 +21,7 @@
 /* [GLOBAL SETTINGS] */
 
 /** TestLink Release version (MUST BE changed before the release day) */
-define('TL_VERSION', '1.9 (RC1 - Development)'); 
+define('TL_VERSION', '1.9 (Beta 6 - Development)'); 
 
 // needed to avoid problems in install scripts that do not include config.inc.php
 // want to point to root install dir, need to remove fixed part

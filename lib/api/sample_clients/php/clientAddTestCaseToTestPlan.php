4b0e0ef4d71cd6c3ffcbd8706256c18eebfbd895
franciscom <>
2010-07-11 17:30:25 +0000

    *** empty log message ***

--- lib/api/sample_clients/php/clientAddTestCaseToTestPlan.php
+++ lib/api/sample_clients/php/clientAddTestCaseToTestPlan.php
@@ -5,8 +5,8 @@
  *
  * Filename $RCSfile: clientAddTestCaseToTestPlan.php,v $
  *
- * @version $Revision: 1.4 $
- * @modified $Date: 2009/05/01 20:36:56 $ by $Author: franciscom $
+ * @version $Revision: 1.5 $
+ * @modified $Date: 2010/07/11 17:30:25 $ by $Author: franciscom $
  * @Author: francisco.mancardi@gmail.com
  *
  * rev: 
@@ -16,7 +16,8 @@ require_once 'sample.inc.php';
 show_api_db_sample_msg();
 
 $method="addTestCaseToTestPlan";
-$unitTestDescription="Test - {$method}";
+
+$unitTestDescription="Test - {$method} - Test Plan WITHOUT Platforms";
 
 $args=array();
 $args["devKey"]=DEV_KEY;
@@ -29,5 +30,36 @@ $args["testplanid"]=3;
 $debug=true;
 $client = new IXR_Client($server_url);
 $client->debug=$debug;
-runTest($client,$method,$args);
+$answer = runTest($client,$method,$args);
+// ---------------------------------------------------------------------------------------
+
+// ---------------------------------------------------------------------------------------
+$unitTestDescription="Test - {$method} - Test Plan WITH Platforms";
+
+$args=array();
+$args["devKey"]=DEV_KEY;
+$args["testprojectid"]=1;
+$args["testcaseexternalid"]='P1-10'; // P1-6,4
+$args["version"]=2;
+$args["testplanid"]=207;
+$args["platformid"]=5;
+
+$debug=true;
+$client = new IXR_Client($server_url);
+$client->debug=$debug;
+$answer = runTest($client,$method,$args,2);
+
+
+// [ID: 1 ] MAC OS		
+// [ID: 2 ] Solaris 10		
+// [ID: 3 ] Solaris 8		
+// [ID: 4 ] Solaris 9		
+// [ID: 5 ] Windows 2008		
+// [ID: 6 ] Windows 7
+
+// [ID: 213 ] <xml project>		 XML			
+// [ID: 214 ] Italian chars é à ò		 ITA			
+// [ID: 1 ] P1	  sasas	 P1			
+// [ID: 206 ] PROJECT_FOR_U1
+
 ?>

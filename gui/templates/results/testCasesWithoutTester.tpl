7f1d98db8cb6f1b227efd6595e5cf15344d56fa3
erikeloff <>
2010-08-23 16:30:35 +0000

    Test cases without tester: improve report - use ext table and show info on platform and prio

--- gui/templates/results/testCasesWithoutTester.tpl
+++ gui/templates/results/testCasesWithoutTester.tpl
@@ -1,6 +1,6 @@
 {* 
 TestLink Open Source Project - http://testlink.sourceforge.net/ 
-$Id: testCasesWithoutTester.tpl,v 1.1 2008/12/27 18:31:17 franciscom Exp $
+$Id: testCasesWithoutTester.tpl,v 1.2 2010/08/23 16:30:35 erikeloff Exp $
 
 Purpose: For a test plan, list test cases that has no tester assigned
 
@@ -20,30 +20,22 @@ rev: 20081227 - franciscom - BUGID
           arg_tproject_name=$gui->tproject_name arg_tplan_name=$gui->tplan_name}	
 
 {if $gui->warning_msg == ''}
-    {if $gui->resultSet}
-        <table class="simple">
-        {foreach from=$gui->resultSet item=tcase}
-            {assign var="tcase_id" value=$tcase.tc_id}
-            {assign var="tcversion_id" value=$tcase.tcversion_id}
-           <tr bgcolor="{cycle values="#eeeeee,#d0d0d0"}">       
-            <td>
-        	      {foreach from=$gui->path_info[$tcase_id] item=path_part}
-        	          {$path_part|escape} /
-        	      {/foreach}
-        	  <a href="lib/testcases/archiveData.php?edit=testcase&id={$tcase_id}">
-        	  {$gui->tcasePrefix}{$tcase.external_id|escape}:{$tcase.name|escape}</a>
-            </td>
-        	  </tr>
-        {/foreach}
-        </table>
+	{if $gui->tableSet}
+		{$gui->tableSet[0]->renderCommonGlobals()}
+		{if $gui->tableSet[0] instanceof tlExtTable}
+			{include file="inc_ext_js.tpl" bResetEXTCss=1}
+			{include file="inc_ext_table.tpl"}
+		{/if}
+		{$gui->tableSet[0]->renderHeadSection()}
+		{$gui->tableSet[0]->renderBodySection()}
 
-      {$labels.generated_by_TestLink_on} {$smarty.now|date_format:$gsmarty_timestamp_format}
-    {else}
-    	<h2>{$labels.no_testcases_without_tester}</h2>
-    {/if}
+		{$labels.generated_by_TestLink_on} {$smarty.now|date_format:$gsmarty_timestamp_format}
+	{else}
+		<h2>{$labels.no_testcases_without_tester}</h2>
+	{/if}
 {else}
     {$gui->warning_msg}
-{/if}    
+{/if}
 </div>
 </body>
 </html>
--- lib/results/testCasesWithoutTester.php
+++ lib/results/testCasesWithoutTester.php
@@ -4,15 +4,19 @@
  * This script is distributed under the GNU General Public License 2 or later. 
  *  
  * @filesource $RCSfile: testCasesWithoutTester.php,v $
- * @version $Revision: 1.4 $
- * @modified $Date: 2009/08/17 08:01:15 $ by $Author: franciscom $
+ * @version $Revision: 1.5 $
+ * @modified $Date: 2010/08/23 16:30:35 $ by $Author: erikeloff $
  * @author Francisco Mancardi - francisco.mancardi@gmail.com
  * 
  * For a test plan, list test cases that has no tester assigned
+ *
+ * @internal Revisions:
+ * 20100823 - eloff - Improve report with ext table and information on platforms and prio
  * 
  */
 require_once("../../config.inc.php");
 require_once("common.php");
+require_once('exttable.class.php');
 testlinkInitPage($db,false,false,"checkRights");
 
 $templateCfg = templateConfiguration();
@@ -22,34 +26,58 @@ $args = init_args($tplan_mgr);
 $gui = new stdClass();
 $gui->pageTitle = lang_get('caption_testCasesWithoutTester');
 $gui->warning_msg = '';
-$gui->tcasePrefix = '';
-$gui->path_info = null;
-$gui->resultSet = null;
 $gui->tproject_name = $args->tproject_name;
 $gui->tplan_name = $args->tplan_name;
 
 $msg_key = 'no_linked_tcversions';
 if($tplan_mgr->count_testcases($args->tplan_id) > 0)
 {
-    $msg_key = 'all_testcases_have_tester';
-    $filters = array('assigned_to' => TL_USER_NOBODY);
-    // $testCaseSet = $tplan_mgr->get_linked_tcversions($args->tplan_id,null,0,null,TL_USER_NOBODY);
-    $testCaseSet = $tplan_mgr->get_linked_tcversions($args->tplan_id,$filters);
-    if(($gui->row_qty = count($testCaseSet)) > 0)
-    {
-        $msg_key = '';
-        $gui->pageTitle .= " - " . lang_get('match_count') . ":" . $gui->row_qty;
-        $tree_mgr = new tree($db);   
-        $tproject_mgr = new testproject($db);   
-    
-      	$tcase_cfg = config_get('testcase_cfg');
-        $tcase_set = array_keys($testCaseSet);
-        $gui->path_info = $tree_mgr->get_full_path_verbose($tcase_set);
-    
-        $gui->tcasePrefix = $tproject_mgr->getTestCasePrefix($args->tproject_id);
-        $gui->tcasePrefix .= $tcase_cfg->glue_character;
-    	$gui->resultSet = $testCaseSet;
-    }
+	$msg_key = 'all_testcases_have_tester';
+	$filters = array('assigned_to' => TL_USER_NOBODY);
+	$options = array('output' => 'array');
+	$testCaseSet = $tplan_mgr->get_linked_tcversions($args->tplan_id,
+		$filters, $options);
+
+	if(($gui->row_qty = count($testCaseSet)) > 0)
+	{
+		$msg_key = '';
+		$gui->pageTitle .= " - " . lang_get('match_count') . ":" . $gui->row_qty;
+		$tree_mgr = new tree($db);
+		$tproject_mgr = new testproject($db);
+
+		// Collect all tc_id:s and get all test suite paths
+		$tcase_set = array();
+		foreach ($testCaseSet as $item) {
+			$tcase_set[] = $item['tc_id'];
+		}
+		$path_info = $tree_mgr->get_full_path_verbose($tcase_set);
+
+		$prefix = $tproject_mgr->getTestCasePrefix($args->tproject_id);
+
+		$data = array();
+		foreach ($testCaseSet as $item)
+		{
+			$verbosePath = join(" / ", $path_info[$item['tc_id']]);
+			$name = buildExternalIdString($prefix,
+				$item['external_id'] . ': ' . $item['name']);
+			$link = '<a href="lib/testcases/archiveData.php?edit=testcase&id=' .
+				$item['tc_id'] . '">' . $name . '</a>';
+
+			$row = array(
+				$verbosePath,
+				$link,
+				$item['platform_name'],
+			);
+
+			if($_SESSION['testprojectOptions']->testPriorityEnabled)
+			{
+				$row[] = $tplan_mgr->urgencyImportanceToPriorityLevel($item['priority']);
+			}
+			$data[] = $row;
+		}
+
+		$gui->tableSet[] = buildTable($data, $args);
+	}
 }
 
 $gui->warning_msg = lang_get($msg_key);
@@ -57,6 +85,21 @@ $smarty = new TLSmarty();
 $smarty->assign('gui',$gui);
 $smarty->display($templateCfg->template_dir . $templateCfg->default_template);
 
+
+function buildTable($data, &$args, $options) {
+	$columns = array(
+		lang_get('testsuite'),
+		lang_get('testcase'),
+		lang_get('platform'),
+		array('title' => lang_get('priority'), 'type' => 'priority'),
+	);
+	$matrix = new tlExtTable($columns, $data);
+	if($_SESSION['testprojectOptions']->testPriorityEnabled)
+	{
+		$matrix->addCustomBehaviour('priority', array('render' => 'priorityRenderer'));
+	}
+	return $matrix;
+}
 /*
   function: 
 

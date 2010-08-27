06a2c61a1357b7719f7e8394f8d0a135b5892dd4
franciscom <>
2010-08-27 20:06:49 +0000

    refactoring - removed unused variables, created new functions

--- lib/results/testCasesWithCF.php
+++ lib/results/testCasesWithCF.php
@@ -4,13 +4,14 @@
  * This script is distributed under the GNU General Public License 2 or later.
  *
  * @filesource $RCSfile: testCasesWithCF.php,v $
- * @version $Revision: 1.18 $
- * @modified $Date: 2010/08/27 07:59:39 $ by $Author: mx-julian $
+ * @version $Revision: 1.19 $
+ * @modified $Date: 2010/08/27 20:06:49 $ by $Author: franciscom $
  * @author Amit Khullar - amkhullar@gmail.com
  *
  * For a test plan, list test cases with Execution Custom Field Data
  *
  * @internal Revisions:
+ *	20100827 - franciscom - refactoring - removed unused variables
  *  20100827 - Julian - only show test case if at least one custom field has a value
  *	20100825 - eloff - add platform_name in table
  *	                   add test suite column
@@ -25,161 +26,54 @@ require_once("common.php");
 require_once('exttable.class.php');
 testlinkInitPage($db,false,false,"checkRights");
 
-$cfield_mgr = new cfield_mgr($db);
 $templateCfg = templateConfiguration();
 
-$tplan_mgr = new testplan($db);
 $tcase_mgr = new testcase($db);
-$args = init_args($tplan_mgr);
-
-$gui = new stdClass();
-$gui->pageTitle = lang_get('caption_testCasesWithCF');
-$gui->warning_msg = '';
-$gui->tcasePrefix = '';
-$gui->path_info = null;
-$gui->resultSet = null;
-$gui->tproject_name = $args->tproject_name;
-$gui->tplan_name = $args->tplan_name;
-$gui->tplan_id = $args->tplan_id;
-$show_platforms = !is_null($tplan_mgr->getPlatforms($args->tplan_id));
-$testCaseSet = array();
-$msg_key = 'no_linked_tc_cf';
-if($tplan_mgr->count_testcases($args->tplan_id) > 0)
+$args = init_args($db);
+$gui = initializeGui($db,$args);
+if( $args->doIt )
 {
-    $resultsCfg = config_get('results');
-    $tcase_cfg = config_get('testcase_cfg');
-
-    // -----------------------------------------------------------------------------------
-    // Get the mapping for the Verbose Status Description of Test Case Status
-    $map_tc_status_verbose_code = $resultsCfg['code_status'];
-    $map_tc_status_verbose_label = $resultsCfg['status_label'];
-    foreach($map_tc_status_verbose_code as $code => $verbose)
-    {
-        if(isset($map_tc_status_verbose_label[$verbose]))
-        {
-            $label = $map_tc_status_verbose_label[$verbose];
-            $gui->status_code_labels[$code] = lang_get($label);
-        }
-    }
-    // -----------------------------------------------------------------------------------
-    $gui->code_status = $resultsCfg['code_status'];
-    $tproject_mgr = new testproject($db);
-    $gui->tcasePrefix = $tproject_mgr->getTestCasePrefix($args->tproject_id);
-
-    // Get the custom fields linked/enabled on execution to a test project
-    // This will be used on report to give name to header of columns that hold custom field value
-    $gui->cfields = $cfield_mgr->get_linked_cfields_at_execution($args->tproject_id,1,'testcase',
-                                                                 null,null,null,'name');
-    if(!is_null($gui->cfields))
-    {
-        foreach($gui->cfields as $key => $values)
-        {
-           $cf_place_holder['cfields'][$key]='';
-        }
-    }
-   	// Now get exeutions with custom field values
-    $cf_map = $cfield_mgr->get_linked_cfields_at_execution($args->tproject_id,1,'testcase',
-                                                           null,null,$args->tplan_id,'exec_id');
-     
-    // need to transform in structure that allow easy display
-    // Every row is an execution with exec data plus a column that contains following map:
-    // 'cfields' => CFNAME1 => value
-    //              CFNAME2 => value
-    $result = array();
-    if(!is_null($cf_map))
-    {
-        foreach($cf_map as $exec_id => $exec_info)
-        {
-            // Get common exec info and remove useless keys
-            $result[$exec_id]=$exec_info[0];
-            unset($result[$exec_id]['name']);
-            unset($result[$exec_id]['label']);
-            unset($result[$exec_id]['display_order']);
-            unset($result[$exec_id]['id']);
-            unset($result[$exec_id]['value']);
-
-            // Collect custom fields values
-            $result[$exec_id] += $cf_place_holder;
-            foreach($exec_info as $cfield_data)
-            {
-                $result[$exec_id]['cfields'][$cfield_data['name']]=$cfield_data['value'];
-            }
-        }
-    }
-
-    if(($gui->row_qty=count($cf_map)) > 0 )
-    {
-        $msg_key = '';
-        $gui->pageTitle .= " - " . lang_get('match_count') . ":" . $gui->row_qty;
-        $gui->resultSet=$result;
-    }
-
+   	// Get executions with custom field values
+   	buildResultSet($db,$gui,$args->tproject_id,$args->tplan_id);
 
 	// Create column headers
-	$columns = array(
-		array('title' => lang_get('test_suite'), 'width' => 80 ),
-		array('title' => lang_get('test_case'), 'width' => 80),
-		array('title' => lang_get('version'), 'width' => 20),
-	);
-	if ($show_platforms)
-	{
-		$columns[] = array('title' => lang_get('platform'), 'width' => 40);
-	}
-	array_push($columns,
-		array('title' => lang_get('build'), 'width' => 35),
-		array('title' => lang_get('th_owner'), 'width' => 60),
-		array('title' => lang_get('date'), 'width' => 60),
-		array('title' => lang_get('status'), 'type' => 'status', 'width' => 30));
-
-
-	foreach ($gui->cfields as $cfield)
-	{
-		//if custom field is time for computing execution time do not waste space
-		if($cfield['name'] == 'CF_EXEC_TIME') {
-			$columns[] = array('title' => $cfield['label'], 'width' => 20);
-		} else {
-			$columns[] = array('title' => $cfield['label'], 'type' => 'text' );
-		}
-	}
-	
-	
+	$columns = getColumnsDefinition($args->showPlatforms,$gui->cfields);
 
 	// Extract the relevant data and build a matrix
 	$matrixData = array();
-	foreach ($result as $arrData)
+	foreach ($gui->resultSet as $item)
 	{
 		$rowData = array();
 
 		// Get test suite path
-		$dummy = $tcase_mgr->getPathLayered(array($arrData['tcase_id']));
+		$dummy = $tcase_mgr->getPathLayered(array($item['tcase_id']));
 		$dummy = end($dummy);
 		$rowData[] = $dummy['value'];
 
-		$rowData[] = '<a href="lib/testcases/archiveData.php?edit=testcase&id=' . $arrData['tcase_id'] . '">' .
-			buildExternalIdString($gui->tcasePrefix, $arrData['tc_external_id']) .
-			' : ' . $arrData['tcase_name'] . '</a>';
-		$rowData[] = $arrData['tcversion_number'];
-		if ($show_platforms)
+		$rowData[] = '<a href="lib/testcases/archiveData.php?edit=testcase&id=' . $item['tcase_id'] . '">' .
+					 buildExternalIdString($gui->tcasePrefix, $item['tc_external_id']) .
+					 ' : ' . $item['tcase_name'] . '</a>';
+		$rowData[] = $item['tcversion_number'];
+		if ($args->showPlatforms)
 		{
-			$rowData[] = $arrData['platform_name'];
+			$rowData[] = $item['platform_name'];
 		}
-		$rowData[] = $arrData['build_name'];
-		$rowData[] = $arrData['tester'];
+		$rowData[] = $item['build_name'];
+		$rowData[] = $item['tester'];
 
+		// use html comment to be able to sort table by timestamp and not by link
+		// only link is visible in table but comment is used for sorting
 		$dummy = null;
-		//use html comment to be able to sort table by timestamp and not by link
-		//only link is visible in table but comment is used for sorting
-		$rowData[] =
-			"<!--{$arrData['execution_ts']}--><a href=\"lib/execute/execSetResults.php?" .
-			"level=testcase&build_id={$arrData['builds_id']}&id={$arrData['tcase_id']}" .
-			"&version_id={$arrData['tcversion_id']}&tplan_id={$gui->tplan_id}\">" .
-			localize_dateOrTimeStamp(null, $dummy, 'timestamp_format', $arrData['execution_ts']) .
-			'</a>';
+		$rowData[] = "<!--{$item['execution_ts']}--><a href=\"lib/execute/execSetResults.php?" .
+					 "level=testcase&build_id={$item['builds_id']}&id={$item['tcase_id']}" .
+					 "&version_id={$item['tcversion_id']}&tplan_id={$gui->tplan_id}\">" .
+					 localize_dateOrTimeStamp(null, $dummy, 'timestamp_format', $item['execution_ts']) . '</a>';
+			
 		// let the renderer localize status
-		$rowData[] = $arrData['exec_status'];
+		$rowData[] = $item['exec_status'];
 		
 		$hasValue = false;
-		foreach ($arrData['cfields'] as $cf_value)
+		foreach ($item['cfields'] as $cf_value)
 		{
 			$rowData[] = $cf_value;
 			if ($cf_value) {
@@ -190,18 +84,17 @@ if($tplan_mgr->count_testcases($args->tplan_id) > 0)
 			$matrixData[] = $rowData;
 		}
 	}
-	// create unique table id for each project
+
+	// create unique table id for each test plan
 	$table_id = 'tl_'.$args->tplan_id.'_table_tc_with_cf';
 	$table = new tlExtTable($columns, $matrixData, $table_id);
 	$table->addCustomBehaviour('status', array('render' => 'statusRenderer'));
 	$table->addCustomBehaviour('text', array('render' => 'columnWrap'));
 	
-	//group by build, sort by date descending
 	$table->setGroupByColumnName(lang_get('build'));
 	$table->setSortByColumnName(lang_get('date'));
 	$table->sortDirection = 'DESC';
 	
-	//define toolbar
 	$table->showToolbar = true;
 	$table->toolbarExpandCollapseGroupsButton = true;
 	$table->toolbarShowAllColumnsButton = true;
@@ -221,27 +114,157 @@ $smarty->display($templateCfg->template_dir . $templateCfg->default_template);
  returns:
 
  */
-function init_args(&$tplan_mgr)
+function init_args(&$dbHandler)
 {
-    $args = new stdClass();
+    $argsObj = new stdClass();
+	$argsObj->doIt = false;
+    $argsObj->showPlatforms = false;
+    $argsObj->tproject_id = isset($_SESSION['testprojectID']) ? $_SESSION['testprojectID'] : 0;
+    $argsObj->tproject_name = isset($_SESSION['testprojectName']) ? $_SESSION['testprojectName'] : '';
 
-    $args->tproject_id = isset($_SESSION['testprojectID']) ? $_SESSION['testprojectID'] : 0;
-    $args->tproject_name = isset($_SESSION['testprojectName']) ? $_SESSION['testprojectName'] : '';
-
-    $args->tplan_id = isset($_REQUEST['tplan_id']) ? $_REQUEST['tplan_id'] : 0;
-    $args->tplan_name = '';
-    if($args->tplan_id == 0)
+    $argsObj->tplan_name = '';
+    $argsObj->tplan_id = isset($_REQUEST['tplan_id']) ? $_REQUEST['tplan_id'] : 0;
+    if($argsObj->tplan_id == 0)
     {
-        $args->tplan_id = isset($_SESSION['testplanID']) ? $_SESSION['testplanID'] : 0;
+        $argsObj->tplan_id = isset($_SESSION['testplanID']) ? $_SESSION['testplanID'] : 0;
     }
 
-    if($args->tplan_id > 0)
+    if($argsObj->tplan_id > 0)
     {
-        $tplan_info = $tplan_mgr->get_by_id($args->tplan_id);
-        $args->tplan_name = $tplan_info['name'];
+    	$tplan_mgr = new testplan($dbHandler);
+        $tplan_info = $tplan_mgr->get_by_id($argsObj->tplan_id);
+        $argsObj->tplan_name = $tplan_info['name'];
+
+		$argsObj->doIt = $tplan_mgr->count_testcases($argsObj->tplan_id) > 0;
+		$argsObj->showPlatforms = $tplan_mgr->hasLinkedPlatforms($argsObj->tplan_id);
+		unset($tplan_mgr);
     }
 
-    return $args;
+    return $argsObj;
+}
+
+
+
+function initializeGui(&$dbHandler,&$argsObj)
+{
+	$guiObj = new stdClass();
+	$guiObj->pageTitle = lang_get('caption_testCasesWithCF');
+	$guiObj->warning_msg = '';
+	$guiObj->tcasePrefix = '';
+	$guiObj->path_info = null;
+	$guiObj->resultSet = null;
+	$guiObj->tproject_name = $argsObj->tproject_name;
+	$guiObj->tplan_name = $argsObj->tplan_name;
+	$guiObj->tplan_id = $argsObj->tplan_id;
+
+    $tproject_mgr = new testproject($dbHandler);
+    $guiObj->tcasePrefix = $tproject_mgr->getTestCasePrefix($argsObj->tproject_id);
+	unset($tproject_mgr);
+
+    // Get the mapping for the Verbose Status Description of Test Case Status
+    $resultsCfg = config_get('results');
+    $gui->code_status = $resultsCfg['code_status'];
+    foreach($gui->code_status as $code => $verbose)
+    {
+        if(isset($resultsCfg['status_label'][$verbose]))
+        {
+            $gui->status_code_labels[$code] = lang_get($resultsCfg['status_label'][$verbose]);
+        }
+    }
+
+
+	return $guiObj; 
+}
+
+
+function buildResultSet(&$dbHandler,&$guiObj,$tproject_id,$tplan_id)
+{
+	
+	$cfieldMgr = new cfield_mgr($dbHandler);
+
+    // Get the custom fields linked/enabled on execution to a test project
+    // This will be used on report to give name to header of columns that hold custom field value
+    $guiObj->cfields = $cfieldMgr->get_linked_cfields_at_execution($tproject_id,1,'testcase',
+                                                                 null,null,null,'name');
+    if(!is_null($guiObj->cfields))
+    {
+        foreach($guiObj->cfields as $key => $values)
+        {
+           $cf_place_holder['cfields'][$key]='';
+        }
+    }
+
+    $cf_map = $cfieldMgr->get_linked_cfields_at_execution($tproject_id,1,'testcase',
+                                                          null,null,$tplan_id,'exec_id');
+     
+    // need to transform in structure that allow easy display
+    // Every row is an execution with exec data plus a column that contains following map:
+    // 'cfields' => CFNAME1 => value
+    //              CFNAME2 => value
+    if(!is_null($cf_map))
+    {
+        foreach($cf_map as $exec_id => $exec_info)
+        {
+            // Get common exec info and remove useless keys
+            $guiObj->resultSet[$exec_id] = $exec_info[0];
+            unset($guiObj->resultSet[$exec_id]['name']);
+            unset($guiObj->resultSet[$exec_id]['label']);
+            unset($guiObj->resultSet[$exec_id]['display_order']);
+            unset($guiObj->resultSet[$exec_id]['id']);
+            unset($guiObj->resultSet[$exec_id]['value']);
+
+            // Collect custom fields values
+            $guiObj->resultSet[$exec_id] += $cf_place_holder;
+            foreach($exec_info as $cfield_data)
+            {
+                $guiObj->resultSet[$exec_id]['cfields'][$cfield_data['name']]=$cfield_data['value'];
+            }
+        }
+    }
+
+    if(($guiObj->row_qty=count($cf_map)) > 0 )
+    {
+        $guiObj->pageTitle .= " - " . lang_get('match_count') . ":" . $guiObj->row_qty;
+    }
+
+}
+
+
+/**
+ * get Columns definition for table to display
+ *
+ */
+function getColumnsDefinition($showPlatforms,$customFields)
+{
+
+	$colDef = array(array('title' => lang_get('test_suite'), 'width' => 80 ),
+					array('title' => lang_get('test_case'), 'width' => 80),
+					array('title' => lang_get('version'), 'width' => 20));
+		
+	if ($showPlatforms)
+	{
+		$colDef[] = array('title' => lang_get('platform'), 'width' => 40);
+	}
+	array_push( $colDef,
+				array('title' => lang_get('build'), 'width' => 35),
+				array('title' => lang_get('th_owner'), 'width' => 60),
+				array('title' => lang_get('date'), 'width' => 60),
+				array('title' => lang_get('status'), 'type' => 'status', 'width' => 30));
+
+
+	foreach ($customFields as $cfield)
+	{
+		// if custom field is time for computing execution time do not waste space
+		$dummy = array('title' => $cfield['label']);
+		if($cfield['name'] == 'CF_EXEC_TIME') {
+			$dummy['width'] = 20;
+		} else {
+			$dummy['type'] = 'text';
+		}
+		$colDef[] = $dummy;
+	}
+
+	return $colDef;
 }
 
 

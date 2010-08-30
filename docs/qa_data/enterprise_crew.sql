ea7d405c3241d99b691410f21b502f8f129b1eab
franciscom <>
2010-08-30 09:14:08 +0000

    simple sql to create a bunch of users that can be useful when testing/evaluating TL

---
+++ docs/qa_data/enterprise_crew.sql
@@ -0,0 +1,32 @@
+--- Password for all users: 'star trek'
+INSERT INTO tl_hh_users(
+            "login", "password", role_id, email, "first", "last", locale, 
+             active, script_key)
+    VALUES ('kirk','9651cbc7c0b5fb1a81f2858a07813c82',8,
+            'james.tiberius.kirk@roddenberry.com','James Tiberius','Kirk','en_GB',1,'');
+
+INSERT INTO tl_hh_users(
+            "login", "password", role_id, email, "first", "last", locale, 
+             active, script_key)
+    VALUES ('spock','9651cbc7c0b5fb1a81f2858a07813c82',9,
+            'mr.spock@roddenberry.com','Mr','Spock','it_IT',,1,'');
+
+INSERT INTO tl_hh_users(
+            "login", "password", role_id, email, "first", "last", locale, 
+             active, script_key)
+    VALUES ('maccoy','9651cbc7c0b5fb1a81f2858a07813c82',6,
+            'leonard.mccoy@Roddenberry.com','Leonard H','McCoy','en_GB',,1,'');
+
+
+INSERT INTO tl_hh_users(
+            "login", "password", role_id, email, "first", "last", locale,active, script_key)
+    VALUES ('chekov','9651cbc7c0b5fb1a81f2858a07813c82',6,
+            'pavel.chekov@roddenberry.com','Pavel','Chekov','en_GB',1,'');
+ 
+INSERT INTO tl_hh_users(
+            "login", "password", role_id, email, "first", "last", locale,active, script_key)
+    VALUES ('scott','9651cbc7c0b5fb1a81f2858a07813c82',8,
+            'lt.commander.scott.chekov@roddenberry.com','Lt. Commander','Scott','en_GB',1,'');
+
+
+ 

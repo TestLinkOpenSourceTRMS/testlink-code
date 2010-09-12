517c10ad2054f34c48cf8dea18e1969e7848e7a1
franciscom <>
2010-09-12 17:12:01 +0000

    *** empty log message ***

--- docs/qa_data/enterprise_crew.sql
+++ docs/qa_data/enterprise_crew.sql
@@ -1,19 +1,16 @@
 --- Password for all users: 'star trek'
 INSERT INTO users(
-            "login", "password", role_id, email, "first", "last", locale, 
-             active, script_key)
+            "login", "password", role_id, email, "first", "last", locale,active, script_key)
     VALUES ('kirk','9651cbc7c0b5fb1a81f2858a07813c82',8,
             'james.tiberius.kirk@roddenberry.com','James Tiberius','Kirk','en_GB',1,'DEVKEY-kirk');
 
 INSERT INTO users(
-            "login", "password", role_id, email, "first", "last", locale, 
-             active, script_key)
+            "login", "password", role_id, email, "first", "last", locale,active, script_key)
     VALUES ('spock','9651cbc7c0b5fb1a81f2858a07813c82',9,
             'mr.spock@roddenberry.com','Mr','Spock','it_IT',1,'DEVKEY-spock');
 
 INSERT INTO users(
-            "login", "password", role_id, email, "first", "last", locale, 
-             active, script_key)
+            "login", "password", role_id, email, "first", "last", locale,active, script_key)
     VALUES ('maccoy','9651cbc7c0b5fb1a81f2858a07813c82',6,
             'leonard.mccoy@Roddenberry.com','Leonard H','McCoy','en_GB',1,'DEVKEY-mccoy');
 

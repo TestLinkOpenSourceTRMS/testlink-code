8ba5330aa15ff2024e4a2a8442599a39eca7e557
franciscom <>
2010-09-12 14:19:52 +0000

    *** empty log message ***

--- docs/qa_data/enterprise_crew.sql
+++ docs/qa_data/enterprise_crew.sql
@@ -1,32 +1,32 @@
 --- Password for all users: 'star trek'
-INSERT INTO tl_hh_users(
+INSERT INTO users(
             "login", "password", role_id, email, "first", "last", locale, 
              active, script_key)
     VALUES ('kirk','9651cbc7c0b5fb1a81f2858a07813c82',8,
-            'james.tiberius.kirk@roddenberry.com','James Tiberius','Kirk','en_GB',1,'');
+            'james.tiberius.kirk@roddenberry.com','James Tiberius','Kirk','en_GB',1,'DEVKEY-kirk');
 
-INSERT INTO tl_hh_users(
+INSERT INTO users(
             "login", "password", role_id, email, "first", "last", locale, 
              active, script_key)
     VALUES ('spock','9651cbc7c0b5fb1a81f2858a07813c82',9,
-            'mr.spock@roddenberry.com','Mr','Spock','it_IT',,1,'');
+            'mr.spock@roddenberry.com','Mr','Spock','it_IT',1,'DEVKEY-spock');
 
-INSERT INTO tl_hh_users(
+INSERT INTO users(
             "login", "password", role_id, email, "first", "last", locale, 
              active, script_key)
     VALUES ('maccoy','9651cbc7c0b5fb1a81f2858a07813c82',6,
-            'leonard.mccoy@Roddenberry.com','Leonard H','McCoy','en_GB',,1,'');
+            'leonard.mccoy@Roddenberry.com','Leonard H','McCoy','en_GB',1,'DEVKEY-mccoy');
 
 
-INSERT INTO tl_hh_users(
+INSERT INTO users(
             "login", "password", role_id, email, "first", "last", locale,active, script_key)
     VALUES ('chekov','9651cbc7c0b5fb1a81f2858a07813c82',6,
-            'pavel.chekov@roddenberry.com','Pavel','Chekov','en_GB',1,'');
+            'pavel.chekov@roddenberry.com','Pavel','Chekov','en_GB',1,'DEVKEY-chekov');
  
-INSERT INTO tl_hh_users(
+INSERT INTO users(
             "login", "password", role_id, email, "first", "last", locale,active, script_key)
     VALUES ('scott','9651cbc7c0b5fb1a81f2858a07813c82',8,
-            'lt.commander.scott.chekov@roddenberry.com','Lt. Commander','Scott','en_GB',1,'');
+            'lt.commander.scott.chekov@roddenberry.com','Lt. Commander','Scott','en_GB',1,'DEVKEY-scott');
 
 
  

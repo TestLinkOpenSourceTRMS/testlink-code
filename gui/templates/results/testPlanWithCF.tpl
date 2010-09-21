9fdee9d96e01557c8dd2fb1b2e22ed900127c3be
mx-julian <>
2010-09-21 13:44:32 +0000

    BUGID 3797 - use exttable

--- gui/templates/results/testPlanWithCF.tpl
+++ gui/templates/results/testPlanWithCF.tpl
@@ -1,58 +1,51 @@
 {* 
 TestLink Open Source Project - http://testlink.sourceforge.net/ 
-$Id: testPlanWithCF.tpl,v 1.4 2010/09/06 13:29:47 mx-julian Exp $
+$Id: testPlanWithCF.tpl,v 1.5 2010/09/21 13:44:32 mx-julian Exp $
 
 Purpose: For a test plan, list test cases with Custom Fields at Execution
 
-rev:  
+rev:
+  20100921 - Julian - BUGID 3797 - use exttable
 *}
 
 {lang_get var="labels" 
           s='no_uncovered_testcases,testproject_has_no_reqspec,
              testproject_has_no_requirements,no_linked_tplan_cf,generated_by_TestLink_on,
-             test_case,build,th_owner,date,status'}
+             test_case,build,th_owner,date,status,info_testPlanWithCF'}
 {include file="inc_head.tpl" openHead="yes"}
+{foreach from=$gui->tableSet key=idx item=matrix name="initializer"}
+  {assign var=tableID value=$matrix->tableID}
+  {if $smarty.foreach.initializer.first}
+    {$matrix->renderCommonGlobals()}
+    {if $matrix instanceof tlExtTable}
+        {include file="inc_ext_js.tpl" bResetEXTCss=1}
+        {include file="inc_ext_table.tpl"}
+    {/if}
+  {/if}
+  {$matrix->renderHeadSection()}
+{/foreach}
 </head>
 <body>
 <h1 class="title">{$gui->pageTitle|escape}</h1>
 <div class="workBack" style="overflow-y: auto;">
 
- {include file="inc_result_tproject_tplan.tpl" 
-          arg_tproject_name=$gui->tproject_name arg_tplan_name=$gui->tplan_name}	
-
-
-
 {if $gui->warning_msg == ''}
-    {if $gui->resultSet}
-        <table class="simple">
-	          <tr>
-	          <th> {$labels.test_case}</th>
-
-	          {foreach from=$gui->cfields item=cfield}
-	              <th>{$cfield.label|escape}</th>
-	          {/foreach}
-	          </tr>
-            
-	          {foreach from=$gui->resultSet item=arrData}
-	            <tr bgcolor="{cycle values="#eeeeee,#d0d0d0"}">  
-	            <td>	<a href="lib/testcases/archiveData.php?edit=testcase&id={$arrData.tcase_id}">
-	          	{$gui->tcasePrefix}{$arrData.tc_external_id|escape}:{$arrData.tcase_name|escape}</a>
-	            </td>
-	          	{foreach from=$arrData.cfields item=cfield_value}
-	          		<td> {$cfield_value}</td>
-	          	{/foreach}	
-	            </tr>
-	          {/foreach}
-                 
-        </table>
-
-      {$labels.generated_by_TestLink_on} {$smarty.now|date_format:$gsmarty_timestamp_format}
-    {else}
-    	<h2>{$labels.no_linked_tplan_cf}</h2>
-    {/if}
+    {include file="inc_result_tproject_tplan.tpl" 
+         arg_tproject_name=$gui->tproject_name arg_tplan_name=$gui->tplan_name}	
+    {foreach from=$gui->tableSet key=idx item=matrix}
+       {assign var=tableID value=table_$idx}
+       {$matrix->renderBodySection($tableID)}
+    {/foreach}
+    <br />
+    <p class="italic">{$labels.info_testPlanWithCF}</p>
+    <br />
+    {$labels.generated_by_TestLink_on} {$smarty.now|date_format:$gsmarty_timestamp_format}
 {else}
+    <br />
+    <div class="user_feedback">
     {$gui->warning_msg}
-{/if}    
+    </div>
+{/if} 
 </div>
 </body>
 </html>
--- lib/results/testPlanWithCF.php
+++ lib/results/testPlanWithCF.php
@@ -4,34 +4,41 @@
  * This script is distributed under the GNU General Public License 2 or later.
  *
  * @filesource $RCSfile: testPlanWithCF.php,v $
- * @version $Revision: 1.7 $
- * @modified $Date: 2009/09/07 06:51:12 $ by $Author: franciscom $
+ * @version $Revision: 1.8 $
+ * @modified $Date: 2010/09/21 13:44:32 $ by $Author: mx-julian $
  * @author Amit Khullar - amkhullar@gmail.com
  *
  * For a test plan, list associated Custom Field Data
  *
  * rev:
+ *      20100921 - Julian - BUGID 3797 - use exttable
  * 		20090504 - amitkhullar - BUGID 2465
  */
 require_once("../../config.inc.php");
 require_once("common.php");
+require_once('exttable.class.php');
 testlinkInitPage($db,false,false,"checkRights");
 
 $cfield_mgr = new cfield_mgr($db);
 $templateCfg = templateConfiguration();
+$tproject_mgr = new testproject($db);
 $tplan_mgr = new testplan($db);
+$tcase_mgr = new testcase($db);
 $args = init_args($tplan_mgr);
+$charset = config_get('charset');
+$glue_char = config_get('gui_title_separator_1');
 
 $gui = new stdClass();
 $gui->pageTitle = lang_get('caption_testPlanWithCF');
 $gui->warning_msg = '';
-$gui->tcasePrefix = '';
 $gui->path_info = null;
 $gui->resultSet = null;
 $gui->tproject_name = $args->tproject_name;
 $gui->tplan_name = $args->tplan_name;
+$gui->tcasePrefix = $tproject_mgr->getTestCasePrefix($args->tproject_id);
+
 $testCaseSet = array();
-$msg_key = 'no_linked_tc_cf';
+
 if($tplan_mgr->count_testcases($args->tplan_id) > 0)
 {
     $resultsCfg = config_get('results');
@@ -39,12 +46,12 @@ if($tplan_mgr->count_testcases($args->tplan_id) > 0)
 
     // -----------------------------------------------------------------------------------
     $gui->code_status = $resultsCfg['code_status'];
-    $tproject_mgr = new testproject($db);
 
     // Get the custom fields linked/enabled on execution to a test project
     // This will be used on report to give name to header of columns that hold custom field value
     $gui->cfields = $cfield_mgr->get_linked_cfields_at_testplan_design($args->tproject_id,1,'testcase',
                                                                        null,null,null,'name');
+                                                                       
     if(!is_null($gui->cfields))
     {
         foreach($gui->cfields as $key => $values)
@@ -76,15 +83,86 @@ if($tplan_mgr->count_testcases($args->tplan_id) > 0)
     }
     if(($gui->row_qty = count($cf_map)) > 0 )
     {
-        $msg_key = '';
-        $gui->pageTitle .= " - " . lang_get('match_count') . ":" . $gui->row_qty;
+        $gui->warning_msg = '';
         $gui->resultSet = $result;
-    }
+    } else {
+		$gui->warning_msg = lang_get('no_linked_tplan_cf');
+	}
 }
 
+$table = buildExtTable($gui,$tcase_mgr,$glue_char,$charset);
+
+if (!is_null($table)) {
+	$gui->tableSet[] = $table;
+}
 $smarty = new TLSmarty();
 $smarty->assign('gui',$gui);
 $smarty->display($templateCfg->template_dir . $templateCfg->default_template);
+
+/**
+ * 
+ *
+ */
+function buildExtTable($gui,$tcase_mgr,$gluechar,$charset)
+{
+	$table = null;
+	if(count($gui->resultSet) > 0) {
+		$columns = array();
+		$columns[] = array('title' => lang_get('test_suite'));
+		$columns[] = array('title' => lang_get('test_case'), 'width' => 80, 'type' => 'text');
+		
+		foreach ($gui->cfields as $cfield)
+		{
+			$dummy = array('title' => $cfield['label'], 'type' => 'text');
+			$columns[] = $dummy;
+		}
+	
+		// Extract the relevant data and build a matrix
+		$matrixData = array();
+		
+		foreach ($gui->resultSet as $item)
+		{
+			$rowData = array();
+	
+			// Get test suite path
+			$dummy = $tcase_mgr->getPathLayered(array($item['tcase_id']));
+			$dummy = end($dummy);
+			$rowData[] = $dummy['value'];
+	
+			$rowData[] = '<a href="lib/testcases/archiveData.php?edit=testcase&id=' . $item['tcase_id'] . '">' .
+						 buildExternalIdString($gui->tcasePrefix, $item['tc_external_id']) .
+						 $gluechar . $item['tcase_name'] . '</a>';
+			
+			$hasValue = false;
+
+			foreach ($item['cfields'] as $cf_value)
+			{
+				$rowData[] = preg_replace('!\s+!', ' ', htmlentities($cf_value, ENT_QUOTES, $charset));
+				if ($cf_value) {
+					$hasValue = true;
+				}
+			}
+			if ($hasValue) {
+				$matrixData[] = $rowData;
+			}
+		}
+	
+		// create unique table id for each test plan
+		$table_id = 'tl_table_tplan_with_cf';
+		$table = new tlExtTable($columns, $matrixData, $table_id);
+
+		$table->addCustomBehaviour('text', array('render' => 'columnWrap'));
+		
+		$table->setGroupByColumnName(lang_get('test_suite'));
+		$table->setSortByColumnName(lang_get('test_case'));
+		$table->sortDirection = 'ASC';
+		
+		$table->showToolbar = true;
+		$table->toolbarExpandCollapseGroupsButton = true;
+		$table->toolbarShowAllColumnsButton = true;
+	}
+	return($table);
+}
 
 /*
  function:

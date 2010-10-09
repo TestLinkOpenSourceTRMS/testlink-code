6deb642976d14692411fe9514d2c2f156b5f35a7
franciscom <>
2010-10-09 15:24:14 +0000

    added platform id on call to exportTestPlanDataToXML()

--- lib/plan/planExport.php
+++ lib/plan/planExport.php
@@ -16,7 +16,7 @@
  * @package 	TestLink
  * @author 		Francisco Mancardi
  * @copyright 	2003-2009, TestLink community 
- * @version    	CVS: $Id: planExport.php,v 1.6 2010/10/09 10:11:43 franciscom Exp $
+ * @version    	CVS: $Id: planExport.php,v 1.7 2010/10/09 15:24:14 franciscom Exp $
  * @link 		http://www.teamst.org/index.php
  * 
  * @internal Revisions:
@@ -45,7 +45,7 @@ if ($args->doExport)
 		
 		case 'tree':
 		// need to be developed
-		$content = $tplan_mgr->exportTestPlanDataToXML($args->tplan_id);
+		$content = $tplan_mgr->exportTestPlanDataToXML($args->tplan_id,$args->platform_id);
 		break;
 	}
 	downloadContentsToFile($content,$gui->export_filename);

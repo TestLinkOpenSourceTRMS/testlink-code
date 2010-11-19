c761afad1d20c878e22f2c10c00380431deadee4
franciscom <>
2010-11-19 19:44:27 +0000

    fixed missing set INDENTITY_* thanks to contributor

--- install/sql/alter_tables/1.9/mssql/DB.1.3/stepZ/z_final_step.sql
+++ install/sql/alter_tables/1.9/mssql/DB.1.3/stepZ/z_final_step.sql
@@ -1,14 +1,17 @@
 /* 
-$Revision: 1.2 $
-$Date: 2010/02/13 23:00:49 $
-$Author: havlat $
+$Revision: 1.3 $
+$Date: 2010/11/19 19:44:27 $
+$Author: franciscom $
 $Name:  $
-*/
 
+20101119 - franciscom - fixed missing set INDENTITY_* thanks to contributor
+*/
+set IDENTITY_INSERT rights on;
 INSERT INTO /*prefix*/rights (id,description) VALUES (24,'platform_management');
 INSERT INTO /*prefix*/rights (id,description) VALUES (25,'platform_view');
 INSERT INTO /*prefix*/rights (id,description) VALUES (26,'project_infrastructure_edit');
 INSERT INTO /*prefix*/rights (id,description) VALUES (27,'project_infrastructure_view');
+set IDENTITY_INSERT rights off;
 
 INSERT INTO /*prefix*/role_rights (role_id,right_id) VALUES (8,24);
 INSERT INTO /*prefix*/role_rights (role_id,right_id) VALUES (8,25);

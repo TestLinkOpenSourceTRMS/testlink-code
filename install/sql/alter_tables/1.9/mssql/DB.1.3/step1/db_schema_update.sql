92b8c11c9455c882683e7deccf47edacf0aa40a7
franciscom <>
2010-11-23 21:36:40 +0000

    fixed errors on builds ADD release_date

--- install/sql/alter_tables/1.9/mssql/DB.1.3/step1/db_schema_update.sql
+++ install/sql/alter_tables/1.9/mssql/DB.1.3/step1/db_schema_update.sql
@@ -1,5 +1,5 @@
--- $Revision: 1.2.2.1 $
--- $Date: 2010/11/19 19:39:23 $
+-- $Revision: 1.2.2.2 $
+-- $Date: 2010/11/23 21:36:40 $
 -- $Author: franciscom $
 -- $RCSfile: db_schema_update.sql,v $
 -- DB: MSSQL
@@ -11,6 +11,7 @@
 --
 --
 -- rev: 
+-- 20101123 - franciscom - fixed errors on builds ADD release_date 
 -- 20101119 - franciscom - bad default for date (now() -> getdate())
 -- 20100705 - asimon - added new column build_id to user_assignments
 --
@@ -30,7 +31,7 @@ ALTER TABLE /*prefix*/testplans ADD is_public tinyint NOT NULL DEFAULT '1';
 -- builds
 ALTER TABLE /*prefix*/builds ADD author_id INT NULL DEFAULT NULL;
 ALTER TABLE /*prefix*/builds ADD creation_ts datetime NOT NULL DEFAULT getdate();
-ALTER TABLE /*prefix*/builds ADD relase_date datetime NOT NULL;
+ALTER TABLE /*prefix*/builds ADD release_date datetime NULL;
 
 -- user_assignments
 ALTER TABLE /*prefix*/user_assignments ADD build_id INT NULL DEFAULT NULL;

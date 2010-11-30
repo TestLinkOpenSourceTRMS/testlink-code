bd09f7c325ba81503aa8004bec333613a15faf21
mx-julian <>
2010-11-30 08:24:33 +0000

    changed default toolbar to have the same elements that were available on testlink 1.9. changed skin to office2003 as it fits better

--- cfg/tl_ckeditor_config.js
+++ cfg/tl_ckeditor_config.js
@@ -1,19 +1,49 @@
+/*  
+TestLink Open Source Project - http://testlink.sourceforge.net/
+$Id: tl_ckeditor_config.js,v 1.2 2010/11/30 08:24:33 mx-julian Exp $
+
+Configure CKEditor
+See: http://docs.cksource.com/ for more information
+
+List of all config parameters that can be set here can be found on:
+http://docs.cksource.com/ckeditor_api/symbols/CKEDITOR.config.html
+*/
+
 CKEDITOR.editorConfig = function( config )
 {
-
-	config.filebrowserBrowseUrl = '/third_party/ckfinder/ckfinder.html';
-	config.filebrowserImageBrowseUrl = '/third_party/ckfinder/ckfinder.html?Type=Images';
-	config.filebrowserFlashBrowseUrl = '/third_party/ckfinder/ckfinder.html?Type=Flash';
-	config.filebrowserUploadUrl = '/third_party/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files';
-	config.filebrowserImageUploadUrl = '/third_party/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images';
-	config.filebrowserFlashUploadUrl = '/third_party/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash';
+	// You can use theses definitions if you buy ckfinder
+	// more informations on http://ckfinder.com/
 	
-	//kama
-	//office2003
-	//v2
-	config.skin = 'kama';
+	//config.filebrowserBrowseUrl = '/third_party/ckfinder/ckfinder.html';
+	//config.filebrowserImageBrowseUrl = '/third_party/ckfinder/ckfinder.html?Type=Images';
+	//config.filebrowserFlashBrowseUrl = '/third_party/ckfinder/ckfinder.html?Type=Flash';
+	//config.filebrowserUploadUrl = '/third_party/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files';
+	//config.filebrowserImageUploadUrl = '/third_party/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images';
+	//config.filebrowserFlashUploadUrl = '/third_party/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash';
 	
+	// choose your prefered ckedtior skin
+	// available skins: kama, office2003, v2
+	config.skin = 'office2003';
+	
+	// default Toolbar
 	config.toolbar_Testlink = 
+	[
+		['Source','Templates','SpellChecker','Find','Undo','Redo','-',
+		 'NumberedList','BulletedList','-',
+		 'JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-',
+		 'Outdent','Indent','-',
+		 'Table','HorizontalRule','-',
+		 'Link','Image','Anchor'],
+		 '/',
+		 ['Format','Bold','Italic','Underline','Strike','-',
+		  'Subscript','Superscript','TextColor','BGColor','RemoveFormat']
+	];
+	
+	// Toolbar with all available features - can be used as template for custom toolbars
+	// '-' creates toolbar seperator
+	// '/' creates a new toolbar "line"
+	// [...] defines sub-toolbars
+	config.toolbar_Full =
 	[
 		['Source','-','NewPage','Preview','-','Templates'],
 		['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print','SpellChecker'],
@@ -29,5 +59,4 @@ CKEDITOR.editorConfig = function( config )
 		['TextColor','BGColor'],
 		['Maximize','ShowBlocks','-','About']
 	];
-
 };

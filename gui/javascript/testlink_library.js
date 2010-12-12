2db70327dc7951e08a312cd195d2fd314e876514
franciscom <>
2010-12-12 10:11:10 +0000

    BUGID 4056: Requirement Revisioning

--- gui/javascript/testlink_library.js
+++ gui/javascript/testlink_library.js
@@ -1,7 +1,7 @@
 // TestLink Open Source Project - http://testlink.sourceforge.net/
 // This script is distributed under the GNU General Public License 2 or later.
 //
-// $Id: testlink_library.js,v 1.112.2.3 2010/11/19 16:47:47 asimon83 Exp $
+// $Id: testlink_library.js,v 1.112.2.4 2010/12/12 10:11:10 franciscom Exp $
 //
 // Javascript functions commonly used through the GUI
 // Rule: DO NOT ADD FUNCTIONS FOR ONE USING
@@ -901,31 +901,31 @@ function openLinkedReqVersionWindow(req_id, req_version_id, anchor)
  */
 function openLinkedReqWindow(req_id, anchor)
 {
+	// 20101008 - asimon - BUGID 3311
+	var width = getCookie("ReqPopupWidth");
+	var height = getCookie("ReqPopupHeight");
+	var windowCfg='';
+	var feature_url = "lib/requirements/reqView.php";
+
+
 	if (anchor == null) {
 		anchor = '';
 	} else {
 		anchor = '#' + anchor;
 	}
 	
-	var windowCfg='';
-	var feature_url = "lib/requirements/reqView.php";
-	feature_url += "?showReqSpecTitle=1&requirement_id=" + req_id + anchor;
-
-	// 20101008 - asimon - BUGID 3311
-	var width = getCookie("ReqPopupWidth");
-	var height = getCookie("ReqPopupHeight");
-
 	if (width == null)
 	{
-		var width = "800";
+		width = "800";
 	}
 
 	if (height == null)
 	{
-		var height = "600";
+		height = "600";
 	}
 
-	var windowCfg = "width="+width+",height="+height+",resizable=yes,scrollbars=yes,dependent=yes";
+	feature_url += "?showReqSpecTitle=1&requirement_id=" + req_id + anchor;
+	windowCfg = "width="+width+",height="+height+",resizable=yes,scrollbars=yes,dependent=yes";
 	window.open(fRoot+feature_url,"Requirement",windowCfg);
 }
 
@@ -1406,5 +1406,44 @@ function scrollToShowMe(oid) {
 	obj = document.getElementById(oid);
 	obj.scrollIntoView();
 	obj.focus();
+}
+
+
+/**
+ * open a requirement in a popup window
+ * 
+ * @param req_id Requirement ID
+ * @param anchor string with anchor name
+ */
+function openReqRevisionWindow(item_id, anchor)
+{
+	var width = getCookie("ReqPopupWidth");
+	var height = getCookie("ReqPopupHeight");
+	var windowCfg='';
+	var feature_url = "lib/requirements/reqViewRevision.php";
+
+
+	if (anchor == null) {
+		anchor = '';
+	} else {
+		anchor = '#' + anchor;
+	}
+
+	if (width == null)
+	{
+		width = "800";
+	}
+
+	if (height == null)
+	{
+		height = "600";
+	}
+
+	feature_url += "?showReqSpecTitle=1&item_id=" + item_id + anchor;
+	windowCfg = "width="+width+",height="+height+",resizable=yes,scrollbars=yes,dependent=yes";
+
+  // Warning YOU CAN NOT HAVE spaces on windows title IE does not like it
+	windowTitle = " ";
+	window.open(fRoot+feature_url,"Requirement Revision",windowCfg);
 }
 

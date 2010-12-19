992e55351c218209b80de1e35822d445f2e2f8a1
franciscom <>
2010-12-19 14:55:10 +0000

    work to managed MSSQL better feedback

--- install/migration/migrate_18/migrate_18_to_19.php
+++ install/migration/migrate_18/migrate_18_to_19.php
@@ -15,10 +15,15 @@
  *  
  * Included on installNewDB.php
  *
- * $Id: migrate_18_to_19.php,v 1.11 2010/11/19 21:28:14 franciscom Exp $
+ * $Id: migrate_18_to_19.php,v 1.12 2010/12/19 14:55:10 franciscom Exp $
  * Author: franciscom
  * 
  * @internal rev:
+ *	20101219 - franciscom - BUGID 4040 - Check for MSSQL to skip some operations THAT HAVE
+ *										 to be done MANUALLY
+ *
+ *	20101218 - franciscom - BUGID 4040 - changed drop column strategy
+ *	20101212 - franciscom - BUGID 4040
  *	20101119 - franciscom - fixed DROP COLUMN syntax for SQL SERVER
  *	20100911 - franciscom - migrate_cfield_links()
  *  20100707 - asimon - req spec type set to "1" like done for requirements
@@ -42,12 +47,34 @@ define('DBVERSION4MIG', 'DB 1.2');
  */
 function migrate_18_to_19(&$dbHandler,$tableSet)
 {
+	// Need To Add Some Feedback
+	echo '<b><br>-------------------------------------<br>'; 
+	echo 'Data Migration Process STARTED<br>'; 
+	echo '-------------------------------------<br></b>'; 
+
     migrate_requirements($dbHandler,$tableSet);
     migrate_req_specs($dbHandler,$tableSet);
     migrate_testcases($dbHandler,$tableSet);
     migrate_project_options($dbHandler,$tableSet);
     migrate_user_assignments($dbHandler, $tableSet);
     migrate_cfield_links($dbHandler, $tableSet);
+
+
+	if( $dbHandler->dbType == 'mssql')
+	{
+		echo "<b><br>**********************************************************************************<br>";
+		echo "IMPORTANT NOTICE FOR MSSQL USERS<br>";
+		echo "**********************************************************************************<br>";
+		echo "Some updates to DB SCHEMA HAS TO BE DONE manually due to <br>";
+		echo "MSSQL Restrictions<br>";
+		echo "ALTER TABLE /*prefix*/requirements ALTER req_doc_id VARCHAR(64)<br>";
+		echo "ALTER TABLE /*prefix*/custom_fields ALTER COLUMN possible_values varchar(4000)<br>";
+		echo "ALTER TABLE /*prefix*/custom_fields ALTER COLUMN default_value varchar(4000)<br>";
+		echo "**********************************************************************************<br></b>";
+	}
+	echo '<br>-------------------------------------<br>'; 
+	echo 'Data Migration Process Finished<br>'; 
+	echo '-------------------------------------<br><br><br>'; 
 }
 
 
@@ -59,9 +86,10 @@ function migrate_requirements(&$dbHandler,$tableSet)
 	// do requirements exist?
 	$sql = "SELECT id FROM {$tableSet['requirements']}";
  	$reqSet = $dbHandler->get_recordset($sql);
-    
+    echo 'Step - Requirements Migration - STARTED <br> ';
 	if( !is_null($reqSet) && count($reqSet) > 0)
 	{
+		echo 'Working - Requirements Migration <br> ';
 		$tree_mgr = new tree($dbHandler);
 		$node_types_descr_id= $tree_mgr->get_available_node_types();
     	$node_types_id_descr=array_flip($node_types_descr_id);
@@ -96,8 +124,21 @@ function migrate_requirements(&$dbHandler,$tableSet)
         // STEP 3 - Remove fields from requirements
         $adodbObj = $dbHandler->get_dbmgr_object();
         $colNames = $adodbObj->MetaColumnNames($tableSet['requirements']);
+
+		$warning = "<b>{$tableSet['requirements']} - For MSSQL You NEED TO DROP " .  
+		           "THESE COLUMNS manually: status,node_order,creation_ts</b>";
+		if($dbHandler->dbType == 'mssql')
+		{
+        	$cols2drop = array("scope", "type", "author_id","modifier_id","modification_ts","title");
+		}
+		else
+		{
+			$warning = '';
         $cols2drop = array("scope", "status", "type", "author_id","creation_ts",
                            "modifier_id","modification_ts","node_order","title");
+		}
+
+
         $cols2drop = array_flip($cols2drop);
         foreach($cols2drop as $colname => $dummy)
         {
@@ -110,10 +151,19 @@ function migrate_requirements(&$dbHandler,$tableSet)
         		$cols2drop[$colname] = " DROP COLUMN $colname ";
         	}
         }
-        $drop_clause = implode(",", $cols2drop);
-        $sql = "ALTER TABLE {$tableSet['requirements']} {$drop_clause} ";
+        // $drop_clause = implode(",", $cols2drop);
+		foreach($cols2drop as $stm)
+		{
+			$sql = "ALTER TABLE {$tableSet['requirements']} {$stm} ";
         $dbHandler->exec_query($sql);
 	} 
+}
+	if( !is_null($warning) )
+	{
+		echo "$warning<br><br>";
+	}
+    echo 'Step - Requirements Migration - Finished !!! <br><br> ';
+
 }
 
 
@@ -122,6 +172,7 @@ function migrate_requirements(&$dbHandler,$tableSet)
  */
 function migrate_req_specs(&$dbHandler,$tableSet)
 {
+	echo 'Step - Requirements Specification Migration - STARTED<br> ';
 	// get all requirements in system
 	$sql = "SELECT * FROM {$tableSet['req_specs']}";
 	$rs = $dbHandler->get_recordset($sql);
@@ -130,6 +181,7 @@ function migrate_req_specs(&$dbHandler,$tableSet)
 	// Set req spec type to '1' as it was set to n/N on 1.8 but has never been used
 	if( !is_null($rs) && count($rs) > 0)
 	{
+		echo 'Working - Requirements Specification Migration<br> ';
 		$keyset = array_keys($rs);
 		foreach($keyset as $id)
 		{
@@ -155,9 +207,12 @@ function migrate_req_specs(&$dbHandler,$tableSet)
 			$cols2drop[$colname] = " DROP COLUMN $colname ";
 		}
 	}
-	$drop_clause = implode(",", $cols2drop);
-	$sql = "ALTER TABLE {$tableSet['req_specs']} {$drop_clause} ";
+	foreach($cols2drop as $stm)
+	{
+		$sql = "ALTER TABLE {$tableSet['req_specs']} {$stm} ";
 	$dbHandler->exec_query($sql);
+}
+    echo 'Step - Requirements Migration - Finished !!! <br><br> ';
 }
 
 
@@ -166,6 +221,7 @@ function migrate_req_specs(&$dbHandler,$tableSet)
  */
 function migrate_project_options(&$dbHandler,$tableSet)
 {
+	echo 'Step - Test Project Options Migration -  STARTED<br> ';
 	// get all projects in system
 	$sql = "SELECT * FROM {$tableSet['testprojects']}";
 	$rs = $dbHandler->get_recordset($sql);
@@ -173,6 +229,7 @@ function migrate_project_options(&$dbHandler,$tableSet)
 	// Set new parameter
 	if( !is_null($rs) && count($rs) > 0)
 	{
+		echo 'Working - Test Project Options Migration <br> ';
 		$keyset = array_keys($rs);
 		foreach($keyset as $id)
 		{
@@ -197,25 +254,28 @@ function migrate_project_options(&$dbHandler,$tableSet)
 	//
 	// To avoid complex code we will inform user that 
 	// he/she had to do this manually
-	$adodbObj = $dbHandler->get_dbmgr_object();
-	$colNames = $adodbObj->MetaColumnNames($tableSet['testprojects']);
-	$cols2drop = array('option_reqs','option_priority','option_automation');
-	$cols2drop = array_flip($cols2drop);
-	foreach($cols2drop as $colname => $dummy)
-	{
-		if( !isset($colNames[strtoupper($colname)]) )
-		{
-			unset($cols2drop[$colname]);
-		}
-		else
-		{
-			$cols2drop[$colname] = " DROP COLUMN $colname ";
-		}
-	}
-	echo "Please DROP Manually COLUMNS with it's constraints (I'm sorry )<b> ";
-	// $drop_clause = implode(",", $cols2drop);
-	// $sql = "ALTER TABLE {$tableSet['testprojects']} {$drop_clause} ";
-	// $dbHandler->exec_query($sql);
+	// $adodbObj = $dbHandler->get_dbmgr_object();
+	// $colNames = $adodbObj->MetaColumnNames($tableSet['testprojects']);
+	// $cols2drop = array('option_reqs','option_priority','option_automation');
+	// $cols2drop = array_flip($cols2drop);
+	// foreach($cols2drop as $colname => $dummy)
+	// {
+	// 	if( !isset($colNames[strtoupper($colname)]) )
+	// 	{
+	// 		unset($cols2drop[$colname]);
+	// 	}
+	// 	else
+	// 	{
+	// 		$cols2drop[$colname] = " DROP COLUMN $colname ";
+	// 	}
+	// }
+	echo "<b><br><br>";
+	echo "**********************************************************************************<br>";
+	echo "******* ATTENTION!!!! *** <br> {$tableSet['testprojects']} <br> ";
+	echo "Please DROP Manually COLUMNS with it's constraints (I'm sorry )<br> ";
+	echo "'option_reqs','option_priority','option_automation'<br> ";
+	echo "**********************************************************************************<br></b>";
+    echo 'Step - Test Project Options Migration - Finished !!! <br><br> ';
 }
 
 
@@ -283,14 +343,18 @@ function migrate_testcases(&$dbHandler,$tableSet)
 //   "execution_type" INT2 NOT NULL DEFAULT '1',
 //   PRIMARY KEY ("id")
 // ); 
-	echo __FUNCTION__;
 	
 	// do test cases exist?
-	$sql = "SELECT id FROM {$tableSet['tcversions']}";
+	$sql = " SELECT TCV.id,NHTC.name AS name FROM {$tableSet['tcversions']} TCV " .
+		   " JOIN {$tableSet['nodes_hierarchy']} NHV ON NHV.id=TCV.id " .
+		   " JOIN {$tableSet['nodes_hierarchy']} NHTC ON NHTC.id=NHV.parent_id ";
+	
+	echo 'Step - Test Case Migration - STARTED <br> ';
  	$itemSet = $dbHandler->get_recordset($sql);
 
 	if( !is_null($itemSet) && count($itemSet) > 0)
 	{
+		echo 'Working - Test Case Migration <br> ';
 		$tree_mgr = new tree($dbHandler);
 		$node_types_descr_id= $tree_mgr->get_available_node_types();
     	$node_types_id_descr=array_flip($node_types_descr_id);
@@ -314,17 +378,31 @@ function migrate_testcases(&$dbHandler,$tableSet)
         // STEP 2 - Create nodes for tcsteps on nodes_hierarchy table
 	    foreach($itemSet as $dummy => $item_info)
 	    {
+        	echo "Test Case:" . $item_info['name'] . " Version DBID: {$item_info['id']} <br>";
+	    	
 	    	$item_id = $tree_mgr->new_node($item_info['id'],$node_types_descr_id['testcase_step']);
+	    	if( $item_id > 0 )
+	    	{
 	    	$sql = " UPDATE {$tableSet['tcsteps']} " .
 	    	       " SET id = {$item_id} WHERE id={$item_info['id']}";
             $dbHandler->exec_query($sql);
 	    }
+            else
+            {
+            	echo "<b> **** <br> CRITIC ***** <br>";
+            	echo "MIGRATION FAILURE - Unable to create Test Case Step NODE on nodes hierarchy <br></b>";
+            }
+	    }
 
         // STEP 3 - Remove fields from tcversions
-        $sql = "ALTER TABLE {$tableSet['tcversions']} " .
-               "DROP COLUMN steps, DROP COLUMN expected_results ";
+        $sql = "ALTER TABLE {$tableSet['tcversions']} DROP COLUMN steps";
         $dbHandler->exec_query($sql);
+
+        $sql = "ALTER TABLE {$tableSet['tcversions']} DROP COLUMN expected_results ";
+        $dbHandler->exec_query($sql);
+
 	} 
+	echo 'Step - Test Case Migration - Finished !! <br><br> ';
 }
 
 
@@ -340,6 +418,10 @@ function migrate_testcases(&$dbHandler,$tableSet)
  */
 function migrate_user_assignments(&$dbHandler, $tableSet) {
 	//$starttime = microtime(true);
+
+	echo 'Step - User Execution Assignment Migration - STARTED <br> ';
+	echo 'Working - User Execution Assignment Migration <br> ';
+
 	$testplan_mgr = new testplan($dbHandler);
 	
 	// get assignment type for execution
@@ -362,6 +444,8 @@ function migrate_user_assignments(&$dbHandler, $tableSet) {
 	// In $testplan_builds, we then have an array consisting of testplan_id => max_build_id
 	// If no build for a test plan is found, its assignments will not be changed (build_id=0).
 	$testplan_builds = array();
+	if( !is_null($testplans) )
+	{
 	foreach ($testplans as $key => $tp_id) {
 		// first we try to get an active build
 		$max_build_id = $testplan_mgr->get_max_build_id($tp_id, testplan::GET_ACTIVE_BUILD);
@@ -380,12 +464,31 @@ function migrate_user_assignments(&$dbHandler, $tableSet) {
 		$subquery = " SELECT id as feature_id FROM {$tp_tcv} " .
 		            " WHERE testplan_id = {$testplan_id} ";
 
-		$sql = " UPDATE {$ua} UA " .
-		       " SET UA.build_id = {$build_id} " .
-		       " WHERE UA.feature_id IN($subquery) " .
-		       " AND (UA.type={$execution} OR UA.type IS NULL) ";
+		// This update with ALIAS is not LIKED by Postgres
+		//
+		// OK
+		// UPDATE user_assignments UA  
+		// SET build_id = 2  WHERE UA.feature_id IN 
+		// ( SELECT id AS feature_id FROM testplan_tcversions  WHERE testplan_id = 70)
+		//
+		// if used UA.build_id -> problems on Postgre 8.4
+		// Message:
+		// ERROR:  column "ua" of relation "user_assignments" does not exist
+		// LINE 2: SET UA.build_id = 2  WHERE UA.feature_id IN              
+		
+		// $sql = " UPDATE {$ua} UA " .
+		//        " SET UA.build_id = {$build_id} " .
+		//        " WHERE UA.feature_id IN($subquery) " .
+		//        " AND (UA.type={$execution} OR UA.type IS NULL) ";
+		// 
+		// 20101212 - franciscom		
+		$sql = " UPDATE {$ua} " .
+		       " SET build_id = {$build_id} " .
+		       " WHERE feature_id IN($subquery) " .
+		       " AND (type={$execution} OR type IS NULL) ";
 		
 		$dbHandler->exec_query($sql);
+	}
 	}
 	
 	// delete objects
@@ -394,6 +497,7 @@ function migrate_user_assignments(&$dbHandler, $tableSet) {
 	// check how long the function is running on huge databases...
 	//$endtime = microtime(true) - $starttime;
 	//echo "<br/>migrate_user_assignments() needed $endtime seconds to finish<br/>";
+	echo 'Step - User Execution Assignment Migration - Finished <br><br> ';
 }
 
 /**
@@ -410,7 +514,6 @@ function migrate_cfield_links(&$dbHandler, $tableSet)
 {
 	$treeMgr = new tree($dbHandler);
 	$nodeTypes = $treeMgr->get_available_node_types();
-	echo $nodesTypes['testcase'];
 	unset($treeMgr);
 	
 	$sql = " SELECT CFDV.*, NHITEM.node_type_id, NHVERSION.id AS version_node_id" .
@@ -421,25 +524,39 @@ function migrate_cfield_links(&$dbHandler, $tableSet)
 	
 	$workingSet = $dbHandler->get_recordset($sql);
 	
-	echo "Records to process: count($workingSet)<br>";
+	echo 'Step - Custom Fields (DESIGN TIME) Migration - STARTED <br> ';
 	if( !is_null($workingSet) )
 	{
+		echo "Working - Custom Fields Migration - Records to process:" .  count($workingSet) . "<br>";
 		foreach($workingSet as $target)
 		{
-			$values[] = "( {$target['field_id']}, {$target['version_node_id']}, '{$target['value']}' )";
+			// $values[] = "( {$target['field_id']}, {$target['version_node_id']}, '{$target['value']}' )";
 			$victims[$target['node_id']] = $target['node_type_id'];
+			
+			// Ay!, I've forgot to escape target value
+			$sql = " INSERT INTO {$tableSet['cfield_design_values']} (field_id,node_id,value) VALUES " . 
+			       "( {$target['field_id']}, {$target['version_node_id']}," .
+			       "'" . $dbHandler->prepare_string($target['value']) . "' )";
+			$dbHandler->exec_query($sql);
 		}
 	
-		$sql = " INSERT INTO {$tableSet['cfield_design_values']} (field_id,node_id,value) VALUES ";
-		$vSet = implode(',',$values);
+		// depending amount of data on db this can create an statement with too many characters
+		// and can fail.
+		// change to slower but safer strategy: one by one insert
+		// $sql = " INSERT INTO {$tableSet['cfield_design_values']} (field_id,node_id,value) VALUES ";
+		// $vSet = implode(',',$values);
+		// $sql .= $vSet;
+		// $dbHandler->exec_query($sql);
 		
-		$sql .= $vSet;
-		$dbHandler->exec_query($sql);
+		
+		
 		foreach($victims as $node_id => $node_type_id)
 		{
 			$sql = " DELETE FROM {$tableSet['cfield_design_values']} WHERE node_id = $node_id "; 
 		    $dbHandler->exec_query($sql);
 		}
 	}
+	echo 'Step - Custom Fields (DESIGN TIME) Migration - Finished !! <br><br> ';
+
 }
 ?>

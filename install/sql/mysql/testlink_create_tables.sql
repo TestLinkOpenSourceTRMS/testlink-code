06bb0a9be611295c96341e94f76a9ca3201a3510
fman <francisco.mancardi@gmail.com>
2011-03-23 07:47:38 +0100

    BUGID 4025: option to avoid that obsolete test cases can be added to new test plans. fixed sql create tables for MySQL

--- install/sql/mysql/testlink_create_tables.sql
+++ install/sql/mysql/testlink_create_tables.sql
@@ -1,7 +1,7 @@
 # TestLink Open Source Project - http://testlink.sourceforge.net/
 # This script is distributed under the GNU General Public License 2 or later.
 # ---------------------------------------------------------------------------------------
-# $Id: testlink_create_tables.sql,v 1.83 2010/12/19 17:47:19 franciscom Exp $
+# @filesource	testlink_create_tables.sql
 #
 # SQL script - create all DB tables for MySQL
 # tables are in alphabetic order  
@@ -50,80 +50,6 @@
 # 20100113 - franciscom - doc_id increased to 64 and setted NOT NULL
 # 20100106 - franciscom - Test Case Step feature
 # 
-# 20091228 - franciscom - changes to requirements table and new table req_versions
-#                         to implement requirement versioning
-#                         req_doc_id and doc_id => changed to NOT NULL
-#                         
-# 20091221 - havlatm - infrastructure table added.
-#                      tcversions.layout added 
-#                      testproject.options added
-# 20091220 - franciscom - fields removed form req_spec and requirements "title"
-# 20091119 - franciscom - requirements table - new field expected_coverage
-# 20091119 - franciscom - req_specs added doc_id field
-# 20090919 - franciscom - custom field values increased to 4000
-# 20090910 - franciscom - added milestones.start_date
-# 20090831 - franciscom - added preconditions
-# 20090806 - franciscom - added testplan_platforms,platforms,platform_id to tables
-# 20090717 - franciscom - added cfield_testprojects.location field
-# 20090512 - franciscom - BUGID - builds release_date
-#                         BUGID - is_public attribute for testprojects and testplans
-# 20090411 - franciscom - BUGID 2369 - testplan_tcversions
-# 20090103 - franciscom - changed case of unique fields in UPPER CASE (milestones table A,B,C)
-# 20090103 - franciscom - milestones table - added new unique index
-# 20081018 - franciscom - renamed indexes on events table according to dev standards
-# 20080810 - franciscom - BUGID 1650 (REQ)
-#                         custom_fields.show_on_testplan_design
-#                         custom_fields.enable_on_testplan_design
-#                         new table cfield_testplan_design_values 
-# 20080720 - franciscom - fixed bug on text_templates definition
-# 20080703 - franciscom - removed MyISAM on create table
-# 20080701 - havlatm - redefine test prioritization fields
-# 20080628 - franciscom - create_ts -> creation_ts
-# 20080528 - franciscom - BUGID 1504 - added executions.tcversion_number
-# 20080331 - franciscom - testplan_tcversions added node_order
-# 20080226 - franciscom - removed autoincrement id on req_spec, requirements
-# 20080119 - franciscom - testprojects.option_automation
-#	20080117 - schlundus - added table for events and transactions
-# 20080117 - franciscom - prefix size increased (16)
-# 20080114 - franciscom - usergroup_id -> id
-#	20080114 - mht - changes for priorities (add 2 + delete 1 table)
-#			 add table for templates
-#			 add table for usergroups
-# 20080112 - franciscom - tcversions.tc_external_id,testprojects.prefix
-#                         testprojects.tc_counter
-# 20080102 - franciscom - added changes for API feature (DB 1.2)
-#                         added notes fields on db_version
-# 20071202 - franciscom - added tcversions.execution_type
-# 20071010 - franciscom - open -> is_open due to MSSQL reserved word problem
-# 20070519 - franciscom - milestones table date -> target_date, because
-#                         date is reserved word for Oracle
-# 20070414 - franciscom - table requirements: added field node_order 
-# 20070204 - franciscom - changes in tables priorities, risk_assignments 
-# 20070131 - franciscom - requirements -> req_doc_id(32), 
-# 20070120 - franciscom - following BUGID 458 ( really a new feature request)
-#                         two new fields on builds table: active, open
-# 20070113 - franciscom - table cfield_testprojects added fields
-#                         required_on_design,required_on_execution
-# 20070106 - franciscom - again, and again  'en_GB' as default NOT en_US
-# 20061228 - franciscom - added field active on table cfield_testprojects
-# 20061224 - franciscom - changes to custom field related tables
-# 20061220 - franciscom - added new indexes to solve performance problems
-#                         executions, user_assignment, testplan_tcversions
-#                         changed column order on index on testplan_tcversions
-# 20061009 - franciscom - changes to index names for rights and roles tables
-#                         added UNIQUE to req_doc_id KEY in table requirements
-# 20060908 - franciscom - changes to user_assignments
-#                         new tables assignment_types, assignment_status
-# 20060815 - franciscom - changes to user_assignments, risk_assignments, added object_keywords
-# 20060715 - schlundus - changes to milestones table.
-# 20060711 - franciscom - added index pid_m_nodeorder on nodes_hierarchy
-#						  to improve performance
-# 20060424 - franciscom - redoing asiel changes on users table due to wrong name
-# 20060312 - franciscom - changed bud_id column type to varchar(16) as requested by Asiel
-# 						  to avoid problems with JIRA bug tracking system.
-# 						  added name to nodes_hierarchy table to improve performance in
-# 						  tree operations changed some int(11) to int(10)
-#
 # ---------------------------------------------------------------------------------------
 
 
@@ -481,6 +407,7 @@ CREATE TABLE /*prefix*/tcversions (
   `active` tinyint(1) NOT NULL default '1',
   `is_open` tinyint(1) NOT NULL default '1',
   `execution_type` tinyint(1) NOT NULL default '1' COMMENT '1 -> manual, 2 -> automated',
+  `workflow_status` tinyint(1) NOT NULL default '1' COMMENT '1 -> draft, for more values see on const.inc.php',
   PRIMARY KEY  (`id`)
 ) DEFAULT CHARSET=utf8;
 

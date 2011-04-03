b742a0d467a82830acc22c8488b4d1244410b9e2
fman <francisco.mancardi@gmail.com>
2011-04-03 18:19:27 +0200

    new users example for QA

---
+++ docs/qa_data/motogp.sql
@@ -0,0 +1,58 @@
+--- Password for all users: 'motogp'
+INSERT INTO users(
+            "login", "password", role_id, email, "first", "last", locale,active, script_key)
+    VALUES ('Casey.STONER','b5315cc7893bd48e1c8bc0ce8505183c',8,
+            'Casey.STONER@formulaone.com','Casey','STONER','en_GB',1,'DEVKEY-STONER');
+
+INSERT INTO users(
+            "login", "password", role_id, email, "first", "last", locale,active, script_key)
+    VALUES ('Jorge.LORENZO','b5315cc7893bd48e1c8bc0ce8505183c',9,
+            'Jorge.LORENZO@formulaone.com','Jorge','LORENZO','it_IT',1,'DEVKEY-LORENZO');
+
+INSERT INTO users(
+            "login", "password", role_id, email, "first", "last", locale,active, script_key)
+    VALUES ('Dani.PEDROSA','b5315cc7893bd48e1c8bc0ce8505183c',6,
+            'Dani.PEDROSA@formulaone.com','Dani','PEDROSA','en_GB',1,'DEVKEY-PEDROSA');
+
+
+INSERT INTO users(
+            "login", "password", role_id, email, "first", "last", locale,active, script_key)
+    VALUES ('Andrea.DOVIZIOSO','b5315cc7893bd48e1c8bc0ce8505183c',6,
+            'Andrea.DOVIZIOSO@formulaone.com','Andrea','DOVIZIOSO','en_GB',1,'DEVKEY-DOVIZIOSO');
+ 
+INSERT INTO users(
+            "login", "password", role_id, email, "first", "last", locale,active, script_key)
+    VALUES ('Marco.SIMONCELLI','b5315cc7893bd48e1c8bc0ce8505183c',8,
+            'Marco.SIMONCELLI@formulaone.com','Marco','SIMONCELLI','en_GB',1,'DEVKEY-SIMONCELLI');
+
+
+INSERT INTO users(
+            "login", "password", role_id, email, "first", "last", locale,active, script_key)
+    VALUES ('Ben.SPIES','b5315cc7893bd48e1c8bc0ce8505183c',8,
+            'Ben.SPIES@formulaone.com','Ben','SPIES','en_GB',1,'DEVKEY-SPIES');
+
+
+INSERT INTO users(
+            "login", "password", role_id, email, "first", "last", locale,active, script_key)
+    VALUES ('Valentino.ROSSI','b5315cc7893bd48e1c8bc0ce8505183c',8,
+            'Valentino.ROSSI@formulaone.com','Valentino','ROSSI','en_GB',1,'DEVKEY-ROSSI');
+
+
+INSERT INTO users(
+            "login", "password", role_id, email, "first", "last", locale,active, script_key)
+    VALUES ('Colin.EDWARDS','b5315cc7893bd48e1c8bc0ce8505183c',8,
+            'Colin.EDWARDS@motogp.com','Colin','EDWARDS','en_GB',1,'DEVKEY-EDWARDS');
+
+
+INSERT INTO users(
+            "login", "password", role_id, email, "first", "last", locale,active, script_key)
+    VALUES ('Nicky.HAYDEN','b5315cc7893bd48e1c8bc0ce8505183c',8,
+            'Nicky.HAYDEN@motogp.com','Nicky','HAYDENmacher','en_GB',1,'DEVKEY-HAYDEN');
+
+
+INSERT INTO users(
+            "login", "password", role_id, email, "first", "last", locale,active, script_key)
+    VALUES ('Hiroshi.AOYAMA','b5315cc7893bd48e1c8bc0ce8505183c',8,
+            'Hiroshi.AOYAMA@motogp.com','Hiroshi','AOYAMA','en_GB',1,'DEVKEY-AOYAMA');
+
+                                  

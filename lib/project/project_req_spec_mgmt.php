749a9e98f310f4e7d930f0ecdd89bb803e0f50bf
fman <francisco.mancardi@gmail.com>
2011-04-25 19:40:47 +0200

    BUGID 4429: Code refactoring to remove global coupling as much as possible
    BUGID 4339: Working with two different projects within one Browser (same session) is not possible without heavy side-effects

--- lib/project/project_req_spec_mgmt.php
+++ lib/project/project_req_spec_mgmt.php
@@ -3,8 +3,8 @@
  * TestLink Open Source Project - http://testlink.sourceforge.net/
  * This script is distributed under the GNU General Public License 2 or later.
  *
- * @version $Id: project_req_spec_mgmt.php,v 1.6 2009/01/07 19:55:35 schlundus Exp $
- * @author Martin Havlat
+ * @filesource	project_req_spec_mgmt.php
+ * @author 		Martin Havlat
  *
  * Allows you to show test suites, test cases.
  * Normally launched from tree navigator.
@@ -12,19 +12,35 @@
  */
 require_once('../../config.inc.php');
 require_once('common.php');
-testlinkInitPage($db,false,false,"checkRights");
+testlinkInitPage($db,!TL_UPDATE_ENVIRONMENT,false,"checkRights");
 
-$tproject_id   = isset($_SESSION['testprojectID']) ? $_SESSION['testprojectID'] : 0;
-$tproject_name = isset($_SESSION['testprojectName']) ? $_SESSION['testprojectName'] : 'undefined';
 
+$args = init_args($db);
 $gui = new stdClass();
-$gui->main_descr = lang_get('testproject') .  TITLE_SEP . $tproject_name;
-$gui->tproject_id = $tproject_id;
+$gui->main_descr = lang_get('testproject') .  TITLE_SEP . $args->tproject_name;
+$gui->tproject_id = $args->tproject_id;
+$gui->tproject_name = $args->tproject_name;
 $gui->refresh_tree = 'no';
 
 $smarty = new TLSmarty();
 $smarty->assign('gui', $gui);
 $smarty->display('requirements/project_req_spec_mgmt.tpl');
+
+
+function init_args(&$dbHandler)
+{
+	$argsObj = new stdClass();
+	
+	$argsObj->tproject_name = '';
+	$argsObj->tproject_id   = isset($_REQUEST['tproject_id']) ? intval($_REQUEST['tproject_id']) : 0;
+	if( $argsObj->tproject_id > 0 )
+	{
+		$treeMgr = new tree($dbHandler);
+		$dummy = $treeMgr->get_node_hierarchy_info($argsObj->tproject_id);
+		$argsObj->tproject_name = $dummy['name'];
+	}
+	return $argsObj;
+}
 
 function checkRights(&$db,&$user)
 {

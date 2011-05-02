66c4d703f4e9471c956af7c1a3f66cbfd7c49be6
fman <francisco.mancardi@gmail.com>
2011-05-02 12:31:18 +0200

    BUGID 4429: Code refactoring to remove global coupling as much as possible
    BUGID 4339: Working with two different projects within one Browser (same session) is not possible without heavy side-effects

--- lib/cfields/cfieldsView.php
+++ lib/cfields/cfieldsView.php
@@ -57,8 +57,13 @@ function init_args(&$dbHandler)
  */
 function checkRights(&$db,&$userObj,$argsObj)
 {
-	$env['tproject_id'] = isset($argsObj->tproject_id) ? $argsObj->tproject_id : 0;
-	$env['tplan_id'] = isset($argsObj->tplan_id) ? $argsObj->tplan_id : 0;
+	// Custom fields are System Wide items, for this reason for this feature
+	// check must be done on Global Rights => those that belong to role assigned to user 
+	// when user was created (Global/Default Role) => enviroment is ignored.
+	// To instruct method to ignore enviromente, we need to set enviroment but with INEXISTENT ID 
+	// (best option is negative value)
+	$env['tproject_id'] = -1;
+	$env['tplan_id'] = -1;
 	checkSecurityClearance($db,$userObj,$env,array('cfield_view'),'and');
 }
 

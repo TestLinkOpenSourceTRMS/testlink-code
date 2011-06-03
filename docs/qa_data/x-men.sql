643865b5c49648136d3d571bd2404e46e893b922
fman <francisco.mancardi@gmail.com>
2011-06-03 12:59:27 +0200

    Sql sentences to allow insert of xmen as users ti be used for tests

---
+++ docs/qa_data/x-men.sql
@@ -0,0 +1,27 @@
+--- valid for TestLink 2.0
+--- Password for all users: www -> '4eae35f1b35977a00ebd8086c259d4c9'
+
+INSERT INTO users("login", "password", role_id, email, "first", "last", locale,active, script_key,cookie_string)
+VALUES ('Professor X','4eae35f1b35977a00ebd8086c259d4c9',8,'professor-x@xmen.org','Professor Charles Francis','Xavier',
+        'en_GB',1,'DEVKEY-Professor-X','6a28d0171447ec8d5c0320352cff6979f505920c222222380775b2c94bdea333');
+
+INSERT INTO users("login", "password", role_id, email, "first", "last", locale,active, script_key,cookie_string)
+VALUES ('Cyclops','4eae35f1b35977a00ebd8086c259d4c9',8,'cyclops@xmen.org','Scott','Summers','en_GB',1,
+        'DEVKEY-Cyclops','6a28d0171447ec8d5c0320352cff6979f505920c222222380775b2c94bdea6b8');
+
+INSERT INTO users("login", "password", role_id, email, "first", "last", locale,active, script_key,cookie_string)
+VALUES ('Iceman','4eae35f1b35977a00ebd8086c259d4c9',8,'iceman@xmen.org','Robert Louis','Drake','en_GB',1,
+        'DEVKEY-Iceman','6a28d0171447ec8d5c0320352cff6979f505920c222222380775b2c94bdea6b7');
+
+INSERT INTO users("login", "password", role_id, email, "first", "last", locale,active, script_key,cookie_string)
+VALUES ('Beast','4eae35f1b35977a00ebd8086c259d4c9',8,'beast@xmen.org','Dr. Henry Philip','McCoy','en_GB',1,
+        'DEVKEY-Beast','6a28d0171447ec8d5c0320352cff6979f505920c222222380775b2c94bdea456');
+
+INSERT INTO users("login", "password", role_id, email, "first", "last", locale,active, script_key,cookie_string)
+VALUES ('Marvel Girl/Phoenix','4eae35f1b35977a00ebd8086c259d4c9',8,'Jean.Grey-Summers@xmen.org','Jean','Grey-Summers','en_GB',1,
+        'DEVKEY-Phoenix','6a28d0171447ec8d5c0320352cff6979f505920c222222380775b2c94bbbb456');
+
+INSERT INTO users("login", "password", role_id, email, "first", "last", locale,active, script_key,cookie_string)
+VALUES ('Polaris','4eae35f1b35977a00ebd8086c259d4c9',8,'polaris@xmen.org','Lorna','Dane','en_GB',1,
+        'DEVKEY-Polaris','6a28d0171447ec8d5c0320352cff69888505920c222222380775b2c94bbbb456');
+

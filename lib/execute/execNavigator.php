f06f05b65f43cf8402d18ce4b9b0440f3872a04f
fman <francisco.mancardi@gmail.com>
2011-08-20 16:37:50 +0200

    old comments removed

--- lib/execute/execNavigator.php
+++ lib/execute/execNavigator.php
@@ -5,13 +5,15 @@
  *
  * link/unlink test cases to a test plan
  *
+ * @filesource	execNavigator.php
  * @package 	TestLink
- * @copyright 	2007-2009, TestLink community
- * @version    	CVS: $Id: execNavigator.php,v 1.122 2010/10/07 19:53:26 franciscom Exp $
- * @filesource	http://testlink.cvs.sourceforge.net/viewvc/testlink/testlink/lib/functions/object.class.php?view=markup
+ * @copyright 	2007-2011, TestLink community
  * @link 		http://www.teamst.org/index.php
  *
- * @internal Revisions:
+ * @internal revisions
+ * @since 1.9.4
+ *
+ * @since 1.9.3
  * 20101007 - franciscom - 	BUGID 3270 - Export Test Plan in XML Format
  * 20100628 - asimon - removal of constants from filter control class
  * 20100624 - asimon - CVS merge (experimental branch to HEAD)
@@ -25,18 +27,6 @@
  * 20100222 - asimon - fixes in initializeGui() for testplan select box when there are no builds
  * 20100217 - asimon - added check for open builds on initBuildInfo()
  * 20100202 - asimon - changed filtering, BUGID 2455, BUGID 3026
- * 20090828 - franciscom - added contribution platform feature
- * 20090828 - franciscom - BUGID 2296 - filter by Last Exec Result on Any of previous builds
- * 20081227 - franciscom - BUGID 1913 - filter by same results on ALL previous builds
- * 20081220 - franciscom - advanced/simple filters
- * 20081217 - franciscom - only users that have effective role with right
- *                         that allow test case execution are displayed on
- *                         filter by user combo.
- *
- * 20080517 - franciscom - fixed testcase filter bug
- * 20080428 - franciscom - keyword filter can be done on multiple keywords
- * 20080224 - franciscom - refactoring
- * 20080224 - franciscom - BUGID 1056
  *
  **/
 
@@ -49,6 +39,7 @@ require_once('exec.inc.php');
 testlinkInitPage($db);
 
 $templateCfg = templateConfiguration();
+
 
 $control = new tlTestCaseFilterControl($db, 'execution_mode');
 $gui = initializeGui($control);

16c17a34f4a25b11f66aae2e001f9c8153af7e89
fman <francisco.mancardi@gmail.com>
2011-12-26 12:48:02 +0100

    added new test cases to test - TICKET 4843: 'getTestCasesForTestPlan' - add support for new argument 'details'

--- lib/api/sample_clients/php/clientGetTestCasesForTestPlan.php
+++ lib/api/sample_clients/php/clientGetTestCasesForTestPlan.php
@@ -3,16 +3,10 @@
  * TestLink Open Source Project - http://testlink.sourceforge.net/
  * This script is distributed under the GNU General Public License 2 or later.
  *
- * Filename $RCSfile: clientGetTestCasesForTestPlan.php,v $
+ * @filesource	clientGetTestCasesForTestPlan.php
+ * @Author		francisco.mancardi@gmail.com
  *
- * @version $Revision: 1.5.2.1 $
- * @modified $Date: 2010/11/10 15:01:01 $ by $Author: franciscom $
- * @Author: francisco.mancardi@gmail.com
- *
- * rev: 
- *		20100715 - franciscom - new argument getstepinfo
  */
- 
 require_once 'util.php';
 require_once 'sample.inc.php';
 show_api_db_sample_msg();
@@ -21,12 +15,72 @@ $method='getTestCasesForTestPlan';
 $test_num=1;
 $unitTestDescription="Test {$test_num} - {$method}";
 
-$tplan_id = 3020;
+$tplan_id = 96315;
 $args=array();
 $args["devKey"]=DEV_KEY;
 $args["testplanid"]=$tplan_id;
 $args["executiontype"]=2;
 $additionalInfo='';
+
+$debug=true;
+echo $unitTestDescription;
+echo $additionalInfo;
+
+$client = new IXR_Client($server_url);
+$client->debug=$debug;
+
+$answer = runTest($client,$method,$args,$test_num);
+// ---------------------------------------------------------------------------------
+
+// ---------------------------------------------------------------------------------
+$test_num++;
+
+$args=array();
+$args["devKey"]=DEV_KEY;
+$args["testplanid"]=$tplan_id;
+$args["getstepsinfo"]=false;
+$args["details"]='simple';
+$additionalInfo = '$args["details"]: ->'  . $args["details"] . '<br>';
+
+$debug=true;
+echo $unitTestDescription;
+echo $additionalInfo;
+
+$client = new IXR_Client($server_url);
+$client->debug=$debug;
+
+$answer = runTest($client,$method,$args,$test_num);
+// ---------------------------------------------------------------------------------
+
+// ---------------------------------------------------------------------------------
+$test_num++;
+
+$args=array();
+$args["devKey"]=DEV_KEY;
+$args["testplanid"]=$tplan_id;
+$args["getstepsinfo"]=false;
+$args["details"]='full';
+$additionalInfo = '$args["details"]: ->'  . $args["details"] . '<br>';
+
+$debug=true;
+echo $unitTestDescription;
+echo $additionalInfo;
+
+$client = new IXR_Client($server_url);
+$client->debug=$debug;
+
+$answer = runTest($client,$method,$args,$test_num);
+// ---------------------------------------------------------------------------------
+
+// ---------------------------------------------------------------------------------
+$test_num++;
+
+$args=array();
+$args["devKey"]=DEV_KEY;
+$args["testplanid"]=$tplan_id;
+$args["getstepsinfo"]=false;
+$args["details"]='summary';
+$additionalInfo = '$args["details"]: ->'  . $args["details"] . '<br>';
 
 $debug=true;
 echo $unitTestDescription;

86a55db980d2e557b269fef9804922403f9cec22
fman <francisco.mancardi@gmail.com>
2012-04-14 19:28:53 +0200

    new view last_executions

--- lib/functions/object.class.php
+++ lib/functions/object.class.php
@@ -286,7 +286,8 @@ abstract class tlObject implements iSerialization
 
 	static public function getDBViews($itemNames = null)
 	{
-		$items = array('tcversions_last_active' => DB_TABLE_PREFIX . 'tcversions_last_active'); 
+		$items = array('tcversions_last_active' => DB_TABLE_PREFIX . 'tcversions_last_active',
+					   'last_executions' => DB_TABLE_PREFIX . 'last_executions'); 
 
 		if ($itemNames != null)
 		{	

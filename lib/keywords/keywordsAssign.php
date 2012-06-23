26e52c8b76cf1e0e08fff5413953853f7c5a4795
fman <francisco.mancardi@gmail.com>
2012-06-23 19:21:03 +0200

    TICKET 5074: Bulk Keyword assignment - All keywords are assigned to the test case instead of the selected keyword

--- lib/keywords/keywordsAssign.php
+++ lib/keywords/keywordsAssign.php
@@ -3,13 +3,15 @@
  * TestLink Open Source Project - http://testlink.sourceforge.net/ 
  * This script is distributed under the GNU General Public License 2 or later. 
  *
- * Filename $RCSfile: keywordsAssign.php,v $
- *
- * @version $Revision: 1.41 $
- * @modified $Date: 2009/05/13 19:30:18 $ $Author: schlundus $
- *
- * Purpose:  Assign keywords to set of testcases in tree structure
- *
+ * @filesource	keywordsAssign.php
+ * @package 	TestLink
+ * @copyright 	2007-2012, TestLink community 
+ * @link 		http://www.teamst.org/index.php
+ * 
+ * @internal revisions
+ * @since 1.9.4
+ * 20120623 - franciscom -	TICKET 5074: Bulk Keyword assignment - 
+ *							All keywords are assigned to the test case instead of the selected keyword
  *
 **/
 require_once("../../config.inc.php");
@@ -37,57 +39,67 @@ $tcase_mgr = new testcase($db);
 $result = null;
 $keyword_assignment_subtitle = null;
 $can_do = 0;
-$itemID = null;
+// $itemID = null;
 
+
+// Important Development Notice
+// option transfer do the magic on GUI, 
+// analizing content of from->map and to->map, is able to populate
+// each side as expected.
+//
 $opt_cfg->global_lbl = '';
 $opt_cfg->additional_global_lbl = null;
 $opt_cfg->from->lbl = lang_get('available_kword');
-$opt_cfg->to->lbl = lang_get('assigned_kword');
 $opt_cfg->from->map = $tproject_mgr->get_keywords_map($args->testproject_id);
-$opt_cfg->to->map = $tcase_mgr->get_keywords_map($args->id," ORDER BY keyword ASC ");
 
 if ($args->edit == 'testsuite')
 {
+	$opt_cfg->to->lbl = lang_get('kword_to_be_assigned_to_testcases');
+	$opt_cfg->to->map = null;
+
 	// We are going to walk all test suites contained
 	// in the selected container, and assign/remove keywords on each test case.
 	$tsuite_mgr = new testsuite($db);
 	$testsuite = $tsuite_mgr->get_by_id($args->id);
 	$keyword_assignment_subtitle = lang_get('test_suite') . TITLE_SEP . $testsuite['name'];
 	$tcs = $tsuite_mgr->get_testcases_deep($args->id,'only_id');
-	if (sizeof($tcs))
+	if( ($loop2do = sizeof($tcs)) )
 	{
 		$can_do = 1;
 		if ($args->bAssignTestSuite)
 		{
 			$result = 'ok';
-			for($i = 0;$i < sizeof($tcs);$i++)
+			for($idx = 0; $idx < $loop2do; $idx++)
 			{
-				$tcID = $tcs[$i];
-				$tcase_mgr->setKeywords($tcID,$args->keywordArray);
+				if(is_null($args->keywordArray))
+				{	
+					$tcase_mgr->deleteKeywords($tcs[$idx]);
+				}
+				else
+				{
+					$tcase_mgr->addKeywords($tcs[$idx],$args->keywordArray);
+				}	
 			}
 		}
-		$itemID = $tcs;
 	}
 }
 else if($args->edit == 'testcase')
 {
+	$doRecall = true;
 	$can_do = 1;
 	$tcData = $tcase_mgr->get_by_id($args->id);
 	if (sizeof($tcData))
 	{
-		$tcData = $tcData[0];
-   		$keyword_assignment_subtitle = lang_get('test_case') . TITLE_SEP . $tcData['name'];
+   		$keyword_assignment_subtitle = lang_get('test_case') . TITLE_SEP . $tcData[0]['name'];
 	}
 	if($args->bAssignTestCase)
 	{
 		$result = 'ok';
 		$tcase_mgr->setKeywords($args->id,$args->keywordArray);
-		$itemID = $args->id;
+		$doRecall = !is_null($args->keywordArray);	
 	}
-}
-if ($itemID)
-{
-	$opt_cfg->to->map = $tcase_mgr->get_keywords_map($itemID," ORDER BY keyword ASC ");
+	$opt_cfg->to->lbl = lang_get('assigned_kword');
+	$opt_cfg->to->map = $doRecall ? $tcase_mgr->get_keywords_map($args->id," ORDER BY keyword ASC ") : null;
 }
 
 keywords_opt_transf_cfg($opt_cfg, $args->keywordList);
@@ -115,15 +127,18 @@ function init_args(&$opt_cfg)
     
 	$args = new stdClass();
     $args->id = $pParams["id"];
-    $args->keywordArray = null;
-    $args->keywordList = $pParams[$rl_html_name];
-    if ($args->keywordList != "")
-    	$args->keywordArray = explode(",",$args->keywordList);
-    
     $args->edit = $pParams["edit"];
     $args->bAssignTestCase = ($pParams["assigntestcase"] != "") ? 1 : 0;
     $args->bAssignTestSuite = ($pParams["assigntestsuite"] != "") ? 1 : 0;
     $args->testproject_id = isset($_SESSION['testprojectID']) ? $_SESSION['testprojectID'] : 0;
+
+    $args->keywordArray = null;
+    $args->keywordList = $pParams[$rl_html_name];
+    if ($args->keywordList != "")
+    {	
+    	$args->keywordArray = explode(",",$args->keywordList);
+    }
+
         
     return $args;
 }

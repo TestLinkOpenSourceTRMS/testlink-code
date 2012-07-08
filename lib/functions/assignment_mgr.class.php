3a946d9051fa026dd530bdcded5a5d3a085fac8a
fman <francisco.mancardi@gmail.com>
2012-07-08 10:05:30 +0200

    comments clean-up

--- lib/functions/assignment_mgr.class.php
+++ lib/functions/assignment_mgr.class.php
@@ -12,19 +12,6 @@
  * @link 		http://www.teamst.org/index.php
  * 
  * @internal revisions:
- * 20110326 - franciscom - new methods 	getExecAssignmentsCountByBuild($buildID)
- *										getNotRunAssignmentsCountByBuild($buildID)
- *
- * 20110207 - asimon - BUGID 4203 - use build_id when updating or deleting assignments because
- *                                  of assignments per build
- * 20101222 - franciscom - BUGID 4121: get_not_run_tc_count_per_build() crashes
- * 20101206 - asimon - introduced new query for get_not_run_tc_count_per_build(), 
- *                     proposed by Francisco per mail
- * 20101204 - franciscom - BUGID 4070 get_not_run_tc_count_per_build()
- * 20100722 - asimon - BUGID 3406 - added copy_assignments(), delete_by_build_id(),
- *                                  get_not_run_tc_count_per_build() and
- *                                  get_count_of_assignments_per_build_id(),
- *                                  modified assign() and update() to include build_id
  */
  
 /**
@@ -87,8 +74,7 @@ class assignment_mgr extends tlObjectWithDB
 		$result = $this->db->exec_query($sql);
 	}
 	
-	// BUGID 4203 - new function to delete assignments by feature id
-	//              and build_id
+	// new function to delete assignments by feature id and build_id
 	 
 	function delete_by_feature_id_and_build_id($feature_map) 
 	{
@@ -100,7 +86,8 @@ class assignment_mgr extends tlObjectWithDB
 		// build_id is the same for all entries because of assignment form
 		// -> skip foreach after first iteration
 		$build_id = 0;
-		foreach ($feature_map as $key => $feature) {
+		foreach ($feature_map as $key => $feature) 
+    {
 			$build_id = $feature['build_id'];
 			break;
 		}
@@ -114,12 +101,11 @@ class assignment_mgr extends tlObjectWithDB
   	 * 
   	 * @param $feature_map
   	 * $feature_map['feature_id']['user_id']
-	 * $feature_map['feature_id']['type']
+	   * $feature_map['feature_id']['type']
   	 * $feature_map['feature_id']['status']
   	 * $feature_map['feature_id']['assigner_id']
   	 * 
-  	 * @internal revisions:
-  	 *   20100714 - asimon - BUGID 3406: modified to include build ID
+  	 * @internal revisions
   	 */
 	function assign($feature_map) 
 	{
@@ -138,13 +124,13 @@ class assignment_mgr extends tlObjectWithDB
 				$values .="," . $elem['deadline_ts']; 
 			}     
 			
-			// BUGID 3406
 			if (isset($elem['build_id'])) {
 				$sql .= ",build_id";
 				$values .= "," . $elem['build_id'];
 			}
 			
 			$sql .= ") " . $values . ")";
+			tLog(__METHOD__ . '::' . $sql,"DEBUG");
 			$this->db->exec_query($sql);
 		}
 	}

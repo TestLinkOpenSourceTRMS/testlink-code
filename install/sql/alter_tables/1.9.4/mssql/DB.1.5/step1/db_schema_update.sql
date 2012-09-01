672336a7489769f4e8cef16cb18b8ea962923087
fman <francisco.mancardi@gmail.com>
2012-09-01 09:34:06 +0200

    migration to 1.9.4

--- install/sql/alter_tables/1.9.4/mssql/DB.1.5/step1/db_schema_update.sql
+++ install/sql/alter_tables/1.9.4/mssql/DB.1.5/step1/db_schema_update.sql
@@ -2,7 +2,7 @@
  * TestLink Open Source Project - http://testlink.sourceforge.net/ 
  * This script is distributed under the GNU General Public License 2 or later. 
  * 
- * SQL script: Update schema Postgres database for TestLink 1.9.4 from version 1.8.1 
+ * SQL script: Update schema MSSQL database for TestLink 1.9.4 from version 1.8.1 
  * "/ *prefix* /" - placeholder for tables with defined prefix, used by sqlParser.class.php.
  *
  * @filesource	db_schema_update.sql
@@ -13,9 +13,6 @@
  * USE SEMICOLONS ONLY to signal END of SQL Statements.
  *
  * @internal revisions:
- *
- * 20110815 - franciscom - improvements on cookie_string generation (after Julian indications)
- * 20110808 - franciscom - manual migration from 1.9.1 (DB 1.4) to 1.9.4 (DB 1.5)
  */
 
 -- ==============================================================================
@@ -27,42 +24,57 @@ INSERT INTO /*prefix*/node_types ("id","description") VALUES (11,'requirement_sp
 
 -- TICKET 4661
 CREATE TABLE /*prefix*/req_specs_revisions (
-  "parent_id" BIGINT NOT NULL DEFAULT '0' REFERENCES  /*prefix*/req_specs (id),
-  "id" BIGINT NOT NULL DEFAULT '0' REFERENCES  /*prefix*/nodes_hierarchy (id),
-  "revision" INTEGER NOT NULL DEFAULT '1',
-  "doc_id" VARCHAR(64) NULL,   /* it's OK to allow a simple update query on code */
-  "name" VARCHAR(100) NULL,
-  "scope" TEXT NULL DEFAULT NULL,
-  "total_req" INTEGER NOT NULL DEFAULT '0',
-  "status" INTEGER NOT NULL DEFAULT '1',
-  "type" CHAR(1) NULL DEFAULT 'N',
-  "author_id" BIGINT NULL DEFAULT NULL,
-  "creation_ts" TIMESTAMP NOT NULL DEFAULT now(),
-  "modifier_id" BIGINT NULL DEFAULT NULL,
-  "modification_ts" TIMESTAMP NULL,
-  "log_message" TEXT NULL DEFAULT NULL,
-  PRIMARY KEY  ("id")
-);
-CREATE UNIQUE INDEX /*prefix*/req_specs_revisions_uidx1 ON /*prefix*/req_revisions ("parent_id","revision");
+  	parent_id int NOT NULL,
+	id int NOT NULL,
+  	revision INTEGER NOT NULL DEFAULT '1',
+	doc_id VARCHAR(64) NOT NULL,
+	name varchar(100) NULL,
+	scope nvarchar(max)   NULL,
+	total_req int NOT NULL CONSTRAINT /*prefix*/DF_req_specs_revisions_total_req DEFAULT ((0)),
+	type char(1)  NOT NULL CONSTRAINT /*prefix*/DF_req_specs_revisions_type DEFAULT (N'n'),
+	status int NULL DEFAULT ((1)),
+	author_id int NULL,
+	creation_ts datetime NOT NULL CONSTRAINT /*prefix*/DF_req_specs_revisions_creation_ts DEFAULT (getdate()),
+	modifier_id int NULL,
+	modification_ts datetime NULL,
+  	log_message nvarchar(max)  NULL DEFAULT NULL,
+ 	CONSTRAINT /*prefix*/PK_req_specs_revisions PRIMARY KEY CLUSTERED 
+	(
+		id ASC
+	) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY];
 
 
 CREATE TABLE /*prefix*/issuetrackers
 (
-   "id" BIGSERIAL NOT NULL ,
-  "name" VARCHAR(100) NOT NULL,
-  "type" INTEGER NOT NULL DEFAULT '0',
-  "cfg" TEXT,
-  PRIMARY KEY  ("id")
-);
-CREATE UNIQUE INDEX /*prefix*/issuetrackers_uidx1 ON /*prefix*/issuetrackers ("name");
+  id int IDENTITY(1,1) NOT NULL,
+  name VARCHAR(100) NOT NULL,
+  type int NOT NULL CONSTRAINT /*prefix*/DF_issuetrackers_type DEFAULT ((0)),
+  cfg nvarchar(max)  NULL,
+  CONSTRAINT /*prefix*/PK_issuetrackers PRIMARY KEY  CLUSTERED 
+	(
+		id
+	)  ON [PRIMARY],
+    CONSTRAINT /*prefix*/UIX_issuetrackers UNIQUE NONCLUSTERED 
+   ( 
+	name ASC
+   ) ON [PRIMARY]	
+) ON [PRIMARY];
 
 
 CREATE TABLE /*prefix*/testproject_issuetracker
 (
-  "testproject_id" BIGINT NOT NULL DEFAULT '0' REFERENCES  /*prefix*/testprojects (id),
-  "issuetracker_id" BIGINT NOT NULL DEFAULT '0' REFERENCES  /*prefix*/issuetrackers (id)
-);
-CREATE UNIQUE INDEX /*prefix*/testproject_issuetracker_uidx1 ON /*prefix*/testproject_issuetracker ("testproject_id");
+  testproject_id int NOT NULL,
+  issuetracker_id int NOT NULL,
+    CONSTRAINT /*prefix*/UIX_testproject_issuetracker UNIQUE NONCLUSTERED 
+   ( 
+	testproject_id ASC
+   ) ON [PRIMARY]	  
+)ON [PRIMARY];
+
+
+/* testprojects */
+ALTER TABLE /*prefix*/testprojects ADD issue_tracker_enabled tinyint NOT NULL CONSTRAINT /*prefix*/DF_testprojects_issue_tracker_enabled DEFAULT ((0));
 
 
 /* Create Req Spec Revision Nodes */
@@ -79,8 +91,6 @@ NHRSPREV.id,NHRSPREV.name
 FROM /*prefix*/req_specs RSP JOIN /*prefix*/nodes_hierarchy NHRSPREV
 ON NHRSPREV.parent_id = RSP.id AND NHRSPREV.node_type_id=11; 
 
-UPDATE /*prefix*/req_specs_revisions SET log_message='Requirement Specification Revision migrated from Testlink <= 1.9.3'; 
-
 /* Drop Columns from Req Specs Table */
 ALTER TABLE /*prefix*/req_specs DROP COLUMN scope;
 ALTER TABLE /*prefix*/req_specs DROP COLUMN total_req;
@@ -90,20 +100,13 @@ ALTER TABLE /*prefix*/req_specs DROP COLUMN creation_ts;
 ALTER TABLE /*prefix*/req_specs DROP COLUMN modifier_id;
 ALTER TABLE /*prefix*/req_specs DROP COLUMN modification_ts;
 
-COMMENT ON TABLE /*prefix*/req_specs IS 'Updated to TL 1.9.4 - DB 1.5';
-
-/* testprojects */
-ALTER TABLE /*prefix*/testprojects ADD COLUMN "issue_tracker_enabled" INT2 NOT NULL DEFAULT '0';
-
 /* users */
-/* 
-Issues with MD5(RANDOM()) - solved thanks to stackoverflow 
-http://stackoverflow.com/questions/3970795/how-do-you-create-a-random-string-in-postgresql
-*/
-ALTER TABLE /*prefix*/users ADD COLUMN cookie_string varchar(64) NOT NULL DEFAULT '';
-UPDATE /*prefix*/users SET cookie_string=(MD5(RANDOM()::text) || MD5(login));
-CREATE UNIQUE INDEX /*prefix*/users_cookie_string ON /*prefix*/users ("cookie_string") ;
-COMMENT ON TABLE /*prefix*/users  IS 'Updated to TL 1.9.4 - DB 1.5';
+ALTER TABLE /*prefix*/users ADD cookie_string varchar(64) NOT NULL DEFAULT '';
+UPDATE /*prefix*/users SET cookie_string=HashBytes('MD5',CAST(RAND() AS CHAR)) + HashBytes('MD5','admin');
+CREATE UNIQUE NONCLUSTERED INDEX /*prefix*/users_cookie_string ON  /*prefix*/users 
+(
+	cookie_string
+) ON [PRIMARY];
 
 /* new rights */
 INSERT INTO /*prefix*/rights  (id,description) VALUES (28,'req_tcase_link_management');

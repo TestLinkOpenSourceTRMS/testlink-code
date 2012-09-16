0935a717a208abfe2c6965d52375648703cfdab8
fman <francisco.mancardi@gmail.com>
2012-09-16 12:57:10 +0200

    TICKET 5219: TRAC Integration says "Bug ID does not exist on BTS!"

--- lib/issuetrackerintegration/tracxmlrpcInterface.class.php
+++ lib/issuetrackerintegration/tracxmlrpcInterface.class.php
@@ -12,19 +12,33 @@
  * [Trac Settings]
  * The XmlRpcPlugin plugin should be installed in your Trac.
  * 
- * In addition, you should add the permission of 'TICKET_VIEW' and 'XML_RPC'
+ * IMPORTANT NOTICE
+ *
+ * Anonymous Access
+ * You should add the permission of 'TICKET_VIEW' and 'XML_RPC'
  * to the user 'anonymous' in Trac.
+ * And use URL without login part
  *
  * Example for TurnKeyLinux trac appliance (@2012-08-26)
  * login into appliance and then:
  * #trac-admin /var/local/lib/trac/hg-helloworld
  * Trac [/var/local/lib/trac/hg-helloworld] permission add anonymous XML_RPC
  * Trac [/var/local/lib/trac/hg-helloworld] permission list anonymous
- *  
+ *
+ * Authenticated Access
+ * You should add the permission of 'TICKET_VIEW' and 'XML_RPC'
+ * to the user YOU HAVE CONFIGURED on TestLink, in Trac.
+ * And use URL WITH login part
+ *
+ * Example: username fman
+ *
+ * Example for TurnKeyLinux trac appliance (@2012-08-26)
+ * login into appliance and then:
+ * #trac-admin /var/local/lib/trac/hg-helloworld
+ * Trac [/var/local/lib/trac/hg-helloworld] permission add fman XML_RPC
+ * Trac [/var/local/lib/trac/hg-helloworld] permission list fman
  *
  * @internal revisions
- * @since 1.9.4
- * 20120825 - franciscom - TICKET 4904: integrate with ITS on test project basis 
 **/
 
 // use phpxmlrpc because support HTTPS, while incutio NO.
@@ -49,10 +63,10 @@ class tracxmlrpcInterface extends issueTrackerInterface
 		$this->interfaceViaDB = false;
 		$this->methodOpt['buildViewBugLink'] = array('addSummary' => true, 'colorByStatus' => false);
 		
-	    $this->setCfg($config);
+    $this->setCfg($config);
 		$this->completeCfg();
-	    $this->connect();
-	    $this->guiCfg = array('use_decoration' => true); // add [] on summary
+	  $this->connect();
+	  $this->guiCfg = array('use_decoration' => true); // add [] on summary
 	}
 
 
@@ -68,92 +82,80 @@ class tracxmlrpcInterface extends issueTrackerInterface
 	function completeCfg()
 	{
 		$base = trim($this->cfg->uribase,"/") . '/'; // be sure no double // at end
-	    if( !property_exists($this->cfg,'urixmlrpc') )
+		$prop2check = array('urixmlrpc' => 'xmlrpc', 'uriview' => 'ticket/', 'uricreate' => 'newticket/');
+		foreach($prop2check as $prop => $add2base)
+		{
+	    if( !property_exists($this->cfg,$prop) )
 	    {
-	    	$this->cfg->urixmlrpc = $base . 'xmlrpc';
-		}
-		
-	    if( !property_exists($this->cfg,'uriview') )
-	    {
-	    	$this->cfg->uriview = $base . 'ticket/';
-		}
-	    
-	    if( !property_exists($this->cfg,'uricreate') )
-	    {
-	    	$this->cfg->uricreate = $base . 'newticket/';
-		}	    
+	      	$this->cfg->$prop = $base . $add2base;
+		  }
+	  }
 	}
 
 	/**
-     * useful for testing 
-     *
-     *
-     **/
+    * useful for testing 
+    *
+   **/
 	function getAPIClient()
 	{
 		return $this->APIClient;
 	}
 
-    /**
-     * checks id for validity
-     *
-	 * @param string issueID
-     *
-     * @return bool returns true if the bugid has the right format, false else
-     **/
-    function checkBugIDSyntax($issueID)
-    {
-        return $this->checkBugIDSyntaxNumeric($issueID);
-    }
+  /**
+    * checks id for validity
+    *
+	  * @param string issueID
+    *
+    * @return bool returns true if the bugid has the right format, false else
+   **/
+  function checkBugIDSyntax($issueID)
+  {
+      return $this->checkBugIDSyntaxNumeric($issueID);
+  }
 
-    /**
-     * establishes connection to the bugtracking system
-     *
-     * @return bool 
-     *
-     **/
-    function connect()
-    {
-    	// echo __METHOD__ . '<br><br>';
-		try
-		{
-			// CRITIC NOTICE for developers
-			// $this->cfg is a simpleXML Object, then seems very conservative and safe
-			// to cast properties BEFORE using it.
-			$this->createAPIClient();
-	       	$this->connected = true;
-	       	
-			//var_dump($this->APIClient);
-			//echo '<br><br><b>END</b> ' . __METHOD__ . '<br><br>';
-			
-        }
-		catch(Exception $e)
-		{
-			$logDetails = '';
-			foreach(array('uribase','apikey') as $v)
-			{
-				$logDetails .= "$v={$this->cfg->$v} / "; 
-			}
-			$logDetails = trim($logDetails,'/ ');
-			$this->connected = false;
-            tLog(__METHOD__ . " [$logDetails] " . $e->getMessage(), 'ERROR');
-		}
+  /**
+   * establishes connection to the bugtracking system
+   *
+   * @return bool 
+   *
+   **/
+  function connect()
+  {
+	  try
+  	{
+	  	// CRITIC NOTICE for developers
+		  // $this->cfg is a simpleXML Object, then seems very conservative and safe
+		  // to cast properties BEFORE using it.
+		  $this->createAPIClient();
+	    $this->connected = true;
     }
+	  catch(Exception $e)
+	  {
+		  $logDetails = '';
+		  foreach(array('uribase','apikey') as $v)
+		  {
+			  $logDetails .= "$v={$this->cfg->$v} / "; 
+		  }
+		  $logDetails = trim($logDetails,'/ ');
+		  $this->connected = false;
+      tLog(__METHOD__ . " [$logDetails] " . $e->getMessage(), 'ERROR');
+	  }
+  }
 
-    /**
-     * 
-     *
-     **/
+  /**
+    * 
+    *
+   **/
 	function isConnected()
 	{
 		return $this->connected;
 	}
 
 
-    /**
-     * 
-     *
-     **/
+  /**
+    * 
+    *
+   **/
 	public function getIssue($issueID)
 	{
 		// array ticket.get(int id) Fetch a ticket. 
@@ -163,21 +165,21 @@ class tracxmlrpcInterface extends issueTrackerInterface
 		// ------------------------------------------
 		// key          | value
 		// ------------------------------------------
-		// status 		| new
+		// status 		  | new
 		// description 	| MERCURIAL FIRST TICKET
-		// reporter 	| admin
-		// cc 			| [empty string]
-		// component 	| component1
-		// summary 		| MERCURIAL FIRST TICKET
-		// priority 	| major
-		// keywords 	| [empty string]
-		// version 		| [empty string]
-		// milestone 	| [empty string]
-		// owner 		| somebody
-		// type 		| defect
+		// reporter 	  | admin
+		// cc 			    | [empty string]
+		// component 	  | component1
+		// summary 		  | MERCURIAL FIRST TICKET
+		// priority 	  | major
+		// keywords 	  | [empty string]
+		// version 		  | [empty string]
+		// milestone 	  | [empty string]
+		// owner 		    | somebody
+		// type 		    | defect
 		//	
 		
-        $resp = $this->sendCmd('ticket.get', $issueID);
+    $resp = $this->sendCmd('ticket.get', $issueID);
 		if( $resp == false )
 		{
 			$issue = null;
@@ -186,12 +188,11 @@ class tracxmlrpcInterface extends issueTrackerInterface
 		{
 			$attrib = $resp[self::TICKET_GET_ATTRIBUTES_IDX];
 			$issue = new stdClass();
-		    $issue->IDHTMLString = "<b>{$issueID} : </b>";
+		  $issue->IDHTMLString = "<b>{$issueID} : </b>";
 			$issue->statusCode = 0;
 			$issue->statusVerbose = $attrib['status'];
 			$issue->statusHTMLString = "[$issue->statusVerbose] ";
 			$issue->summary = $issue->summaryHTMLString = $attrib['summary'];
-		
 		}
 		return $issue;
 	}
@@ -220,7 +221,7 @@ class tracxmlrpcInterface extends issueTrackerInterface
 	 **/
 	function getIssueStatusVerbose($issueID)
 	{
-        return $this->getIssueStatusCode($issueID);
+    return $this->getIssueStatusCode($issueID);
 	}
 
 	/**
@@ -232,64 +233,61 @@ class tracxmlrpcInterface extends issueTrackerInterface
 	 **/
 	function getIssueSummaryHTMLString($issueID)
 	{
-        $issue = $this->getIssue($issueID);
-        $str = $issue->summaryHTMLString;
+    $issue = $this->getIssue($issueID);
+    $str = $issue->summaryHTMLString;
 		if($this->guiCfg['use_decoration'])
 		{
 			$str = "[" . $str . "] ";	
 		}
-        return $str;
+    return $str;
 	}
 
-    /**
+  /**
 	 * @param string issueID
-     *
-     * @return bool true if issue exists on BTS
-     **/
-    function checkBugIDExistence($issueID)
+   *
+   * @return bool true if issue exists on BTS
+   **/
+  function checkBugIDExistence($issueID)
+  {
+    $dBugLabel = array('label' => __METHOD__);
+    if(($status_ok = $this->checkBugIDSyntax($issueID)))
     {
-    	$dBugLabel = array('label' => __METHOD__);
-        if(($status_ok = $this->checkBugIDSyntax($issueID)))
-        {
-            $issue = $this->getIssue($issueID);
-            
-            // new dBug($issue,$dBugLabel);
-            $status_ok = is_object($issue) && !is_null($issue);
-        }
-        return $status_ok;
+      $issue = $this->getIssue($issueID);
+      $status_ok = is_object($issue) && !is_null($issue);
     }
+    return $status_ok;
+  }
 
 
-    /**
-     * 
-     *
-     **/
+  /**
+   * 
+   *
+   **/
 	function createAPIClient()
 	{
 		try
 		{
-            // Create a new connection with the TRAC-server.
-            $this->APIClient = new xmlrpc_client($this->cfg->urixmlrpc);
+      // Create a new connection with the TRAC-server.
+      $this->APIClient = new xmlrpc_client($this->cfg->urixmlrpc);
             
-            // Set the credentials to use to log in.
-			$this->APIClient->setCredentials($this->cfg->user, $this->cfg->password);
+      // Set the credentials to use to log in.
+			$this->APIClient->setCredentials($this->cfg->username, $this->cfg->password);
 
 			// Disable certificate checking. Don't need to check it. 
 			$this->APIClient->verifyhost = false;
 			$this->APIClient->verifypeer = false;
-			
 		}
 		catch(Exception $e)
 		{
 			$this->connected = false;
-            tLog(__METHOD__ .  $e->getMessage(), 'ERROR');
+      tLog(__METHOD__ .  $e->getMessage(), 'ERROR');
 		}
 	}	
 
 
 
-    function sendCmd($cmd, $id)
-    {
+  function sendCmd($cmd, $id)
+  {
     	$param = new xmlrpcval(intval($id));
     	$msg = new xmlrpcmsg($cmd);
     	$msg->addParam($param);
@@ -299,34 +297,42 @@ class tracxmlrpcInterface extends issueTrackerInterface
     	if (!$response->errno) 
     	{
     		$response = php_xmlrpc_decode($response->val);
-			//new dBug($response);
     	} 
     	else 
     	{
-    		tLog();
     		$response = false;
+    		tLog(__METHOD__ .  'Error Number:' . $response->errno, 'ERROR');
     	}
-    	   		
     	return $response;
-    }
+  }
 
 
 
 
-    /**
-     *
-     * @author francisco.mancardi@gmail.com>
-     **/
+  /**
+   *
+   * @author francisco.mancardi@gmail.com>
+   **/
 	public static function getCfgTemplate()
-  	{
+  {
 		$template = "<!-- Template " . __CLASS__ . " -->\n" .
-					"<issuetracker>\n" .
-					"<username>USERNAME</username>\n" .
-					"<password>PASSWORD</password>\n" .
-					"<uribase>'http://<YourTracServer>/<YourTracProjectName</uribase>\n" .
-					"</issuetracker>\n";					
-					
+					      "<issuetracker>\n" .
+					      "<username>USERNAME</username>\n" .
+					      "<password>PASSWORD</password>\n" .
+					      "<!-- READ CAREFULLY --->\n" .
+                "<!-- The XmlRpcPlugin plugin should be installed in your Trac. \n -->" .
+                "<!-- Anonymous Access: \n". 
+                "     You should add the permission of 'TICKET_VIEW' and 'XML_RPC' \n" .
+                "     to the user 'anonymous' in Trac. \n" .
+                "     Then you HAVE TO USE THIS URI. --> \n" .
+					      "<uribase>'http://<YourTracServer>/<YourTracProjectName</uribase>\n" .
+                "<!-- Authenticated Access: \n". 
+                "     You should add the permission of 'TICKET_VIEW' and 'XML_RPC' \n" .
+                "     to the user you HAVE DECLARED HERE,  in Trac. \n" .
+                "     Then you HAVE TO USE THIS URI. --> \n" .
+					      "<uribase>'http://<YourTracServer>/<YourTracProjectName/login</uribase>\n" .
+					      "</issuetracker>\n";					
 		return $template;
-  	}
+  }
 }
 ?>

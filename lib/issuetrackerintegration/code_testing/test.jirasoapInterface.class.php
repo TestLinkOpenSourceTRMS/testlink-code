b3dc89c61d4920033089a4c937d05475a5efea28
fman <francisco.mancardi@gmail.com>
2012-10-20 10:18:09 +0200

    changed user to do test (worked ok @20121020)

--- lib/issuetrackerintegration/code_testing/test.jirasoapInterface.class.php
+++ lib/issuetrackerintegration/code_testing/test.jirasoapInterface.class.php
@@ -14,9 +14,19 @@ require_once('common.php');
 $it_mgr = new tlIssueTracker($db);
 $itt = $it_mgr->getTypes();
 
+/*
 $cfg =  "<issuetracker>\n" .
 		"<username>testlink.helpme</username>\n" .
 		"<password>jira</password>\n" .
+		"<uribase>http://testlink.atlassian.net/</uribase>\n" .
+		"<uriwsdl>http://testlink.atlassian.net/rpc/soap/jirasoapservice-v2?wsdl</uriwsdl>\n" .
+		"<uriview>http://testlink.atlassian.net/browse/</uriview>\n" .
+		"<uricreate>http://testlink.atlassian.net/secure/CreateIssue!default.jspa</uricreate>\n" .
+		"</issuetracker>\n";
+*/
+$cfg =  "<issuetracker>\n" .
+		"<username>testlink.forum</username>\n" .
+		"<password>forum</password>\n" .
 		"<uribase>http://testlink.atlassian.net/</uribase>\n" .
 		"<uriwsdl>http://testlink.atlassian.net/rpc/soap/jirasoapservice-v2?wsdl</uriwsdl>\n" .
 		"<uriview>http://testlink.atlassian.net/browse/</uriview>\n" .
@@ -33,20 +43,27 @@ echo '<hr><br><br>';
 
 // $safe_cfg = str_replace("\n",'',$cfg);
 // echo $safe_cfg;
+echo 'Creating INTERFACE<br>';
 $its = new jirasoapInterface($itt['JIRASOAP'],$cfg);
-// var_dump($its);
+
+echo 'Connection OK?<br>';
+var_dump($its->isConnected());
 
 if( $its->isConnected() )
 {
 	echo '<b>Connected !</br></b>';
+	echo '<pre>';
 	var_dump($its->getStatusDomain());
+	echo '</pre>';
+  echo 'Get Issue Summary<br>';
+	echo($its->getIssueSummary('ZOFF-8'));
+  echo '<br>';
+  
 
 	/*
 	$issue2check = array( array('issue' => 'TLJIRASOAPINTEGRATION-1', 'exists' => true),
 	  					  array('issue' => 'TLJIRASOAPINTEGRATION-199999', 'exists' => false));
 
-	$methods = array('getBugSummaryString','getBugStatus','getBugStatusString',
-					 'checkBugID_existence','buildViewBugLink');
 	*/
 
 

b74320c429b380618e2f9fe717d03c2afb9ab761
fman <francisco.mancardi@gmail.com>
2012-12-05 23:12:13 +0100

    change on filename generation to avoid undefined variable when using direct link

--- lib/results/displayMgr.php
+++ lib/results/displayMgr.php
@@ -3,14 +3,10 @@
  * TestLink Open Source Project - http://testlink.sourceforge.net/ 
  * This script is distributed under the GNU General Public License 2 or later.
  *  
- * @filesource $RCSfile: displayMgr.php,v $
- * @version $Revision: 1.26 $
- * @modified $Date: 2010/11/01 17:15:37 $ by $Author: franciscom $
- * @author	Kevin Levy
+ * @filesource  displayMgr.php
+ * @author	    Kevin Levy
  * 
- * Revision:
- * 	20090213 - havlatm - added flushHttpHeader function instead of particular headers
- * 						support for OpenOffice
+ * @internal revisions
  */
 require_once('email_api.php');
 require_once('../../cfg/reports.cfg.php');
@@ -70,17 +66,17 @@ function displayReport($template_file, &$smarty, $doc_format, $mailCfg = null)
 		case FORMAT_XLS:
 		case FORMAT_MSWORD:
 		case FORMAT_PDF:
-	  		flushHttpHeader($doc_format, $doc_kind = 0);
-    		break;  
+	  	flushHttpHeader($doc_format, $doc_kind = 0);
+    break;  
 
-	    case FORMAT_MAIL_HTML:
-		  	$op = generateHtmlEmail($smarty, $template_file,  $mailCfg);
-		  	$message = $op->status_ok ? '' : lang_get('send_mail_ko');	
+	  case FORMAT_MAIL_HTML:
+		  $op = generateHtmlEmail($smarty, $template_file,  $mailCfg);
+		  $message = $op->status_ok ? '' : lang_get('send_mail_ko');	
 			$smarty = new TLSmarty();
 			$smarty->assign('message', $message . ' ' . $op->msg);
 			$smarty->assign('title', $mailCfg->subject);
-		  	$template_file = "emailSent.tpl";
-      		break;
+		  $template_file = "emailSent.tpl";
+    break;
 	} 
 
 	$smarty->display($template_file);
@@ -109,16 +105,19 @@ function flushHttpHeader($format, $doc_kind = 0)
 	}
 	
 	if ($format == FORMAT_MAIL_HTML)
+	{
 		tLog('flushHttpHeader> Invalid format: '.$format, 'ERROR');
-
-	$filename = $_SESSION['testprojectPrefix'] . $kind_acronym . '-' . date('Y-m-d') . '.' . $file_extensions[$format];
+  }
+  
+	$filename = isset($_SESSION['testprojectPrefix']) ? $_SESSION['testprojectPrefix'] : '';
+	$filename .= $kind_acronym . '-' . date('Y-m-d') . '.' . $file_extensions[$format];
 	tLog('Flush HTTP header for '.$format); 
 
 
-    header("Content-Description: TestLink - Generated Document");
-    if ($format != FORMAT_HTML)
-		header("Content-Disposition: attachment; filename=$filename");
-   	header("Content-type: {$reports_applications[$format]}; name='Testlink_$format'");
+  header("Content-Description: TestLink - Generated Document");
+  if ($format != FORMAT_HTML)
+	header("Content-Disposition: attachment; filename=$filename");
+  header("Content-type: {$reports_applications[$format]}; name='Testlink_$format'");
 	flush();
 }
 

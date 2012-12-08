a37f83d7e2c952aa121d4e3dfb89419f2435f452
fman <francisco.mancardi@gmail.com>
2012-12-08 16:01:35 +0100

    version updated

--- cfg/const.inc.php
+++ cfg/const.inc.php
@@ -22,7 +22,7 @@
 
 /** TestLink Release version (MUST BE changed before the release day) */
 define('TL_FACE_DIR', 'prague'); 
-define('TL_VERSION', 'Prague 1.9.5'); 
+define('TL_VERSION', 'Development Prague 1.9.6 (Q1 2013)'); 
 
 /** Last Database version that is used to give users feedback about necesssary upgrades
  * if you set this parameter also upgrade lib/functions/configCheck.php - checkSchemaVersion() */

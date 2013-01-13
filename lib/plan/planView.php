6c1a64e01dad554e4b9454d54e486ca6f87a1e8e
fman <francisco.mancardi@gmail.com>
2013-01-13 10:28:32 +0100

    TICKET 5465: Inactive test plan are not displayed on Test management

--- lib/plan/planView.php
+++ lib/plan/planView.php
@@ -5,9 +5,9 @@
  *
  * @filesource	planView.php
  * @internal revisions
- * @since 1.9.5
- * 20121202 - franciscom - TICKET 5386: Test Plan management - user can manage Private Test plan 
- *                                      where she/he has no role assigned
+ * @since 1.9.6
+ * 20130113 - franciscom - TICKET 5465: Inactive test plan are not displayed on Test management
+ *
  */
 require_once('../../config.inc.php');
 require_once("common.php");
@@ -19,32 +19,35 @@ $args=init_args();
 $gui = initializeGui($db,$args);
 if($args->tproject_id)
 {
-	$tproject_mgr = new testproject($db);
-  $gui->tplans = $args->user->getAccessibleTestPlans($db,$args->tproject_id,null,array('output' =>'mapfull'));
-	$gui->drawPlatformQtyColumn = false;
-	
-	if( !is_null($gui->tplans) && count($gui->tplans) > 0 )
-	{
-		// do this test project has platform definitions ?
-		$tplan_mgr = new testplan($db);
-		$tplan_mgr->platform_mgr->setTestProjectID($args->tproject_id);
-		$dummy = $tplan_mgr->platform_mgr->testProjectCount();
-		$gui->drawPlatformQtyColumn = $dummy[$args->tproject_id]['platform_qty'] > 0;
+  $tproject_mgr = new testproject($db);
 
-		$tplanSet = array_keys($gui->tplans);
-		$dummy = $tplan_mgr->count_testcases($tplanSet,null,array('output' => 'groupByTestPlan'));
-		foreach($tplanSet as $idk)
-		{
-			$gui->tplans[$idk]['tcase_qty'] = isset($dummy[$idk]['qty']) ? $dummy[$idk]['qty'] : 0;
-			if( $gui->drawPlatformQtyColumn )
-			{
-				$plat = $tplan_mgr->getPlatforms($idk);
-				$gui->tplans[$idk]['platform_qty'] = is_null($plat) ? 0 : count($plat);
-			}
-		}		
-		unset($tplan_mgr);	
-	}
-	unset($tproject_mgr);	
+  // TICKET 5465: Inactive test plan are not displayed on Test management
+  $gui->tplans = $args->user->getAccessibleTestPlans($db,$args->tproject_id,null,
+                                                     array('output' =>'mapfull', 'active' => null));
+  $gui->drawPlatformQtyColumn = false;
+  
+  if( !is_null($gui->tplans) && count($gui->tplans) > 0 )
+  {
+    // do this test project has platform definitions ?
+    $tplan_mgr = new testplan($db);
+    $tplan_mgr->platform_mgr->setTestProjectID($args->tproject_id);
+    $dummy = $tplan_mgr->platform_mgr->testProjectCount();
+    $gui->drawPlatformQtyColumn = $dummy[$args->tproject_id]['platform_qty'] > 0;
+
+    $tplanSet = array_keys($gui->tplans);
+    $dummy = $tplan_mgr->count_testcases($tplanSet,null,array('output' => 'groupByTestPlan'));
+    foreach($tplanSet as $idk)
+    {
+      $gui->tplans[$idk]['tcase_qty'] = isset($dummy[$idk]['qty']) ? $dummy[$idk]['qty'] : 0;
+      if( $gui->drawPlatformQtyColumn )
+      {
+        $plat = $tplan_mgr->getPlatforms($idk);
+        $gui->tplans[$idk]['platform_qty'] = is_null($plat) ? 0 : count($plat);
+      }
+    }    
+    unset($tplan_mgr);  
+  }
+  unset($tproject_mgr);  
 }
 
 $smarty = new TLSmarty();
@@ -87,6 +90,6 @@ function initializeGui(&$dbHandler,$argsObj)
  */
 function checkRights(&$db,&$user)
 {
-	return $user->hasRight($db,'mgt_testplan_create');
+  return $user->hasRight($db,'mgt_testplan_create');
 }
 ?>

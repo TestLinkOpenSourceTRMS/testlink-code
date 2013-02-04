b431cf43cd767a69edd854993e09d4be819e08bf
fman <francisco.mancardi@gmail.com>
2013-02-04 21:03:30 +0100

    TICKET 5517: Create Issue (SOAP) fails due to required attributes (not managed yet on TestLink)

--- lib/issuetrackerintegration/code_testing/test.jiraOnDemand.jirasoapInterface.class.php
+++ lib/issuetrackerintegration/code_testing/test.jiraOnDemand.jirasoapInterface.class.php
@@ -22,6 +22,10 @@ $cfg =  "<issuetracker>\n" .
 		"<uriwsdl>http://testlink.atlassian.net/rpc/soap/jirasoapservice-v2?wsdl</uriwsdl>\n" .
 		"<uriview>http://testlink.atlassian.net/browse/</uriview>\n" .
 		"<uricreate>http://testlink.atlassian.net/secure/CreateIssue!default.jspa</uricreate>\n" .
+        "<projectkey>ZOFF</projectkey>\n" .
+        "<issuetype>1</issuetype>\n" .
+		"<attributes><components><id>10100</id><id>10101</id></components>\n" .
+		"</attributes>\n" .
 		"</issuetracker>\n";
 
 echo '<hr><br>';
@@ -62,11 +66,16 @@ if( $its->isConnected() )
 // I watched C-beams glitter in the dark near the Tannhauser gate. 
 // All those moments will be lost in time... like tears in rain... Time to die. 	
 //
-  $issue = array('project' => 'ZOFF','summary' => 'My Firts ISSUE VIA API',
-                 'description' => 'Do Androids Dream of Electric Sheep?',
-                 'type' => 1, 'components' => array( array('id' => '10100'), array('id' => '10101')));
- $zorro = $its->addIssueFromArray($issue);
- var_dump($zorro);
+  //$issue = array('project' => 'ZOFF','summary' => 'My Firts ISSUE VIA API',
+  //               'description' => 'Do Androids Dream of Electric Sheep?',
+  //               'type' => 1, 'components' => array( array('id' => '10100'), array('id' => '10101')));
+  // $zorro = $its->addIssueFromArray($issue);
+  // var_dump($zorro);
+  $issue = array('summary' => 'Issue Via API 2013-02-04','description' => 'Do Androids Dream of Electric Sheep?');
+  //               'type' => 1, 'components' => array( array('id' => '10100'), array('id' => '10101')));
+  
+  $zorro = $its->addIssue($issue['summary'],$issue['description']);
+  var_dump($zorro);
 
 
 	/*

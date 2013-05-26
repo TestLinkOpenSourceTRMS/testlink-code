b0f0ec0ec79e18febcfe56e28d37ae27ab9bdd83
fman <francisco.mancardi@gmail.com>
2013-05-26 20:29:03 +0200

    TICKET 5640: Popup for not saved changes is not shown if using CKEDITOR

--- gui/javascript/checkmodified.js
+++ gui/javascript/checkmodified.js
@@ -2,18 +2,21 @@
  * TestLink Open Source Project - http://testlink.sourceforge.net/
  * This script is distributed under the GNU General Public License 2 or later.
  *
- * @package TestLink
- * @author Erik Eloff
- * @copyright 2010, TestLink community
- * @version CVS: $Id: checkmodified.js,v 1.3 2010/02/21 15:24:04 franciscom Exp $
- * @filesource http://testlink.cvs.sourceforge.net/viewvc/testlink/testlink/gui/javascript/checkmodified.js
+ * @package     TestLink
+ * @author      Erik Eloff
+ * @copyright   2010,2013 TestLink community
+ * @filesource  checkmodified.js
  * @link http://www.teamst.org
  * @since 1.9
  *
- * Functions for warning the user of unsaved content in an FCKeditor.
+ * Functions for warning the user of unsaved content in an FCKeditor or CKeditor.
  *
- * Usage: Import this javascript and make sure the variables unload_msg and
- *        tc_editor are defined.
+ * Usage: 
+ * Import this javascript and make sure the variables unload_msg and tc_editor are defined.
+ *
+ * @internal revisions
+ * @since 1.9.8
+ * user contribution to make this work with CKeditor and also with Chrome
  */
 
 /** Any input can change content_modified to true if it is modified */
@@ -25,39 +28,58 @@ var show_modified_warning = true;
 // Notify on exit with unsaved data
 function doBeforeUnload()
 {
-	if (!show_modified_warning) {
-		return;
-	}
+  if (!show_modified_warning) 
+  {
+    return;
+  }
 
-	if (FCKEditorChanged()) {
-		content_modified = true;
-	}
+  if (editorChanged()) 
+  {
+    content_modified = true;
+  }
 
-	if (!content_modified) {
-		return; // Let the page unload
-	}
+  if (!content_modified) 
+  {
+    return; // Let the page unload
+  }
 
-	if(window.event) 
-	{
-		window.event.returnValue = unload_msg; // IE
-	} 
-	else 
-	{
-		return unload_msg; // Firefox
-	}
+  if(window.event) 
+  {
+    window.event.returnValue = unload_msg; // IE
+    return unload_msg; // Chrome
+  } 
+  else 
+  {
+    return unload_msg; // Firefox
+  }
 }
 
 // verify if content of any editor changed
-function FCKEditorChanged()
+function editorChanged()
 {
-	if (tc_editor == "fckeditor") {
-		for (var editorname in FCKeditorAPI.Instances) {
-			if (FCKeditorAPI.GetInstance(editorname).IsDirty()) {
-				return true;
-			}
-		}
-	}
-	return false;
+  if (tc_editor == "fckeditor") 
+  {
+    for (var id in FCKeditorAPI.Instances) 
+    {
+      if (FCKeditorAPI.GetInstance(id).IsDirty()) 
+      {
+        return true;
+      }
+    }
+  }
+
+  if (tc_editor == "ckeditor") 
+  {
+    for (var id in CKEDITOR.instances) 
+    {
+      if (CKEDITOR.instances[id].checkDirty()) 
+      {
+        return true;
+      }
+    }
+  }
+
+  return false;
 }
 
 // Tell FCKeditor that it is clean from start
@@ -69,9 +91,9 @@ function FCKeditor_OnComplete(editorInstance)
 // set unload checking if configured to use
 if(window.body) 
 {
-	window.body.onbeforeunload = doBeforeUnload; // IE
+  window.body.onbeforeunload = doBeforeUnload; // IE
 } 
 else 
 {
-	window.onbeforeunload = doBeforeUnload; // Firefox
+  window.onbeforeunload = doBeforeUnload; // Firefox
 }

0bf20ef416dab37a4a272233216003b672072fcb
fman <francisco.mancardi@gmail.com>
2013-08-16 17:02:08 +0200

    missing argument on call to reqSpecCommand() constructor

--- lib/requirements/reqSpecViewRevision.php
+++ lib/requirements/reqSpecViewRevision.php
@@ -3,11 +3,12 @@
  * TestLink Open Source Project - http://testlink.sourceforge.net/
  * This script is distributed under the GNU General Public License 2 or later. 
  *  
- * @filesource	reqSpecViewRevision.php
+ * @filesource  reqSpecViewRevision.php
  * @author francisco.mancardi@gmail.com
  * 
  *
- *	@internal revision
+ * @internal revisions
+ * 
  */
 require_once('../../config.inc.php');
 require_once('common.php');
@@ -30,17 +31,17 @@ $smarty->display($templateCfg->template_dir . 'reqSpecViewRevision.tpl');
  */
 function init_args()
 {
-	$iParams = array("item_id" => array(tlInputParameter::INT_N),
-			         "showContextInfo" => array(tlInputParameter::INT_N));	
-		
-	$args = new stdClass();
-	R_PARAMS($iParams,$args);
-	
+  $iParams = array("item_id" => array(tlInputParameter::INT_N),
+               "showContextInfo" => array(tlInputParameter::INT_N));  
+    
+  $args = new stdClass();
+  R_PARAMS($iParams,$args);
+  
     $args->tproject_id = isset($_SESSION['testprojectID']) ? $_SESSION['testprojectID'] : 0;
     $args->tproject_name = isset($_SESSION['testprojectName']) ? $_SESSION['testprojectName'] : null;
     $user = $_SESSION['currentUser'];
-	$args->userID = $user->dbID;
-	
+  $args->userID = $user->dbID;
+  
     return $args;
 }
 
@@ -52,7 +53,7 @@ function initialize_gui(&$dbHandler,$argsObj)
 {
     $tproject_mgr = new testproject($dbHandler);
     $itemMgr = new requirement_spec_mgr($dbHandler);
-    $commandMgr = new reqSpecCommands($dbHandler);
+    $commandMgr = new reqSpecCommands($dbHandler,$argsObj->tproject_id);
 
     $gui = $commandMgr->initGuiBean();
     $gui->itemCfg = config_get('req_spec_cfg');
@@ -66,10 +67,10 @@ function initialize_gui(&$dbHandler,$argsObj)
     $gui->pieceSep = config_get('gui_title_separator_1');
     
     $gui->item_id = $argsObj->item_id;
-	$info = $itemMgr->getRevisionByID($gui->item_id,array('decode_user' => true));
+  $info = $itemMgr->getRevisionByID($gui->item_id,array('decode_user' => true));
     $gui->item = $info;
-	
-	$gui->cfields = $itemMgr->html_table_of_custom_field_values(null,$gui->item_id,$argsObj->tproject_id);
+  
+  $gui->cfields = $itemMgr->html_table_of_custom_field_values(null,$gui->item_id,$argsObj->tproject_id);
     $gui->show_title = false;
     $gui->main_descr = lang_get('req_spec') . $gui->pieceSep .  $gui->item['name'];
     
@@ -92,6 +93,6 @@ function initialize_gui(&$dbHandler,$argsObj)
  */
 function checkRights(&$dbHandler,&$user)
 {
-	return $user->hasRight($dbHandler,'mgt_view_req');
+  return $user->hasRight($dbHandler,'mgt_view_req');
 }
 ?>

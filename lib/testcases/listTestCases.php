9bff55fe0e79163b33bea7e848aefd9ecc144e90
fman <francisco.mancardi@gmail.com>
2013-11-08 22:46:49 +0100

    old comments removed

--- lib/testcases/listTestCases.php
+++ lib/testcases/listTestCases.php
@@ -2,26 +2,13 @@
 /**
 * 	TestLink Open Source Project - http://testlink.sourceforge.net/
 *
-* 	@version 	$Id: listTestCases.php,v 1.60 2010/09/15 20:55:12 franciscom Exp $
+* 	@filesource listTestCases.php
 * 	@author 	Martin Havlat
 *
 * 	Generates tree menu with test specification.
 *   It builds the javascript tree that allows the user to choose testsuite or testcase.
 *
 *	@internal revision
-*
-*   20100628 - asimon - removal of constants from filter control class
-*   20100624 - asimon - CVS merge (experimental branch to HEAD)
-*   20100622 - asimon - huge refactorization for new tlTestCaseFilterControl class
-*   20100517 - asimon - BUGID 3301 and related - huge refactoring for first implementation
-*                       of filter panel class hierarchy to simplify/standardize
-*                       filter panel handling for test cases and requirements
-*   20100428 - asimon - BUGID 3301 and related issues - changed name or case
-*                       of some variables used in new common template,
-*                       added custom field filtering logic
-*	20091210 - franciscom - test case execution type filter
-*   20090308 - franciscom - added option Any in keywords filter
-*   20090210 - BUGID 2062 - franciscom -
 */
 require_once('../../config.inc.php');
 require_once("common.php");

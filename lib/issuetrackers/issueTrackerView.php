5692cff9a0974dbbe3d2ac535122ec98e9692167
Francisco Mancardi <francisco.mancardi@gmail.com>
2013-12-01 19:08:49 +0100

    code layout

--- lib/issuetrackers/issueTrackerView.php
+++ lib/issuetrackers/issueTrackerView.php
@@ -3,13 +3,11 @@
  * TestLink Open Source Project - http://testlink.sourceforge.net/
  * This script is distributed under the GNU General Public License 2 or later.
  *
- * @filesource	issueTrackerView.php
+ * @filesource  issueTrackerView.php
  *
- * @author	francisco.mancardi@gmail.com
+ * @author   francisco.mancardi@gmail.com
  * @internal revisions
  * 
- * @since 1.9.5
- * 20121012 - franciscom - TICKET 5281: On list view add check to environment (example SOAP ext is enabled?) 
  *
 **/
 require_once(dirname(__FILE__) . "/../../config.inc.php");
@@ -42,33 +40,32 @@ $smarty->display($templateCfg->template_dir . $templateCfg->default_template);
  */
 function init_args()
 {
-	$args = new stdClass();
-	$args->tproject_id = isset($_SESSION['testprojectID']) ? intval($_SESSION['testprojectID']) : 0;
+  $args = new stdClass();
+  $args->tproject_id = isset($_SESSION['testprojectID']) ? intval($_SESSION['testprojectID']) : 0;
 
-	if( $args->tproject_id == 0 )
-	{
-		$args->tproject_id = isset($_REQUEST['tproject_id']) ? intval($_SESSION['tproject_id']) : 0;
-	}
-	$args->currentUser = $_SESSION['currentUser']; 
-	
-	$args->user_feedback = array('type' => '', 'message' => '');
-	
-	// only way I've found in order to give feedback for delete
-	// need to undertand if we really need/want to do all this mess
-	// $args->user_feedback = array('type' => '', 'message' => '');
-	// if( isset($_SESSION['issueTrackerView.user_feedback']) )
-	// {
-	// 	$args->user_feedback = array('type' => '', 'message' => $_SESSION['issueTrackerView.user_feedback']);
-	// 	unset($_SESSION['issueTrackerView.user_feedback']);
-	// }
+  if( $args->tproject_id == 0 )
+  {
+    $args->tproject_id = isset($_REQUEST['tproject_id']) ? intval($_SESSION['tproject_id']) : 0;
+  }
+  $args->currentUser = $_SESSION['currentUser']; 
+  
+  $args->user_feedback = array('type' => '', 'message' => '');
+  
+  // only way I've found in order to give feedback for delete
+  // need to undertand if we really need/want to do all this mess
+  // $args->user_feedback = array('type' => '', 'message' => '');
+  // if( isset($_SESSION['issueTrackerView.user_feedback']) )
+  // {
+  //  $args->user_feedback = array('type' => '', 'message' => $_SESSION['issueTrackerView.user_feedback']);
+  //  unset($_SESSION['issueTrackerView.user_feedback']);
+  // }
 
   $args->id = isset($_REQUEST['id']) ? intval($_REQUEST['id']) : 0;
-	return $args;
+  return $args;
 }
 
 
 function checkRights(&$db,&$user)
 {
-	return $user->hasRight($db,"issuetracker_view") || $user->hasRight($db,"issuetracker_management");
+  return $user->hasRight($db,"issuetracker_view") || $user->hasRight($db,"issuetracker_management");
 }
-?>

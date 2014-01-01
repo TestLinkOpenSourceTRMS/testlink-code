ab8ac1095b5b5ad0d12a893492764d4f62701a67
fman <francisco.mancardi@gmail.com>
2014-01-01 18:49:49 +0100

    added control->formAction, to remove warning on TestLink Eventi viewer

--- lib/testcases/listTestCases.php
+++ lib/testcases/listTestCases.php
@@ -1,15 +1,15 @@
 <?php
 /**
-* 	TestLink Open Source Project - http://testlink.sourceforge.net/
-*
-* 	@filesource listTestCases.php
-* 	@author 	Martin Havlat
-*
-* 	Generates tree menu with test specification.
-*   It builds the javascript tree that allows the user to choose testsuite or testcase.
-*
-*	@internal revision
-*/
+ * TestLink Open Source Project - http://testlink.sourceforge.net/
+ *
+ * @filesource listTestCases.php
+ * @author     Martin Havlat
+ *
+ * Generates tree menu with test specification.
+ *
+ * @internal revisions
+ * @since 1.9.10
+ */
 require_once('../../config.inc.php');
 require_once("common.php");
 require_once("treeMenu.inc.php");
@@ -20,6 +20,7 @@ $templateCfg = templateConfiguration();
 $control = new tlTestCaseFilterControl($db, 'edit_mode');
 $gui = initializeGui($db, $control);
 $control->build_tree_menu($gui);
+$control->formAction = '';
 
 $smarty = new TLSmarty();
 $smarty->assign('gui', $gui);
@@ -38,21 +39,21 @@ $smarty->display($templateCfg->template_dir . 'tcTree.tpl');
  */
 function initializeGui(&$dbHandler, &$control) 
 {
-	$gui = new stdClass();
-	$gui->feature = $control->args->feature;
-	$gui->treeHeader = lang_get('title_navigator'). ' - ' . lang_get('title_test_spec');
+  $gui = new stdClass();
+  $gui->feature = $control->args->feature;
+  $gui->treeHeader = lang_get('title_navigator'). ' - ' . lang_get('title_test_spec');
 
-	$lblkey = (config_get('testcase_reorder_by') == 'NAME') ? '_alpha' : '_externalid';
-	$gui->btn_reorder_testcases = lang_get('btn_reorder_testcases' . $lblkey);
+  $lblkey = (config_get('testcase_reorder_by') == 'NAME') ? '_alpha' : '_externalid';
+  $gui->btn_reorder_testcases = lang_get('btn_reorder_testcases' . $lblkey);
 
-	$feature_path = array('edit_tc' => "lib/testcases/archiveData.php",
-	                      'keywordsAssign' => "lib/keywords/keywordsAssign.php",
-	                      'assignReqs' => "lib/requirements/reqTcAssign.php");
+  $feature_path = array('edit_tc' => "lib/testcases/archiveData.php",
+                        'keywordsAssign' => "lib/keywords/keywordsAssign.php",
+                        'assignReqs' => "lib/requirements/reqTcAssign.php");
 
-	$gui->tree_drag_and_drop_enabled = array('edit_tc' => (has_rights($dbHandler, "mgt_modify_tc") == 'yes'),
-	                                         'keywordsAssign' => false,
-	                                         'assignReqs' => false);
+  $gui->tree_drag_and_drop_enabled = array('edit_tc' => (has_rights($dbHandler, "mgt_modify_tc") == 'yes'),
+                                           'keywordsAssign' => false,
+                                           'assignReqs' => false);
 
-	$gui->menuUrl = $feature_path[$gui->feature];
-	return $gui;
+  $gui->menuUrl = $feature_path[$gui->feature];
+  return $gui;
 }

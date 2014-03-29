618dd608ff79c0c481a12cebc1d2efdfe74b6513
fman <francisco.mancardi@gmail.com>
2014-03-29 16:39:43 +0100

    information regarding upgrade, updated to include version 1.9.9

--- install/index.php
+++ install/index.php
@@ -6,7 +6,7 @@
  * Navigation for installation scripts
  *
  * @package     TestLink
- * @copyright   2007,2013 TestLink community
+ * @copyright   2007,2014 TestLink community
  * @filesource  index.php
  *
  * @internal revisions
@@ -22,7 +22,7 @@ session_start();
 $_SESSION['session_test'] = 1;
 $_SESSION['testlink_version'] = TL_VERSION;
 
-$prev_ver = '1.9.3/4/5/6/7/8';
+$prev_ver = '1.9.3/4/5/6/7/8/9';
 $forum_url = 'forum.testlink.org';
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">

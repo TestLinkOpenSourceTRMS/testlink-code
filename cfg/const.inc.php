747e9622b23b819657c8403ef7df5917833c88e9
fman <francisco.mancardi@gmail.com>
2014-04-25 19:01:28 +0200

    1.9.11 Journey started

--- cfg/const.inc.php
+++ cfg/const.inc.php
@@ -23,13 +23,13 @@
 define('TL_SMARTY_VERSION',3);  // @since 1.9.8
 
 /** TestLink Release version (MUST BE changed before the release day) */
-define('TL_VERSION_NUMBER', '1.9.10'); 
-define('TL_VERSION', TL_VERSION_NUMBER . '(El D1eG0)'); 
+define('TL_VERSION_NUMBER', '1.9.11'); 
+define('TL_VERSION', 'DEV' . TL_VERSION_NUMBER . '(The Robots of Dawn)'); 
 define('TL_FACE_DIR', 'prague'); 
 
 /** Last Database version that is used to give users feedback about necesssary upgrades
  * if you set this parameter also upgrade lib/functions/configCheck.php - checkSchemaVersion() */
-define('TL_LAST_DB_VERSION', 'DB 1.9.9');
+define('TL_LAST_DB_VERSION', 'DB 1.9.10');
 
 // needed to avoid problems in install scripts that do not include config.inc.php
 // want to point to root install dir, need to remove fixed part

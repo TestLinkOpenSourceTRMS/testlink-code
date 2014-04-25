fd1f07aa4c2a1db4cdc12a48519ad9142f291751
fman <francisco.mancardi@gmail.com>
2014-04-25 16:57:32 +0200

    ready for 1.9.10

--- cfg/const.inc.php
+++ cfg/const.inc.php
@@ -24,7 +24,7 @@ define('TL_SMARTY_VERSION',3);  // @since 1.9.8
 
 /** TestLink Release version (MUST BE changed before the release day) */
 define('TL_VERSION_NUMBER', '1.9.10'); 
-define('TL_VERSION', 'Dev' . TL_VERSION_NUMBER . '(El D1eG0) Target Release Date 2013-04-01'); 
+define('TL_VERSION', TL_VERSION_NUMBER . '(El D1eG0)'); 
 define('TL_FACE_DIR', 'prague'); 
 
 /** Last Database version that is used to give users feedback about necesssary upgrades

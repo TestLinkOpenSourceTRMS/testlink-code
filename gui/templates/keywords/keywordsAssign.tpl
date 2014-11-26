4ca8ce80afd93402a78f9708e49aaebd99a055fb
fman <francisco.mancardi@gmail.com>
2014-11-26 20:18:37 +0100

    TICKET 6797: Assign keyword AT TEST SUITE LEVEL - add option to assign keywords ONLY TO FILTERED TEST CASES

--- gui/templates/keywords/keywordsAssign.tpl
+++ gui/templates/keywords/keywordsAssign.tpl
@@ -2,14 +2,15 @@
 TestLink Open Source Project - http://testlink.sourceforge.net/ 
 @filesource keywordsAssign.tpl
 *}
-{lang_get var="labels" s='keyword_assignment,keyword_assignment_empty_tsuite'}
+{lang_get var="labels" s='keyword_assignment,keyword_assignment_empty_tsuite,
+                          btn_save,assignToFilteredTestCases'}
 
 
 {include file="inc_head.tpl" openHead='yes'}
 <script language="JavaScript" src="gui/javascript/OptionTransfer.js" type="text/javascript"></script>
 <script language="JavaScript" src="gui/javascript/expandAndCollapseFunctions.js" type="text/javascript"></script>
 
-{if $can_do} 
+{if $gui->can_do} 
 <script type="text/javascript" language="JavaScript">
 var {$opt_cfg->js_ot_name} = new OptionTransfer("{$opt_cfg->from->name}","{$opt_cfg->to->name}");
 {$opt_cfg->js_ot_name}.saveRemovedLeftOptions("{$opt_cfg->js_ot_name}_removedLeft");
@@ -23,25 +24,33 @@ var {$opt_cfg->js_ot_name} = new OptionTransfer("{$opt_cfg->from->name}","{$opt_
 </head>
 
 <body 
-{if $can_do} 
+{if $gui->can_do} 
 	onLoad="{$opt_cfg->js_ot_name}.init(document.forms[0])"
 {/if}	
 >
 
-{* improved feedback *}
 <div class="workBack">
+ {include file="inc_update.tpl" result=$sqlResult item=$level action='updated'}
+
   <h1 class="title">{$labels.keyword_assignment}</h1>
-  {if $keyword_assignment_subtitle neq ''}
-    <h2>{$keyword_assignment_subtitle|escape}</h2>
+  {if $gui->keyword_assignment_subtitle neq ''}
+    <h2>{$gui->keyword_assignment_subtitle|escape}</h2>
   {/if}
 
-	{if $can_do} 
-    {include file="inc_update.tpl" result=$sqlResult item=$level action='updated'}
+	{if $gui->can_do} 
     <div style="margin-top: 25px;">
-    	<form method="post" action="lib/keywords/keywordsAssign.php?id={$data}&amp;edit={$level}">
+    	<form method="post" action="lib/keywords/keywordsAssign.php?id={$gui->id}&amp;edit={$gui->level}">
+        <input type="hidden" name="form_token" id="form_token" value="{$gui->form_token}"> 
+
+      {if $gui->level == 'testsuite'}
+        {$labels.assignToFilteredTestCases}
+        <input type="checkbox" name="useFilteredSet" id="useFilteredSet" value="1" 
+               {if $gui->useFilteredSet} checked {/if} />
+      {/if}
+
       {include file="opt_transfer.inc.tpl" option_transfer=$opt_cfg}
 	    <br />
-    	<input type="submit" name="assign{$level}" value="{lang_get s='btn_save'}" />
+    	<input type="submit" name="assign{$gui->level}" id="assign{$gui->level}" value="{$labels.btn_save}" />
     	</form>
     </div>
   {else}
--- lib/keywords/keywordsAssign.php
+++ lib/keywords/keywordsAssign.php
@@ -3,15 +3,13 @@
  * TestLink Open Source Project - http://testlink.sourceforge.net/ 
  * This script is distributed under the GNU General Public License 2 or later. 
  *
- * @filesource	keywordsAssign.php
- * @package 	TestLink
- * @copyright 	2007-2012, TestLink community 
- * @link 		http://www.teamst.org/index.php
+ * @filesource  keywordsAssign.php
+ * @package     TestLink
+ * @copyright   2007-2014, TestLink community 
+ * @link        http://www.testlink.org/
  * 
  * @internal revisions
- * @since 1.9.4
- * 20120623 - franciscom -	TICKET 5074: Bulk Keyword assignment - 
- *							All keywords are assigned to the test case instead of the selected keyword
+ * @since 1.9.13
  *
 **/
 require_once("../../config.inc.php");
@@ -25,11 +23,13 @@ $opt_cfg = opt_transf_empty_cfg();
 $opt_cfg->js_ot_name = 'ot';
 $args = init_args($opt_cfg);
 
+$gui = initializeGui($args);
+
 if ($args->edit == 'testproject')
 {
-	// We can NOT assign/remove keywords on a whole test project
-	show_instructions('keywordsAssign');
-	exit();
+  // We can NOT assign/remove keywords on a whole test project
+  show_instructions('keywordsAssign');
+  exit();
 }
 
 $smarty = new TLSmarty();
@@ -37,10 +37,6 @@ $tproject_mgr = new testproject($db);
 $tcase_mgr = new testcase($db);
 
 $result = null;
-$keyword_assignment_subtitle = null;
-$can_do = 0;
-// $itemID = null;
-
 
 // Important Development Notice
 // option transfer do the magic on GUI, 
@@ -52,100 +48,136 @@ $opt_cfg->additional_global_lbl = null;
 $opt_cfg->from->lbl = lang_get('available_kword');
 $opt_cfg->from->map = $tproject_mgr->get_keywords_map($args->testproject_id);
 
-if ($args->edit == 'testsuite')
+switch($args->edit)
 {
-	$opt_cfg->to->lbl = lang_get('kword_to_be_assigned_to_testcases');
-	$opt_cfg->to->map = null;
 
-	// We are going to walk all test suites contained
-	// in the selected container, and assign/remove keywords on each test case.
-	$tsuite_mgr = new testsuite($db);
-	$testsuite = $tsuite_mgr->get_by_id($args->id);
-	$keyword_assignment_subtitle = lang_get('test_suite') . TITLE_SEP . $testsuite['name'];
-	$tcs = $tsuite_mgr->get_testcases_deep($args->id,'only_id');
-	if( ($loop2do = sizeof($tcs)) )
-	{
-		$can_do = 1;
-		if ($args->bAssignTestSuite)
-		{
-			$result = 'ok';
-			for($idx = 0; $idx < $loop2do; $idx++)
-			{
-				if(is_null($args->keywordArray))
-				{	
-					$tcase_mgr->deleteKeywords($tcs[$idx]);
-				}
-				else
-				{
-					$tcase_mgr->addKeywords($tcs[$idx],$args->keywordArray);
-				}	
-			}
-		}
-	}
-}
-else if($args->edit == 'testcase')
-{
-	$doRecall = true;
-	$can_do = 1;
-	$tcData = $tcase_mgr->get_by_id($args->id);
-	if (sizeof($tcData))
-	{
-   		$keyword_assignment_subtitle = lang_get('test_case') . TITLE_SEP . $tcData[0]['name'];
-	}
-	if($args->bAssignTestCase)
-	{
-		$result = 'ok';
-		$tcase_mgr->setKeywords($args->id,$args->keywordArray);
-		$doRecall = !is_null($args->keywordArray);	
-	}
-	$opt_cfg->to->lbl = lang_get('assigned_kword');
-	$opt_cfg->to->map = $doRecall ? $tcase_mgr->get_keywords_map($args->id,
-		                              array('orderByClause' =>" ORDER BY keyword ASC ")) : null;
-}
+  case 'testsuite':
+    $opt_cfg->to->lbl = lang_get('kword_to_be_assigned_to_testcases');
+    $opt_cfg->to->map = null;
 
-keywords_opt_transf_cfg($opt_cfg, $args->keywordList);
-$smarty->assign('can_do', $can_do);
-$smarty->assign('sqlResult', $result);
-$smarty->assign('data', $args->id);
-$smarty->assign('level', $args->edit);
-$smarty->assign('opt_cfg', $opt_cfg);
-$smarty->assign('keyword_assignment_subtitle',$keyword_assignment_subtitle);
-$smarty->display($templateCfg->template_dir . $templateCfg->default_template);
+    // We are going to walk all test suites contained
+    // in the selected container, and assign/remove keywords on each test case.
+    $tsuite_mgr = new testsuite($db);
+    $testsuite = $tsuite_mgr->get_by_id($args->id);
+    $gui->keyword_assignment_subtitle = lang_get('test_suite') . TITLE_SEP . $testsuite['name'];
 
-function init_args(&$opt_cfg)
-{
-	$rl_html_name = $opt_cfg->js_ot_name . "_newRight";
-	
-    $iParams = array(
-			"id" => array(tlInputParameter::INT_N),
-			"edit" => array(tlInputParameter::STRING_N,0,100),
-			"assigntestcase" => array(tlInputParameter::STRING_N,0,1),
-    		"assigntestsuite" => array(tlInputParameter::STRING_N,0,1),
-    		$rl_html_name => array(tlInputParameter::STRING_N),
-    );
-		
-	$pParams = R_PARAMS($iParams);
-    
-	$args = new stdClass();
-    $args->id = $pParams["id"];
-    $args->edit = $pParams["edit"];
-    $args->bAssignTestCase = ($pParams["assigntestcase"] != "") ? 1 : 0;
-    $args->bAssignTestSuite = ($pParams["assigntestsuite"] != "") ? 1 : 0;
-    $args->testproject_id = isset($_SESSION['testprojectID']) ? $_SESSION['testprojectID'] : 0;
+    if($args->useFilteredSet)
+    {
+      $tcs = $args->tcaseSet;    
+    } 
+    else
+    {
+      $tcs = $tsuite_mgr->get_testcases_deep($args->id,'only_id');
+    } 
 
-    $args->keywordArray = null;
-    $args->keywordList = $pParams[$rl_html_name];
-    if ($args->keywordList != "")
-    {	
-    	$args->keywordArray = explode(",",$args->keywordList);
+    if( ($loop2do = sizeof($tcs)) )
+    {
+      $gui->can_do = 1;
+      if ($args->assignToTestSuite)
+      {
+        $result = 'ok';
+        for($idx = 0; $idx < $loop2do; $idx++)
+        {
+          if(is_null($args->keywordArray))
+          { 
+            $tcase_mgr->deleteKeywords($tcs[$idx]);
+          }
+          else
+          {
+            $tcase_mgr->addKeywords($tcs[$idx],$args->keywordArray);
+          } 
+        }
+      }
+    }
+  break;
+
+
+  case 'testcase':
+    $doRecall = true;
+    $gui->can_do = 1;
+    $tcData = $tcase_mgr->get_by_id($args->id);
+    if (sizeof($tcData))
+    {
+      $gui->keyword_assignment_subtitle = lang_get('test_case') . TITLE_SEP . $tcData[0]['name'];
     }
 
-        
-    return $args;
+    if($args->assignToTestCase)
+    {
+      $result = 'ok';
+      $tcase_mgr->setKeywords($args->id,$args->keywordArray);
+      $doRecall = !is_null($args->keywordArray);  
+    }
+    $opt_cfg->to->lbl = lang_get('assigned_kword');
+    $opt_cfg->to->map = $doRecall ? $tcase_mgr->get_keywords_map($args->id,
+                                    array('orderByClause' =>" ORDER BY keyword ASC ")) : null;
+  break;
 }
+
+
+keywords_opt_transf_cfg($opt_cfg, $args->keywordList);
+
+
+$smarty->assign('gui', $gui);
+$smarty->assign('sqlResult', $result);
+$smarty->assign('opt_cfg', $opt_cfg);
+$smarty->display($templateCfg->template_dir . $templateCfg->default_template);
+
+
+/**
+ *
+ */ 
+function init_args(&$opt_cfg)
+{
+  $rl_html_name = $opt_cfg->js_ot_name . "_newRight";
+  
+  $iParams = array("id" => array(tlInputParameter::INT_N),
+                   "edit" => array(tlInputParameter::STRING_N,0,100),
+                   "assigntestcase" => array(tlInputParameter::STRING_N,0,1),
+                   "assigntestsuite" => array(tlInputParameter::STRING_N,0,1),
+                   $rl_html_name => array(tlInputParameter::STRING_N) );
+    
+  $pParams = R_PARAMS($iParams);
+    
+  $args = new stdClass();
+  $args->id = $pParams["id"];
+  $args->edit = $pParams["edit"];
+  $args->assignToTestCase = ($pParams["assigntestcase"] != "") ? 1 : 0;
+  $args->assignToTestSuite = ($pParams["assigntestsuite"] != "") ? 1 : 0;
+  $args->testproject_id = isset($_SESSION['testprojectID']) ? $_SESSION['testprojectID'] : 0;
+  $args->useFilteredSet = isset($_REQUEST['useFilteredSet']) ? 1 : 0;                     
+
+  $args->form_token = isset($_REQUEST['form_token']) ? $_REQUEST['form_token'] : 0;
+  $args->tcaseSet = isset($_SESSION['edit_mode']) && isset($_SESSION['edit_mode'][$args->form_token]['testcases_to_show']) ? 
+                    $_SESSION['edit_mode'][$args->form_token]['testcases_to_show'] : null;
+  
+
+  $args->keywordArray = null;
+  $args->keywordList = $pParams[$rl_html_name];
+  if ($args->keywordList != "")
+  { 
+    $args->keywordArray = explode(",",$args->keywordList);
+  }
+
+  return $args;
+}
+
+/**
+ *
+ */
+function initializeGui(&$argsObj)
+{
+  $guiObj = new stdClass();
+  $guiObj->can_do = 0;
+  $guiObj->form_token = $argsObj->form_token;
+  $guiObj->useFilteredSet = $argsObj->useFilteredSet;
+  $guiObj->id = $argsObj->id;
+  $guiObj->level = $argsObj->edit;
+  $guiObj->keyword_assignment_subtitle = null;
+  return $guiObj;
+}
+
 
 function checkRights(&$db,&$user)
 {
-	return ($user->hasRight($db,'mgt_modify_key') && $user->hasRight($db,'mgt_view_key'));
+  return ($user->hasRight($db,'mgt_modify_key') && $user->hasRight($db,'mgt_view_key'));
 }
-?>

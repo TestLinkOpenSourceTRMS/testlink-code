2fc4d3f5f12c518b622e5f05d88a5ad8431d1099
Francisco Mancardi <francisco.mancardi@gmail.com>
2015-03-14 15:46:05 +0100

    remove support files commited by error

--- save.sh
+++
@@ -1,8 +0,0 @@
-mkdir /development/bkp/
-cp lib/attachments/attachmentdownload.php /development/bkp/
-cp lib/execute/execSetResults.php /development/bkp/
-cp lib/functions/common.php /development/bkp/
-cp lib/functions/exec.inc.php /development/bkp/
-cp lib/functions/object.class.php /development/bkp/
-cp lib/functions/testplan.class.php /development/bkp/
-cp lnl.php /development/bkp/

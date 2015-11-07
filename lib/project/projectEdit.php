1cad583d1957efd09434e7fbcea50312ef12894d
Francisco Mancardi <francisco.mancardi@gmail.com>
2015-11-07 14:41:48 +0100

    PRIVATE TICKET 7332: XSS - Test project notes

--- lib/project/projectEdit.php
+++ lib/project/projectEdit.php
@@ -293,7 +293,18 @@ function init_args($tprojectMgr,$request_hash, $session_tproject_id)
     }
   }
   
-  
+  // sanitize output via black list
+  if($args->notes != '')
+  {
+    // The Black List - Jon Bokenkamp
+    $bl = array('<script>','</script>');
+    foreach($bl as $tg)
+    {
+      $cl[] = htmlentities($tg);
+    }  
+    $args->notes = str_replace($bl,$cl,$args->notes);
+  }  
+
   $args->user = isset($_SESSION['currentUser']) ? $_SESSION['currentUser'] : null;
   $args->userID = intval(isset($_SESSION['userID']) ? intval($_SESSION['userID']) : 0);
   $args->testprojects = null;

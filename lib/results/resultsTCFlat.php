a2be8da2711407972782aac928f84271c413d673
Francisco Mancardi <francisco.mancardi@gmail.com>
2016-03-13 12:22:32 +0100

    minor user information improvement

--- lib/results/resultsTCFlat.php
+++ lib/results/resultsTCFlat.php
@@ -295,7 +295,7 @@ function createSpreadsheet($gui,$args)
                            'notes' => null, 'date_time_run' => null, 'execution_duration' => null,
                            'testproject' => null,'generated_by_TestLink_on' => null,'testplan' => null,
                            'result_on_last_build' => null,'last_execution' => null,
-                           'assigned_to' => null,'tcexec_result' => null,
+                           'assigned_to' => null,'tcexec_latest_exec_result' => null,
                            'version' => null,'execution_type' => null));
 
   $buildIDSet = $args->builds->idSet;
@@ -376,7 +376,7 @@ function createSpreadsheet($gui,$args)
   $gui->filterFeedback = null;
   $dataHeader[] = $lbl['build'];
   $dataHeader[] = $lbl['assigned_to'];
-  $dataHeader[] = $lbl['tcexec_result'];
+  $dataHeader[] = $lbl['tcexec_latest_exec_result'];
   $dataHeader[] = $lbl['date_time_run'];
   $dataHeader[] = $lbl['test_exec_by'];
   $dataHeader[] = $lbl['notes'];

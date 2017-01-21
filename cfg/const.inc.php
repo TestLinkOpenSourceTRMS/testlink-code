7648698b3139f441b18a287239d514bc72996bfe
Francisco Mancardi <francisco.mancardi@gmail.com>
2017-01-21 11:40:01 +0100

    Ready to release 1.9.16 Moka Pot

--- cfg/const.inc.php
+++ cfg/const.inc.php
@@ -22,7 +22,7 @@ define('TL_SMARTY_VERSION',3);  // @since 1.9.8
 
 /** TestLink Release version (MUST BE changed before the release day) */
 define('TL_VERSION_NUMBER', '1.9.16'); 
-define('TL_VERSION', TL_VERSION_NUMBER . '[DEV] (Moka pot)'); 
+define('TL_VERSION', TL_VERSION_NUMBER . ' (Moka pot)'); 
 define('TL_FACE_DIR', 'prague'); 
 
 /** Latest Database version that is used to give users feedback about necesssary upgrades

c8550beadcdd64860ae1e91a811a12d13240c998
Bob Le Bricodeur <mr.bricodage@gmail.com>
2018-01-07 23:08:13 +0100

    Create new right for custom field assignment
    cfield_assignment, value 53
    add right to roles that have right 18 cfield_management)

--- install/sql/mssql/testlink_create_default_data.sql
+++ install/sql/mssql/testlink_create_default_data.sql
@@ -98,6 +98,7 @@ INSERT INTO /*prefix*/rights (id,description) VALUES (49,'exec_ro_access');
 INSERT INTO /*prefix*/rights (id,description) VALUES (50,'monitor_requirement');
 INSERT INTO /*prefix*/rights (id,description) VALUES (51,'codetracker_management');
 INSERT INTO /*prefix*/rights (id,description) VALUES (52,'codetracker_view');
+INSERT INTO /*prefix*/rights (id,description) VALUES (53,'cfield_assignment');
 
 SET IDENTITY_INSERT /*prefix*/rights OFF
 
@@ -153,6 +154,7 @@ INSERT INTO /*prefix*/role_rights (role_id,right_id) VALUES (8,48);
 INSERT INTO /*prefix*/role_rights (role_id,right_id) VALUES (8,50);
 INSERT INTO /*prefix*/role_rights (role_id,right_id) VALUES (8,51);
 INSERT INTO /*prefix*/role_rights (role_id,right_id) VALUES (8,52);
+INSERT INTO /*prefix*/role_rights (role_id,right_id) VALUES (8,53);
 
 --  Rights for guest role
 INSERT INTO /*prefix*/role_rights (role_id,right_id) VALUES (5,3);

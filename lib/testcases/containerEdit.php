1448d9c49d7dfbed15156ba7078ed3024a297781
fmancardi <francisco.mancardi@gmail.com>
2018-10-18 07:42:36 +0200

    isssue reported via email by bitnami

--- lib/testcases/containerEdit.php
+++ lib/testcases/containerEdit.php
@@ -478,8 +478,15 @@ function init_args(&$dbHandler,&$tprojectMgr,&$tsuiteMgr,$optionTransferCfg) {
   }
 
   if( intval($args->tprojectID) == 0 ) {
-    $args->tprojectID = intval($_REQUEST['tproject_id']);
+    if( isset($_REQUEST['tproject_id']) ) {
+      $args->tprojectID = intval($_REQUEST['tproject_id']);
+    }
+
+    if( isset($_REQUEST['tprojectID']) ) {
+      $args->tprojectID = intval($_REQUEST['tprojectID']);
+    }    
   }
+
   // very ugly
   $args->level = null; 
   if( isset($_REQUEST['delete_testsuite']) ) {

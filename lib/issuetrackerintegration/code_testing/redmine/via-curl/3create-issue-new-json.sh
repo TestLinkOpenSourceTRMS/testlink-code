4c6f66f88a952c5e30cb4779997de77a1cb3ea98
fmancardi <francisco.mancardi@gmail.com>
2018-10-21 10:34:28 +0200

    code layout

--- lib/issuetrackerintegration/code_testing/redmine/via-curl/3create-issue-new-json.sh
+++ lib/issuetrackerintegration/code_testing/redmine/via-curl/3create-issue-new-json.sh
@@ -1,5 +1,9 @@
+ # 
  curl 'https://testlinktest.planio.com/issues.json' \
   -X POST \
   -H 'X-Redmine-API-Key: c1796c2215a55723f134a4c546e9c121' \
   -H 'Content-Type: application/json' \
-  -d '{ "issue": {"subject": "A new issue 20180220", "project_id": "website-redesign"} }'
+  -d '{ 
+  "issue": 
+  {"subject": "issue 20181020.02", 
+   "project_id": "website-redesign"} }'

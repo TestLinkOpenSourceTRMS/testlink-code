b88c0b7b861bd067094b38494c255acd5c56a324
Francisco Mancardi <francisco.mancardi@gmail.com>
2019-02-02 17:51:47 +0100

    Refactoring to avoid warnings in TestLink event viewer

--- lib/platforms/platformsView.php
+++ lib/platforms/platformsView.php
@@ -3,13 +3,9 @@
  * TestLink Open Source Project - http://testlink.sourceforge.net/ 
  * This script is distributed under the GNU General Public License 2 or later. 
  *
- * Filename $RCSfile: platformsView.php,v $
- *
- * @version $Revision: 1.7 $
- * @modified $Date: 2010/09/12 15:16:09 $ by $Author: franciscom $
+ * @filesource: platformsView.php
  *
  * allows users to manage platforms. 
- * @internal revisions
  */
 require_once("../../config.inc.php");
 require_once("common.php");
@@ -20,14 +16,7 @@ $args = init_args();
 
 $platform_mgr = new tlPlatform($db, $args->testproject_id);
 
-$gui = new stdClass();
-$gui->platforms = $platform_mgr->getAll(array('include_linked_count' => true));
-
-$gui->canManage = $args->currentUser->hasRight($db,"platform_management");
-$gui->user_feedback = null;
-$cfg = getWebEditorCfg('platform');
-$gui->editorType = $cfg['type'];
-	  
+$gui = $platform_mgr->initViewGui($args->currentUser);	  
 
 $smarty = new TLSmarty();
 $smarty->assign('gui',$gui);
@@ -38,8 +27,7 @@ $smarty->display($templateCfg->template_dir . $templateCfg->default_template);
  * 
  *
  */
-function init_args()
-{
+function init_args() {
 	$args = new stdClass();
 	$args->testproject_id = isset($_SESSION['testprojectID']) ? $_SESSION['testprojectID'] : 0;
 	$args->currentUser = $_SESSION['currentUser']; 
@@ -47,8 +35,10 @@ function init_args()
 	return $args;
 }
 
-function checkRights(&$db,&$user)
-{
+/**
+ * 
+ *
+ */
+function checkRights(&$db,&$user) {
 	return ($user->hasRight($db,'platform_management') || $user->hasRight($db,'platform_view'));
 }
-?>

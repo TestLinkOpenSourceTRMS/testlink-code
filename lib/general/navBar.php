ea0c2190c037bad59ca20a5e41d908bf1f0a1482
Francisco Mancardi <francisco.mancardi@gmail.com>
2019-02-02 17:30:30 +0100

    TestLink event viewer warnings

--- lib/general/navBar.php
+++ lib/general/navBar.php
@@ -139,7 +139,7 @@ function initializeGui(&$db,&$args)
         }
       }
 
-      if( $testPlanFound == 0 ) {
+      if( $testPlanFound == 0 && is_array($testPlanSet) ) {
         $tplanID = $testPlanSet[0]['id'];
         setSessionTestPlan($testPlanSet[0]);      
       } 
@@ -170,7 +170,7 @@ function initializeGui(&$db,&$args)
 
     $ckCfg = config_get('cookie');    
     $ckObj = new stdClass();
-    $ckObj->name = $ckObj->prefix . 'TL_lastTestProjectForUserID_'. 
+    $ckObj->name = $ckCfg->prefix . 'TL_lastTestProjectForUserID_'. 
                    $args->user->dbID;
     $ckObj->value = $args->testproject;
     tlSetCookie($ckObj);

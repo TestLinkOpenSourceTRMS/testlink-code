7832694a75e7775e02eb9ad9eaddaecfecfdf99a
Francisco Mancardi <francisco.mancardi@gmail.com>
2019-02-17 11:43:55 +0100

    . #0008569: Test Steps Partial Execution - DB Changes - Postgres

--- install/sql/postgres/testlink_create_tables.sql
+++ install/sql/postgres/testlink_create_tables.sql
@@ -223,6 +223,26 @@ CREATE TABLE /*prefix*/execution_tcsteps (
 );
 CREATE UNIQUE INDEX /*prefix*/execution_tcsteps_uidx1 ON  /*prefix*/execution_tcsteps ("execution_id","tcstep_id");
 
+
+--
+-- Table structure for table "execution_tcsteps_wip"
+--
+CREATE TABLE /*prefix*/execution_tcsteps_wip (
+   "id" BIGSERIAL NOT NULL ,
+   "tcstep_id" INTEGER NOT NULL DEFAULT '0' REFERENCES  /*prefix*/tcsteps (id),
+   "testplan_id" int(10) INTEGER NOT NULL DEFAULT '0' REFERENCES  /*prefix*/testplans (id),
+   "platform_id" int(10) INTEGER NOT NULL DEFAULT '0',
+   "build_id" INTEGER NOT NULL DEFAULT '0',
+   "tester_id" BIGINT NULL DEFAULT NULL,
+   "creation_ts" TIMESTAMP NOT NULL DEFAULT now(),
+   "notes" TEXT NULL DEFAULT NULL,
+   "status" CHAR(1) NULL DEFAULT NULL,
+  PRIMARY KEY ("id")
+);
+CREATE UNIQUE INDEX /*prefix*/execution_tcsteps_wip_uidx1 ON  /*prefix*/execution_tcsteps_wip ("tcstep_id","testplan_id","platform_id","build_id");
+
+
+
 --
 -- Table structure for table "testplan_tcversions"
 --

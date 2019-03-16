a017e96990c9dfb2ef6d4fc0ecf299b26e8831e6
Francisco Mancardi <francisco.mancardi@gmail.com>
2019-03-16 10:02:29 +0100

    code layout

--- third_party/ckeditor/ckeditor.class.php
+++ third_party/ckeditor/ckeditor.class.php
@@ -3,39 +3,42 @@
  * TestLink Open Source Project - http://testlink.sourceforge.net/ 
  * This script is distributed under the GNU General Public License 2 or later. 
  *
- * Filename $RCSfile: ckeditor.class.php,v $
+ * @filesource ckeditor.class.php
  *
-
  **/
 
 require_once("../../third_party/ckeditor/ckeditor.php");
 
-class ckeditorInterface
-{
+class ckeditorInterface {
 	var $InstanceName ;
 	var $Value ;
 	var $Editor ;
 	var $config ;
 
-	function __construct($instanceName)
-	{
+  /**
+   *
+   */
+	function __construct($instanceName) {
 		$this->InstanceName	= $instanceName;
 		$this->Value		= '';
 		$this->Editor		= new CKEditor();
 		$this->Editor->returnOutput = true;
 	}
   
- 	function Create()
-	{
+  /**
+   *
+   */
+ 	function Create() {
 		echo $this->CreateHtml($rows,$cols);
 	}
 
-	function CreateHtml()
-	{
+  /**
+   *
+   */
+	function CreateHtml() {
 		$Html = $this->Editor->editor($this->InstanceName, $this->Value, $config = array());
 		
 		return $Html ;
 	}
 
 } // class end
-?>

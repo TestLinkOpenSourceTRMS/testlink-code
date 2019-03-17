31aa4f46cc5e2b581f16a64b26f7798f4475f064
Francisco Mancardi <francisco.mancardi@gmail.com>
2019-03-17 09:13:56 +0100

    CKEditor version updated

--- third_party/ckeditorWrapper/CKEditorPHPWrapper.php
+++ third_party/ckeditorWrapper/CKEditorPHPWrapper.php
@@ -20,7 +20,8 @@ class CKEditor
 	/**
 	 * The version of %CKEditor.
 	 */
-	const version = '4.6.1';
+	const version = '4.11.3';
+
 	/**
 	 * A constant string unique for each release of %CKEditor.
 	 */
@@ -36,6 +37,7 @@ class CKEditor
 	 * @endcode
 	 */
 	public $basePath;
+
 	/**
 	 * An array that holds the global %CKEditor configuration.
 	 * For the list of available options, see http://docs.cksource.com/ckeditor_api/symbols/CKEDITOR.config.html
@@ -48,13 +50,15 @@ class CKEditor
 	 * @endcode
 	 */
 	public $config = array();
-	/**
+	
+  /**
 	 * A boolean variable indicating whether CKEditor has been initialized.
 	 * Set it to true only if you have already included
 	 * &lt;script&gt; tag loading ckeditor.js in your website.
 	 */
 	public $initialized = false;
-	/**
+	
+  /**
 	 * Boolean variable indicating whether created code should be printed out or returned by a function.
 	 *
 	 * Example 1: get the code creating %CKEditor instance and print it on a page with the "echo" function.
@@ -67,23 +71,27 @@ class CKEditor
 	 * @endcode
 	 */
 	public $returnOutput = false;
-	/**
+	
+  /**
 	 * An array with textarea attributes.
 	 *
 	 * When %CKEditor is created with the editor() method, a HTML &lt;textarea&gt; element is created,
 	 * it will be displayed to anyone with JavaScript disabled or with incompatible browser.
 	 */
 	public $textareaAttributes = array( "rows" => 8, "cols" => 60 );
-	/**
+	
+  /**
 	 * A string indicating the creation date of %CKEditor.
 	 * Do not change it unless you want to force browsers to not use previously cached version of %CKEditor.
 	 */
 	public $timestamp = "GB8C";
-	/**
+	
+  /**
 	 * An array that holds event listeners.
 	 */
 	private $events = array();
-	/**
+	
+  /**
 	 * An array that holds global event listeners.
 	 */
 	private $globalEvents = array();
@@ -432,8 +440,7 @@ class CKEditor
 	/**
 	 * Initializes CKEditor (executed only once).
 	 */
-	private function init()
-	{
+	private function init() {
 		static $initComplete;
 		$out = "";
 

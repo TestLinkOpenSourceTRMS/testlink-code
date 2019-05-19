fdcf63d422c23d71ff66d77621784839887ac1ba
Francisco Mancardi <francisco.mancardi@gmail.com>
2019-05-19 17:56:29 +0200

    improvement: manage smarty template dir config using array

--- lib/functions/tlsmarty.inc.php
+++ lib/functions/tlsmarty.inc.php
@@ -90,8 +90,9 @@ class TLSmarty extends Smarty {
     
     parent::__construct();
   
-    $this->template_dir = TL_ABS_PATH . 'gui/templates/' . 
-                          $tlCfg->gui->ux . '/';
+    $this->template_dir = 
+      array('main' => TL_ABS_PATH . 'gui/templates/' . 
+                      $tlCfg->gui->ux . '/');
                           
     $this->config_dir = TL_ABS_PATH . 'gui/templates/conf';
     $this->compile_dir = TL_TEMP_PATH;

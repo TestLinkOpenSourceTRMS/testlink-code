5722feddbc1510a95cd7b81a2c08b60f219818da
Francisco Mancardi <francisco.mancardi@gmail.com>
2019-09-19 17:18:21 +0200

    fix: #0008770: Uknown PHP sprtinf called

--- lib/functions/testcase.class.php
+++ lib/functions/testcase.class.php
@@ -2011,7 +2011,7 @@ class testcase extends tlObjectWithAttachments {
                   $pfx = $pfx[0] . $this->cfg->testcase->glue_character . $tcversion['tc_external_id'];
 
                   foreach($stepsSet as $key => $step) {
-                    $act = sprtinf(self::GHOSTSTEPMASK,$step['step_number'],
+                    $act = sprintf(self::GHOSTSTEPMASK,$step['step_number'],
                                    $pfx,$tcversion['version']); 
 
                     $this->create_step($to_tcversion_id,

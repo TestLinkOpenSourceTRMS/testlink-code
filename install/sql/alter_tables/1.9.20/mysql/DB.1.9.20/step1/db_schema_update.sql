e0f52961e721b6db5615dd6c70cd3b9d0f1b5706
heuj <58398276+heuj@users.noreply.github.com>
2020-01-21 07:34:18 +0100

    Update db_schema_update.sql

--- install/sql/alter_tables/1.9.20/mysql/DB.1.9.20/step1/db_schema_update.sql
+++ install/sql/alter_tables/1.9.20/mysql/DB.1.9.20/step1/db_schema_update.sql
@@ -41,8 +41,8 @@ CREATE TABLE /*prefix*/baseline_l1l2_context (
   id int(10) unsigned NOT NULL AUTO_INCREMENT,
   testplan_id int(10) unsigned NOT NULL DEFAULT '0',
   platform_id int(10) unsigned NOT NULL DEFAULT '0',
-  begin_exec_ts timestamp NOT NULL,
-  end_exec_ts timestamp NOT NULL,
+  begin_exec_ts timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  end_exec_ts timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   creation_ts timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (id),
   UNIQUE KEY udx1 (testplan_id,platform_id,creation_ts)

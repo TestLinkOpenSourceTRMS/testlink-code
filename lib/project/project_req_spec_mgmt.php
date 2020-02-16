35304d1047d7c7605ccb6f1888655d7e4605312c
Francisco Mancardi <francisco.mancardi@gmail.com>
2020-02-16 12:01:26 +0100

    wip

--- lib/project/project_req_spec_mgmt.php
+++ lib/project/project_req_spec_mgmt.php
@@ -4,31 +4,53 @@
  * This script is distributed under the GNU General Public License 2 or later.
  *
  * @filesource	project_req_spec_mgmt.php
- * @author 		Martin Havlat
  *
- * @internal revisions
- * @since 1.9.10
  */
 require_once('../../config.inc.php');
 require_once('common.php');
 testlinkInitPage($db,false,false,"checkRights");
 
-$tproject_id   = isset($_SESSION['testprojectID']) ? intval($_SESSION['testprojectID']) : 0;
-$tproject_name = isset($_SESSION['testprojectName']) ? $_SESSION['testprojectName'] : 'undefined';
 
+$args = init_args($db);
 $gui = new stdClass();
-$gui->main_descr = lang_get('testproject') .  TITLE_SEP . $tproject_name . TITLE_SEP . lang_get('title_req_spec');
-$gui->tproject_id = $tproject_id;
+$gui->tproject_name = $args->tproject_name;
+$gui->main_descr = lang_get('testproject') .  TITLE_SEP . 
+                    $gui->tproject_name . TITLE_SEP . 
+                    lang_get('title_req_spec');
+$gui->tproject_id = $args->tproject_id;
 $gui->refresh_tree = 'no';
 
-$uo = $_SESSION['currentUser'];
-$gui->grants = new stdClass();
-$gui->grants->modify = $uo->hasRight($db,'mgt_modify_req');
-$gui->grants->ro = $uo->hasRight($db,'mgt_view_req');
+$gui->userGrants = new stdClass();
+$gui->userGrants->modify = $args->user->hasRight($db,'mgt_modify_req');
+$gui->userGrants->ro = $args->user->hasRight($db,'mgt_view_req');
 
 $smarty = new TLSmarty();
 $smarty->assign('gui', $gui);
 $smarty->display('requirements/project_req_spec_mgmt.tpl');
+
+/**
+ *
+ */
+function init_args(&$dbH)
+{
+  list($args,$env) = initContext();
+  $args->id = isset($_REQUEST['id']) ? intval($_REQUEST['id']) : 0;
+
+  if ($args->tproject_id==0) {
+    $args->tproject_id = $args->id;
+  }
+
+  if ($args->tproject_id==0) {
+    throw new Exception("Bad Test Project ID", 1);
+  }
+  
+  $args->tproject_name = testproject::getName($dbH,$args->tproject_id);
+  $args->basehref = $_SESSION['basehref'];
+
+  return $args;
+}
+
+
 
 /**
  *

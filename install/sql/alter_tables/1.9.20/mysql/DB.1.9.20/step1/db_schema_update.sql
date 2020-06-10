561e07b880f05435e511fec24ce92179e4a5028a
kenchan0130 <tt.tanishi100@gmail.com>
2020-06-10 19:53:01 +0900

    apply fmt: triming spaces

--- install/sql/alter_tables/1.9.20/mysql/DB.1.9.20/step1/db_schema_update.sql
+++ install/sql/alter_tables/1.9.20/mysql/DB.1.9.20/step1/db_schema_update.sql
@@ -3,15 +3,15 @@
 # ---------------------------------------------------------------------------------------
 # @filesource db_schema_update.sql
 #
-# SQL script - updates DB schema for MySQL - 
+# SQL script - updates DB schema for MySQL -
 # From TestLink 1.9.19 to 1.9.20
-# 
+#
 #
 -- since 1.9.20
 INSERT INTO /*prefix*/rights (id,description) VALUES (55,'testproject_add_remove_keywords_executed_tcversions');
 INSERT INTO /*prefix*/rights (id,description) VALUES (56,'delete_frozen_tcversion');
 
--- 
+--
 ALTER TABLE /*prefix*/builds ADD COLUMN commit_id varchar(64) NULL;
 ALTER TABLE /*prefix*/builds ADD COLUMN tag varchar(64) NULL;
 ALTER TABLE /*prefix*/builds ADD COLUMN branch varchar(64) NULL;
@@ -20,7 +20,7 @@ ALTER TABLE /*prefix*/builds ADD COLUMN release_candidate varchar(100) NULL;
 --
 ALTER TABLE /*prefix*/users MODIFY password VARCHAR(255) NOT NULL default '';
 
--- 
+--
 ALTER TABLE /*prefix*/testplan_platforms ADD COLUMN active tinyint(1) NOT NULL default '1';
 ALTER TABLE /*prefix*/platforms ADD COLUMN enable_on_design tinyint(1) unsigned NOT NULL default '0';
 ALTER TABLE /*prefix*/platforms ADD COLUMN enable_on_execution tinyint(1) unsigned NOT NULL default '1';
@@ -32,7 +32,7 @@ ALTER TABLE /*prefix*/testcase_keywords ADD INDEX /*prefix*/idx02_testcase_keywo
 ALTER TABLE /*prefix*/milestones MODIFY target_date date NOT NULL;
 ALTER TABLE /*prefix*/milestones MODIFY start_date date DEFAULT NULL;
 
--- 
+--
 CREATE TABLE /*prefix*/execution_tcsteps_wip (
    id int(10) unsigned NOT NULL auto_increment,
    tcstep_id int(10) unsigned NOT NULL default '0',
@@ -84,15 +84,15 @@ CREATE TABLE /*prefix*/baseline_l1l2_details (
 ) DEFAULT CHARSET=utf8;
 
 #
-CREATE OR REPLACE VIEW /*prefix*/latest_exec_by_testplan 
-AS SELECT tcversion_id, testplan_id, MAX(id) AS id 
-FROM /*prefix*/executions 
+CREATE OR REPLACE VIEW /*prefix*/latest_exec_by_testplan
+AS SELECT tcversion_id, testplan_id, MAX(id) AS id
+FROM /*prefix*/executions
 GROUP BY tcversion_id,testplan_id;
 
 #
 CREATE OR REPLACE VIEW /*prefix*/latest_exec_by_context
 AS SELECT tcversion_id, testplan_id,build_id,platform_id,max(id) AS id
-FROM /*prefix*/executions 
+FROM /*prefix*/executions
 GROUP BY tcversion_id,testplan_id,build_id,platform_id;
 
 
@@ -100,40 +100,40 @@ CREATE OR REPLACE VIEW /*prefix*/tcversions_without_platforms
 AS SELECT
    NHTCV.parent_id AS testcase_id,
    NHTCV.id AS id
-FROM /*prefix*/nodes_hierarchy NHTCV 
+FROM /*prefix*/nodes_hierarchy NHTCV
 WHERE NHTCV.node_type_id = 4 AND
 NOT(EXISTS(SELECT 1 FROM /*prefix*/testcase_platforms TCPL
            WHERE TCPL.tcversion_id = NHTCV.id));
 
 CREATE OR REPLACE VIEW /*prefix*/latest_exec_by_testplan_plat
 AS SELECT tcversion_id, testplan_id,platform_id,max(id) AS id
-FROM /*prefix*/executions 
+FROM /*prefix*/executions
 GROUP BY tcversion_id,testplan_id,platform_id;
 
 #
 CREATE OR REPLACE VIEW /*prefix*/tsuites_tree_depth_2
 AS SELECT TPRJ.prefix,
-NHTPRJ.name AS testproject_name,    
+NHTPRJ.name AS testproject_name,
 NHTS_L1.name AS level1_name,
 NHTS_L2.name AS level2_name,
-NHTPRJ.id AS testproject_id, 
-NHTS_L1.id AS level1_id, 
-NHTS_L2.id AS level2_id 
-FROM /*prefix*/testprojects TPRJ 
-JOIN /*prefix*/nodes_hierarchy NHTPRJ 
+NHTPRJ.id AS testproject_id,
+NHTS_L1.id AS level1_id,
+NHTS_L2.id AS level2_id
+FROM /*prefix*/testprojects TPRJ
+JOIN /*prefix*/nodes_hierarchy NHTPRJ
 ON TPRJ.id = NHTPRJ.id
-LEFT OUTER JOIN /*prefix*/nodes_hierarchy NHTS_L1 
+LEFT OUTER JOIN /*prefix*/nodes_hierarchy NHTS_L1
 ON NHTS_L1.parent_id = NHTPRJ.id
 LEFT OUTER JOIN /*prefix*/nodes_hierarchy NHTS_L2
-ON NHTS_L2.parent_id = NHTS_L1.id 
-WHERE NHTPRJ.node_type_id = 1 
+ON NHTS_L2.parent_id = NHTS_L1.id
+WHERE NHTPRJ.node_type_id = 1
 AND NHTS_L1.node_type_id = 2
 AND NHTS_L2.node_type_id = 2;
 
 ##
-CREATE OR REPLACE VIEW /*prefix*/exec_by_date_time 
+CREATE OR REPLACE VIEW /*prefix*/exec_by_date_time
 AS (
-SELECT NHTPL.name AS testplan_name, 
+SELECT NHTPL.name AS testplan_name,
 DATE_FORMAT(E.execution_ts, '%Y-%m-%d') AS yyyy_mm_dd,
 DATE_FORMAT(E.execution_ts, '%Y-%m') AS yyyy_mm,
 DATE_FORMAT(E.execution_ts, '%H') AS hh,

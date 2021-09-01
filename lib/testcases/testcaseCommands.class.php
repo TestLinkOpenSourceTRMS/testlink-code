80240aa08c66503de3e9c1f4776119b47dd8510a
u0113 <francisco.mancardi@tesisquare.com>
2021-09-01 11:24:30 +0200

    documentation

--- lib/testcases/testcaseCommands.class.php
+++ lib/testcases/testcaseCommands.class.php
@@ -1483,7 +1483,7 @@ class testcaseCommands {
   /**
    *
    * @used by tcEdit.php
-   * 
+   * @use tcaseMgr->updateLatestTPlanLinkToTCV()
    */
   function updateTPlanLinkToTCV($argsObj,$request) {
 

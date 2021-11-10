54c6536b1302bf4bda45d538aac6fcd3ab057d5d
u0113 <francisco.mancardi@tesisquare.com>
2021-11-10 16:10:49 +0100

    version additional info updated

--- cfg/const.inc.php
+++ cfg/const.inc.php
@@ -20,7 +20,7 @@ define('TL_SMARTY_VERSION',3);  // @since 1.9.8
 
 /** TestLink Release version (MUST BE changed before the release day) */
 define('TL_VERSION_NUMBER', '1.9.20'); 
-define('TL_VERSION', TL_VERSION_NUMBER . ' [DEV] '); 
+define('TL_VERSION', TL_VERSION_NUMBER . ' [fixed] '); 
 define('TL_FACE_DIR', 'prague'); 
 
 /** Latest Database version that is used to give users feedback 

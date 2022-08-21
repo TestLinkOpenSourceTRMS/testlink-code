4b9343e750346d5111d7c43d8074d5c7bc3499cf
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2022-08-21 22:39:14 +0300

    going to 1.9.21

--- cfg/const.inc.php
+++ cfg/const.inc.php
@@ -19,8 +19,8 @@
 define('TL_SMARTY_VERSION',3);  // @since 1.9.8
 
 /** TestLink Release version (MUST BE changed before the release day) */
-define('TL_VERSION_NUMBER', '1.9.20'); 
-define('TL_VERSION', TL_VERSION_NUMBER . ' [DEV] '); 
+define('TL_VERSION_NUMBER', '1.9.21'); 
+define('TL_VERSION', TL_VERSION_NUMBER . ' [QA] '); 
 define('TL_FACE_DIR', 'prague'); 
 
 /** Latest Database version that is used to give users feedback 

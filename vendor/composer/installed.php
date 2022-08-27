b4916d04f555d1b6b0e708135b284c06fd8d0bdc
u0113 <francisco.mancardi@tesiquare.com>
2022-08-27 17:55:27 +0200

    remove all packages using composer, to update it

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-2.0.0-20220827-PHP8.1',
+        'version' => 'dev-2.0.0-20220827-PHP8.1',
+        'reference' => 'c3b14ad70fe791e5e353b303fa30517fca8ba5fe',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-2.0.0-20220827-PHP8.1',
+            'version' => 'dev-2.0.0-20220827-PHP8.1',
+            'reference' => 'c3b14ad70fe791e5e353b303fa30517fca8ba5fe',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

d709ae5d18e84d62bf39351a43896d4bb8ce367a
u0113 <francisco.mancardi@tesiquare.com>
2022-08-27 18:33:41 +0200

    is_open attribute

--- lib/platforms/platformsEdit.php
+++ lib/platforms/platformsEdit.php
@@ -5,8 +5,11 @@
  *
  * @filesource  platformsEdit.php
  * @package     TestLink
- * @copyright   2009-2020, TestLink community 
- * @link        http://www.testlink.org/
+ * @copyright   2009-2022, TestLink community 
+ * @link        http://www.testlink.org 
+ * @link        http://mantis.testlink.org 
+ * @link        https://github.com/TestLinkOpenSourceTRMS/testlink-code 
+ * 
  *
  * allows users to manage platforms. 
  *
@@ -53,10 +56,14 @@ switch ($args->doAction) {
   case "enableDesign":
   case "disableExec":
   case "enableExec":
+  case "openForExec":
+  case "closeForExec":      
     $platform_mgr->$method($args->platform_id);
     
+    // optimistic
     $op = new stdClass();
     $op->status = 1;
+    $op->user_feedback = '';
     $op->template = 'platformsView.tpl';
   break;
 }
@@ -85,6 +92,7 @@ $smarty->display($tplCfg->template_dir . $tpl);
 function initEnv(&$dbHandler) {
   testlinkInitPage($dbHandler);
   $argsObj = init_args($dbHandler);
+
   checkPageAccess($dbHandler,$argsObj);  // Will exit if check failed
 
   $platMgr = new tlPlatform($dbHandler, $argsObj->tproject_id);
@@ -105,15 +113,18 @@ function init_args( &$dbH )
   $_REQUEST = strings_stripSlashes($_REQUEST);
 
   $args = new stdClass();
-  $iParams = 
-    array("doAction" => array(tlInputParameter::STRING_N,0,50),
-          "id" => array(tlInputParameter::INT_N),
-          "platform_id" => array(tlInputParameter::INT_N),
-          "name" => array(tlInputParameter::STRING_N,0,100),
-          "notes" => array(tlInputParameter::STRING_N),
-          'tproject_id' => array(tlInputParameter::INT_N),
-          "enable_on_execution" => array(tlInputParameter::CB_BOOL),
-          "enable_on_design" => array(tlInputParameter::CB_BOOL));
+  $iParams = [
+    "doAction" => [tlInputParameter::STRING_N,0,50],
+    "id" => [tlInputParameter::INT_N],
+    "platform_id" => [tlInputParameter::INT_N],
+    "name" => [tlInputParameter::STRING_N,0,100],
+    "notes" => [tlInputParameter::STRING_N],
+    "tproject_id" => [tlInputParameter::INT_N],
+    "tplan_id" => [tlInputParameter::INT_N],
+    "enable_on_execution" => [tlInputParameter::CB_BOOL],
+    "enable_on_design" => [tlInputParameter::CB_BOOL],
+    "is_open" => [tlInputParameter::CB_BOOL]
+  ];
     
   R_PARAMS($iParams,$args);
   if (null == $args->platform_id || $args->platform_id <= 0) {
@@ -148,6 +159,7 @@ function init_args( &$dbH )
 
   $args->currentUser = $_SESSION['currentUser'];
   
+  // Checkboxes
   if (null == $args->enable_on_design) {
     $args->enable_on_design = 0;
   }
@@ -155,6 +167,11 @@ function init_args( &$dbH )
   if (null == $args->enable_on_execution) {
     $args->enable_on_execution = 0;
   }
+
+  if (null == $args->is_open) {
+    $args->is_open = 0;
+  }
+
   return $args;
 }
 
@@ -203,13 +220,20 @@ function edit(&$args,&$gui,&$platform_mgr) {
   if ($platform) {
     $args->enable_on_design = $platform['enable_on_design'];
     $args->enable_on_execution = $platform['enable_on_execution'];
+    $args->is_open = $platform['is_open'];
+
     $args->name = $platform['name'];
     $args->notes = $platform['notes'];
 
+    // ---------------------------------------------------------
+    // Copy from args into $gui 
     $gui->enable_on_design = $args->enable_on_design;
     $gui->enable_on_execution = $args->enable_on_execution;
+    $gui->is_open = $args->is_open;
+  
     $gui->name = $args->name;
     $gui->notes = $args->notes;
+    // ---------------------------------------------------------
 
     $gui->action_descr .= TITLE_SEP . $platform['name'];
   }
@@ -222,9 +246,12 @@ function edit(&$args,&$gui,&$platform_mgr) {
 }
 
 /**
+ * function: do_create 
+ *           do operations on db
  *
  */
-function do_create(&$args,&$gui,&$platform_mgr) {
+function do_create(&$args,&$gui,&$platform_mgr) 
+{
   $gui->main_descr = lang_get('platform_management');
   $gui->action_descr = lang_get('create_platform');
   $gui->submit_button_label = lang_get('btn_save');
@@ -234,12 +261,16 @@ function do_create(&$args,&$gui,&$platform_mgr) {
   $ret->template = 'platformsView.tpl';
   $plat = new stdClass();
   $plat->name = $args->name; 
-  $k2c = array('notes' => null,'enable_on_design' => 0,
-               'enable_on_execution' => 1);
+  $k2c = [
+    'notes' => null,
+    'enable_on_design' => 0,
+    'enable_on_execution' => 0,
+    'is_open' => 0
+  ];
+
   foreach ($k2c as $prop => $defa) {
     $plat->$prop = property_exists($args, $prop) ? $args->$prop : $defa;
   }
-
   $op = $platform_mgr->create($plat);
 
   $ret->status = $op['status']; 
@@ -266,10 +297,13 @@ function do_update(&$args,&$gui,&$platform_mgr) {
 
   $ret = new stdClass();
   $ret->template = 'platformsView.tpl';
-  $ret->status = 
-    $platform_mgr->update($args->platform_id,$args->name,$args->notes,
-                          $args->enable_on_design,
-                          $args->enable_on_execution);
+  $ret->status = $platform_mgr->update(
+                    $args->platform_id,
+                    $args->name,$args->notes,
+                    $args->enable_on_design,
+                    $args->enable_on_execution,
+                    $args->is_open
+                 );
   $ret->user_feedback = sprintf(lang_get('platform_updated'), $args->name);
 
   return $ret;
@@ -337,7 +371,13 @@ function init_gui(&$db,&$args,&$platMgr) {
   $gui->name = $args->name;
   $gui->notes = $args->notes;
   $gui->platform_id = $args->platform_id;
-    
+  $gui->tproject_id = $args->tproject_id;
+  $gui->tplan_id = $args->tplan_id;
+
+  $gui->enable_on_design = 0;
+  $gui->enable_on_execution = 0;
+  $gui->is_open = 0;
+
   return $gui;
 }
 
@@ -346,8 +386,7 @@ function init_gui(&$db,&$args,&$platMgr) {
  */
 function checkPageAccess(&$db,&$argsObj) {
   $env['script'] = basename(__FILE__);
-  $env['tproject_id'] = isset($argsObj->tproject_id) ? 
-                        $argsObj->tproject_id : 0;
+  $env['tproject_id'] = isset($argsObj->tproject_id) ? $argsObj->tproject_id : 0;
   $env['tplan_id'] = isset($argsObj->tplan_id) ? $argsObj->tplan_id : 0;
   $argsObj->currentUser->checkGUISecurityClearance($db,$env,array('platform_management'),'and');
 }

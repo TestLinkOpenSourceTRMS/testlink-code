f7356d80a1bd0dcb4b4a0fdc4788e90e3a642c9f
u0113 <francisco.mancardi@tesiquare.com>
2022-12-31 11:43:35 +0100

    code layout -> some array(..) replaced with []

--- lib/functions/treeMenu.inc.php
+++ lib/functions/treeMenu.inc.php
@@ -51,12 +51,14 @@ function generateTestSpecTree(&$db,$tproject_id, $tproject_name,$linkto,$filters
   // may be we can remove ?
 
   // keys -> filter_* come from tlTestCaseFilterControl.class.php
-  $my['filters'] = array('keywords' => null, 
-                         'executionType' => null, 
-                         'importance' => null,
-                         'testplan' => null, 
-                         'filter_tc_id' => null,
-                         'filter_platforms' => null);
+  $my['filters'] = [
+    'keywords' => null, 
+    'executionType' => null, 
+    'importance' => null,
+    'testplan' => null, 
+    'filter_tc_id' => null,
+    'filter_platforms' => null
+  ];
 
   $my['options'] = array_merge($my['options'], (array)$options);
   $my['options']['showTestCaseID'] = config_get('treemenu_show_testcase_id');

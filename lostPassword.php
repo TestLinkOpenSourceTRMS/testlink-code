1eb15d94784536a69b30de969acd520cc69a3a7e
DanProgs <Abenteurer.2901@web.de>
2025-05-21 23:13:22 +0200

    minor adjustments:
    - keywords adjusted (parentheses removed, else if adapted),
    - parameters and returns adjusted in the description,

--- lostPassword.php
+++ lostPassword.php
@@ -4,10 +4,10 @@
  * This script is distributed under the GNU General Public License 2 or later. 
  *
 **/
-require_once('config.inc.php');
-require_once('common.php');
-require_once('users.inc.php');
-require_once('email_api.php');
+require_once 'config.inc.php';
+require_once 'common.php';
+require_once 'users.inc.php';
+require_once 'email_api.php';
 $templateCfg = templateConfiguration();
 
 $args = init_args();
@@ -24,35 +24,35 @@ $op = doDBConnect($db,database::ONERROREXIT);
 
 $userID = false;
 if ($args->login != "") {
-  $userID = tlUser::doesUserExist($db,$args->login);
-  if (!$userID) {
-    $gui->note = lang_get('bad_user');
-  } else {
-    // need to know if auth method for user allows reset
-    $user = new tlUser(intval($userID));
-    $user->readFromDB($db);
-    if(tlUser::isPasswordMgtExternal($user->authentication,$user->authentication)) {
-      $gui->external_password_mgmt = 1;
-      $gui->password_mgmt_feedback = sprintf(lang_get('password_mgmt_feedback'),trim($args->login));
-    }  
-  }
+    $userID = tlUser::doesUserExist($db,$args->login);
+    if (!$userID) {
+        $gui->note = lang_get('bad_user');
+    } else {
+        // need to know if auth method for user allows reset
+        $user = new tlUser(intval($userID));
+        $user->readFromDB($db);
+        if(tlUser::isPasswordMgtExternal($user->authentication,$user->authentication)) {
+            $gui->external_password_mgmt = 1;
+            $gui->password_mgmt_feedback = sprintf(lang_get('password_mgmt_feedback'),trim($args->login));
+        }
+    }
 }
 
 if(!$gui->external_password_mgmt && $userID) {
-  $result = resetPassword($db,$userID);
-  $gui->note = $result['msg'];
-  if ($result['status'] >= tl::OK) {
-    $user = new tlUser($userID);
-    if ($user->readFromDB($db) >= tl::OK) {
-      logAuditEvent(TLS("audit_pwd_reset_requested",$user->login),"PWD_RESET",$userID,"users");
-    }
-    redirect(TL_BASE_HREF ."login.php?note=lost&viewer={$args->viewer}");
-    exit();
-  } else if ($result['status'] == tlUser::E_EMAILLENGTH) {
-    $gui->note = lang_get('mail_empty_address');
-  } else if ($note != "") {
-    $gui->note = getUserErrorMessage($result['status']);
-  } 
+    $result = resetPassword($db,$userID);
+    $gui->note = $result['msg'];
+    if ($result['status'] >= tl::OK) {
+        $user = new tlUser($userID);
+        if ($user->readFromDB($db) >= tl::OK) {
+            logAuditEvent(TLS("audit_pwd_reset_requested",$user->login),"PWD_RESET",$userID,"users");
+        }
+        redirect(TL_BASE_HREF ."login.php?note=lost&viewer={$args->viewer}");
+        exit();
+    } elseif ($result['status'] == tlUser::E_EMAILLENGTH) {
+        $gui->note = lang_get('mail_empty_address');
+    } elseif ($note != "") {
+        $gui->note = getUserErrorMessage($result['status']);
+  }
 }
 
 $smarty = new TLSmarty();
@@ -60,13 +60,14 @@ $smarty->assign('gui',$gui);
 
 $tpl = str_replace('.php','.tpl',basename($_SERVER['SCRIPT_NAME']));
 if( $args->viewer == 'new' ) {
-  $tpl = 'lostPassword-model-marcobiedermann.tpl';
-}  
+    $tpl = 'lostPassword-model-marcobiedermann.tpl';
+}
 $tpl = 'login/' . $tpl;
 $smarty->display($tpl);
 
 /**
- *
+ * 
+ * @return stdClass
  */
 function init_args()
 {

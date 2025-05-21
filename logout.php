ebbd94c21991900c4553fbb3253482e62a997cfd
DanProgs <Abenteurer.2901@web.de>
2025-05-21 23:05:46 +0200

    minor adjustments:
    - keywords adjusted (parentheses removed),
    - parameters and returns adjusted in the description,

--- logout.php
+++ logout.php
@@ -1,40 +1,41 @@
 <?php
 /**
- * TestLink Open Source Project - http://testlink.sourceforge.net/ 
- * This script is distributed under the GNU General Public License 2 or later. 
- * 
+ * TestLink Open Source Project - http://testlink.sourceforge.net/
+ * This script is distributed under the GNU General Public License 2 or later.
+ *
  * @filesource	logout.php
  *
  *
 **/
-require_once('config.inc.php');
-require_once('common.php');
+require_once 'config.inc.php';
+require_once 'common.php';
 testlinkInitPage($db);
 
 $args = init_args();
 if ($args->userID) {
-  logAuditEvent(TLS("audit_user_logout",$args->userName),"LOGOUT",$args->userID,"users");  
+    logAuditEvent(TLS("audit_user_logout",$args->userName),"LOGOUT",$args->userID,"users");
 }
 session_unset();
 session_destroy();
 
 $authCfg = config_get('authentication');
-if(isset($authCfg['SSO_enabled']) && $authCfg['SSO_enabled'] 
+if(isset($authCfg['SSO_enabled']) && $authCfg['SSO_enabled']
    && $args->ssodisable == FALSE) {
-  redirect($authCfg['SSO_logout_destination']);
+    redirect($authCfg['SSO_logout_destination']);
 } else {
-  $std = "login.php?note=logout&viewer={$args->viewer}";
-  $std .= $args->ssodisable ? "&ssodisable" : '';
+    $std = "login.php?note=logout&viewer={$args->viewer}";
+    $std .= $args->ssodisable ? "&ssodisable" : '';
 
-  $xx = config_get('logoutUrl');
-  $lo = is_null($xx) || trim($xx) == '' ? $std : $xx;
-  redirect($lo);
+    $xx = config_get('logoutUrl');
+    $lo = is_null($xx) || trim($xx) == '' ? $std : $xx;
+    redirect($lo);
 }
 exit();
 
 
 /**
  *
+ * @return stdClass
  */
 function init_args() {
 	$args = new stdClass();
@@ -43,7 +44,7 @@ function init_args() {
 	$args->userName = $args->userID ? $_SESSION['currentUser']->getDisplayName() : "";
 	
 	$args->viewer = isset($_GET['viewer']) ? $_GET['viewer'] : '';
-  $args->ssodisable = getSSODisable();
+	$args->ssodisable = getSSODisable();
 	
   return $args;
 }

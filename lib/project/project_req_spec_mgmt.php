76f84c359272fc27058cdb2a291cf214f5a6cdd9
DanProgs <Abenteurer.2901@web.de>
2025-05-26 22:44:03 +0200

    minor adjustments:
    - keywords adjusted (parentheses removed),
    - trailing whitespaces removed
    - parameters and returns adjusted in the description

--- lib/project/project_req_spec_mgmt.php
+++ lib/project/project_req_spec_mgmt.php
@@ -7,8 +7,8 @@
  * @author 		Martin Havlat
  *
  */
-require_once('../../config.inc.php');
-require_once('common.php');
+require_once '../../config.inc.php';
+require_once 'common.php';
 testlinkInitPage($db,false,false);
 
 $tproject_id   = isset($_SESSION['testprojectID']) ? intval($_SESSION['testprojectID']) : 0;
@@ -21,16 +21,14 @@ $context->tproject_id = $tproject_id;
 checkRights($db,$uo,$context);
 
 $gui = new stdClass();
-$gui->main_descr = lang_get('testproject') .  TITLE_SEP . $tproject_name . TITLE_SEP . lang_get('title_req_spec');
+$gui->main_descr = lang_get('testproject') . TITLE_SEP . $tproject_name . TITLE_SEP . lang_get('title_req_spec');
 $gui->tproject_id = $tproject_id;
 $gui->refresh_tree = 'no';
 
 
 $gui->grants = new stdClass();
-$gui->grants->modify = 
-  $uo->hasRight($db,'mgt_modify_req',$context->tproject_id);
-$gui->grants->ro = 
-  $uo->hasRight($db,'mgt_view_req',$context->tproject_id);
+$gui->grants->modify = $uo->hasRight($db,'mgt_modify_req',$context->tproject_id);
+$gui->grants->ro = $uo->hasRight($db,'mgt_view_req',$context->tproject_id);
 
 $smarty = new TLSmarty();
 $smarty->assign('gui', $gui);
@@ -38,8 +36,11 @@ $smarty->display('requirements/project_req_spec_mgmt.tpl');
 
 /**
  *
+ * @param database $db
+ * @param $user
+ * @param $context
  */
-function checkRights(&$db, &$user, $context) 
+ function checkRights(&$db, &$user, $context)
 {
   $context->rightsOr = ["mgt_view_req","mgt_modify_req"];
   $context->rightsAnd = [];

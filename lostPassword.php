b8893fa858d3fb15b23f5db8eeea03f58ceb255f
DanProgs <Abenteurer.2901@web.de>
2025-06-08 23:40:13 +0200

    Cleaned up code - some adjustments to selected files The following adjustments were made: - Files should end with a newline (PHP:S113) - Sections of code should not be commented out (PHP:S125) - Identical expressions should not be used on both sides of a binary operator (PHP:S1764) - PHP keywords and constants "true", "false", "null" should be lower case (PHP:S1781) - "elseif" keyword should be used in place of "else if" keywords (PHP:S1793) - Two branches in a conditional structure should not have exactly the same implementation (PHP:S1871) - Array values should not be replaced unconditionally (PHP:S4143)

--- lostPassword.php
+++ lostPassword.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * TestLink Open Source Project - http://testlink.sourceforge.net/ 
- * This script is distributed under the GNU General Public License 2 or later. 
+ * TestLink Open Source Project - http://testlink.sourceforge.net/
+ * This script is distributed under the GNU General Public License 2 or later.
  *
-**/
+ **/
 require_once 'config.inc.php';
 require_once 'common.php';
 require_once 'users.inc.php';
@@ -24,35 +24,35 @@ $op = doDBConnect($db,database::ONERROREXIT);
 
 $userID = false;
 if ($args->login != "") {
-  $userID = tlUser::doesUserExist($db,$args->login);
-  if (!$userID) {
-    $gui->note = lang_get('bad_user');
-  } else {
-    // need to know if auth method for user allows reset
-    $user = new tlUser(intval($userID));
-    $user->readFromDB($db);
-    if(tlUser::isPasswordMgtExternal($user->authentication,$user->authentication)) {
-      $gui->external_password_mgmt = 1;
-      $gui->password_mgmt_feedback = sprintf(lang_get('password_mgmt_feedback'),trim($args->login));
-    }  
-  }
+    $userID = tlUser::doesUserExist($db,$args->login);
+    if (!$userID) {
+        $gui->note = lang_get('bad_user');
+    } else {
+        // need to know if auth method for user allows reset
+        $user = new tlUser(intval($userID));
+        $user->readFromDB($db);
+        if(tlUser::isPasswordMgtExternal($user->authentication,$user->authentication)) {
+            $gui->external_password_mgmt = 1;
+            $gui->password_mgmt_feedback = sprintf(lang_get('password_mgmt_feedback'),trim($args->login));
+        }
+    }
 }
 
 if(!$gui->external_password_mgmt && $userID) {
-  $result = resetPassword($db,$userID);
-  $gui->note = $result['msg'];
-  if ($result['status'] >= tl::OK) {
-    $user = new tlUser($userID);
-    if ($user->readFromDB($db) >= tl::OK) {
-      logAuditEvent(TLS("audit_pwd_reset_requested",$user->login),"PWD_RESET",$userID,"users");
+    $result = resetPassword($db,$userID);
+    $gui->note = $result['msg'];
+    if ($result['status'] >= tl::OK) {
+        $user = new tlUser($userID);
+        if ($user->readFromDB($db) >= tl::OK) {
+            logAuditEvent(TLS("audit_pwd_reset_requested",$user->login),"PWD_RESET",$userID,"users");
+        }
+        redirect(TL_BASE_HREF ."login.php?note=lost&viewer={$args->viewer}");
+        exit();
+    } elseif ($result['status'] == tlUser::E_EMAILLENGTH) {
+        $gui->note = lang_get('mail_empty_address');
+    } elseif ($note != "") {
+        $gui->note = getUserErrorMessage($result['status']);
     }
-    redirect(TL_BASE_HREF ."login.php?note=lost&viewer={$args->viewer}");
-    exit();
-  } else if ($result['status'] == tlUser::E_EMAILLENGTH) {
-    $gui->note = lang_get('mail_empty_address');
-  } else if ($note != "") {
-    $gui->note = getUserErrorMessage($result['status']);
-  } 
 }
 
 $smarty = new TLSmarty();
@@ -60,21 +60,23 @@ $smarty->assign('gui',$gui);
 
 $tpl = str_replace('.php','.tpl',basename($_SERVER['SCRIPT_NAME']));
 if( $args->viewer == 'new' ) {
-  $tpl = 'lostPassword-model-marcobiedermann.tpl';
-}  
+    $tpl = 'lostPassword-model-marcobiedermann.tpl';
+}
 $tpl = 'login/' . $tpl;
 $smarty->display($tpl);
 
 /**
+ * Initializes the arguments
  *
+ * @return stdClass
  */
 function init_args()
 {
-  $iParams = array("login" => array('POST',tlInputParameter::STRING_N,0,30),
-                   "viewer" => array('GET',tlInputParameter::STRING_N, 0, 3));
-  
-  $args = new stdClass();
-  I_PARAMS($iParams,$args);
-  return $args;
+    $iParams = array("login" => array('POST',tlInputParameter::STRING_N,0,30),
+        "viewer" => array('GET',tlInputParameter::STRING_N, 0, 3));
+    
+    $args = new stdClass();
+    I_PARAMS($iParams,$args);
+    return $args;
 }
 ?>
